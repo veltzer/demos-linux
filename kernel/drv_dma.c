f7e2eeae681af397a87a513dee9cb961fcc6c2f5
Mark Veltzer <mark.veltzer@gmail.com>
2010-09-07 12:00:50 +0300

    improved the test to check the limits on dma alloc

--- kernel/drv_dma.c
+++ kernel/drv_dma.c
@@ -11,6 +11,7 @@
 #include <linux/proc_fs.h>
 #include <linux/mm.h>
 
+#define DO_DEBUG
 #include "kernel_helper.h"
 
 /*
@@ -27,40 +28,78 @@ MODULE_DESCRIPTION("Demo module for testing");
 
 // our own functions
 
-const unsigned int size = 1024 * 1024 * 24;
-//dma_addr_t device_addr;
-unsigned int device_addr;
-void         *vptr;
+//static dma_addr_t device_addr;
 
-#define DO_LOOP
+// just one test (allocate and deallocate)
+//#defie DO_ONE
+// allocate and free in a loop
+//#define DO_LOOP
+// slowly increase the size of the memory we are allocating until we bust...
+#define DO_INC
 
 static int __init mod_init(void) {
+	void* vptr;
+	//unsigned int device_addr;
 #ifdef DO_LOOP
 	int i;
 #endif // DO_LOOP
+#ifdef DO_INC
+	unsigned int size;
+	unsigned int inc;
+	bool stop;
+#endif // DO_INC
+#ifdef DO_ONE
+	const unsigned int size = 1024 * 1024 * 24;
+#endif // DO_ONE
 	DEBUG("start");
 #ifdef DO_LOOP
 	for (i = 0; i < 1000; i++) {
 		vptr = dma_alloc_coherent(NULL, size, &device_addr, GFP_KERNEL | GFP_DMA);
+		if(vptr==NULL) {
+			printk("ERROR! could not allocate memory");
+		}
 		dma_free_coherent(NULL, size, vptr, device_addr);
 	}
-#else // DO_LOOP
-	vptr = dma_alloc_coherent(NULL, size, &device_addr, GFP_KERNEL | GFP_DMA);
-	printk("vptr is %p\n", vptr);
-	printk("size is %d\n", size);
-	printk("device_addr is %d\n", device_addr);
 #endif // DO_LOOP
+#ifdef DO_ONE
+	vptr = dma_alloc_coherent(NULL, size, &device_addr, GFP_KERNEL | GFP_DMA);
+	if(vptr!=NULL) {
+		printk("vptr is %p\n", vptr);
+		printk("size is %d\n", size);
+		printk("device_addr is %d\n", device_addr);
+		dma_free_coherent(NULL, size, vptr, device_addr);
+	} else {
+		printk("ERROR! could not allocate memory");
+	}
+#endif // DO_ONE
+#ifdef DO_INC
+	inc=1024*512;
+	size=1024*512; // half a meg
+	stop=false;
+	while(!stop) {
+		//vptr = dma_alloc_coherent(NULL, size, &device_addr, GFP_DMA);
+		vptr = kmalloc(size,GFP_DMA);
+		if(vptr!=NULL) {
+			printk("vptr is %p\n", vptr);
+			printk("size is %d\n", size);
+			//printk("device_addr is %d\n", device_addr);
+			//dma_free_coherent(NULL, size, vptr, device_addr);
+			kfree(vptr);
+		} else {
+			printk("ERROR! could not allocate memory for size %d",size);
+			stop=true;
+		}
+		size+=inc;
+	}
+#endif // DO_INC
+	DEBUG("end");
 	return(0);
 }
 
-
 static void __exit mod_exit(void) {
 	DEBUG("start");
-#ifndef DO_LOOP
-	dma_free_coherent(NULL, size, vptr, device_addr);
-#endif // DO_LOOP
+	DEBUG("end");
 }
-
 
 // declaration of init/cleanup functions of this module
 
