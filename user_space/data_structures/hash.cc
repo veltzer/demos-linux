cbb3ad82314a1e72bd8fdf0bd549d4576a0c51aa
Mark Veltzer <mark.veltzer@gmail.com>
2010-09-19 14:52:30 +0200

    hash table demos...

---
+++ user_space/data_structures/hash.cc
@@ -0,0 +1,57 @@
+#include<search.h> // for hcreate_r(3), hdestroy_r(3),
+#include<stdlib.h> // for exit(3)
+#include<stdio.h> // for perror(3)
+
+// the hash table
+static hsearch_data HTAB;
+
+int hash_init(void) {
+	int res=hcreate_r(100,&HTAB);
+	return res;
+}
+
+int hash_destroy( void) {
+	hdestroy_r(&HTAB);
+	return 1;
+}
+
+const char* hash_get(const char* key) {
+	ENTRY item;
+	item.key=(char*)key;
+	ENTRY* ritem;
+	int ret=hsearch_r(item,FIND,&ritem,&HTAB);
+	if(ret==0) {
+		return NULL;
+	} else {
+		return (const char*)(ritem->data);
+	}
+}
+int hash_put(const char* key,const char* data) {
+	ENTRY item;
+	item.key=(char*)key;
+	item.data=(void*)data;
+	ENTRY* ritem;
+	return hsearch_r(item,ENTER,&ritem,&HTAB);
+}
+const char* d1_key="mark";
+const char* d1_val="veltzer";
+
+int main(int argc,char** argv,char** envp) {
+	if(hash_init()==0) {
+		perror("hash_init failed");
+		exit(1);
+	}
+	printf("after init\n");
+	if(hash_put(d1_key,d1_val)==0) {
+		perror("hash_put failed");
+		exit(1);
+	}
+	printf("after put\n");
+	const char* getval=hash_get(d1_key);
+	printf("got %s\n",getval);
+	if(hash_destroy()==0) {
+		perror("hash_destroy failed");
+		exit(1);
+	}
+	return 0;
+}
---
+++ user_space/data_structures/hash2.cc
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <search.h>
+
+const char *data[] = { "alpha", "bravo", "charlie", "delta",
+	"echo", "foxtrot", "golf", "hotel", "india", "juliet",
+	"kilo", "lima", "mike", "november", "oscar", "papa",
+	"quebec", "romeo", "sierra", "tango", "uniform",
+	"victor", "whisky", "x-ray", "yankee", "zulu"
+};
+int main() {
+	ENTRY e, *ep;
+	int i;
+
+	/* starting with small table, and letting it grow does not work */
+	hcreate(30);
+	for (i = 0; i < 24; i++) {
+		e.key = (char*)(data[i]);
+		/* data is just an integer, instead of a
+		 * pointer to something */
+		e.data = (void *)i;
+		ep = hsearch(e, ENTER);
+		/* there should be no failures */
+		if (ep == NULL) {
+			fprintf(stderr, "entry failed\n");
+			exit(1);
+		}
+	}
+	for (i = 22; i < 26; i++) {
+		/* print two entries from the table, and
+		 * show that two are not in the table */
+		e.key =(char*)(data[i]);
+		ep = hsearch(e, FIND);
+		printf("%9.9s -> %9.9s:%d\n", e.key,
+			ep ? ep->key : "NULL",
+			ep ? (int)(ep->data) : 0);
+	}
+	return 0;
+}
