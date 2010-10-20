8b9f9bbe07b95f8d149d552c92ec6cb426150f91
Mark Veltzer <mark.veltzer@gmail.com>
2010-10-20 02:52:44 +0200

    application generating seg fault

---
+++ user_space/error_handling/seg_fault.cc
@@ -0,0 +1,18 @@
+#include<stdio.h> // for fprintf(3)
+
+void func() {
+	int i;
+	const char* y __attribute__((unused))="123123dsfshfdkj$%^$%&^^%&@";
+	for(i=0;i<100;i++) {
+		fprintf(stderr,"i is %d\n",i);
+	}
+}
+
+int main(int argc,char** argv,char** envp) {
+	char* f=(char*)func;
+	f++;
+	f++;
+	void (*g)()=(void (*)())f;
+	g();
+	return(0);
+}
