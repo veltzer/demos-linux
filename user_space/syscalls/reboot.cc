1c6f0e728b50a314c79d534e37e7ac9d050a9698
Mark Veltzer <mark.veltzer@gmail.com>
2010-10-20 04:04:06 +0200

    improvements

--- user_space/syscalls/reboot.cc
+++ user_space/syscalls/reboot.cc
@@ -1,28 +1,20 @@
-#include <stdio.h>
-#include <string.h>
+#include<unistd.h> // for reboot(2), syscall(2)
+#include<linux/reboot.h> // for reboot(2)
+#include<sys/syscall.h> // for syscall(2), SYS_xxx definitions
 
-// this is from the reboot(2) manual page...
-#include <unistd.h>
-#include <linux/reboot.h>
-
-// this is from the syscall(2) manual page...
-
-#include <unistd.h>
-#include <sys/syscall.h> // For SYS_xxx definitions
-
-#include "us_helper.hh"
+#include"us_helper.hh"
 
 /*
  *      Example of how to reboot the system
  *      You need to have the right capabilities or you have to be running as root in order
  *      to run this program. If you have the right permissions then this program will reboot
- *      your machine...
+ *      your machine, so you better watch out...
  *
  *              Mark Veltzer
  *
  * EXTRA_LIBS=
  */
 int main(int argc, char **argv, char **envp) {
-	SCIG(syscall(SYS_reboot, LINUX_REBOOT_MAGIC1, LINUX_REBOOT_MAGIC2, LINUX_REBOOT_CMD_RESTART, NULL), "trying to reboot the system");
+	scie(syscall(SYS_reboot, LINUX_REBOOT_MAGIC1, LINUX_REBOOT_MAGIC2, LINUX_REBOOT_CMD_RESTART, NULL), "trying to reboot the system");
 	return(0);
 }
