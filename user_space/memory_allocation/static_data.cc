63026dd8df1c6ff29b117358024e1afa93838e83
Mark Veltzer <mark.veltzer@gmail.com>
2010-10-20 06:44:48 +0200

    another example about memory allocation

---
+++ user_space/memory_allocation/static_data.cc
@@ -0,0 +1,40 @@
+#include<stdio.h> // for printf(3)
+#include<stdlib.h> // for atoi(3)
+#include<string.h> // for memset(3)
+
+#include"us_helper.hh" // for my_system
+
+/*
+ * This example explores how static data (data segement) affects binary size
+ * and speed
+ *
+ * Notes:
+ * - if you initialize the array to 0 (first example) then the binary is small.
+ * - if you do not initialize the array (second example) then the binary is small.
+ * - if you initialize the array to anything which is not 0 then the binary is large.
+ *
+ * 					Mark Veltzer
+ */
+
+const unsigned int size=1000000;
+// first example
+//int myarray[size]={ 0 };
+// second example
+//int myarray[size];
+// third example
+int myarray[size]={ 3 };
+
+int main(int argc,char** argv,char** envp) {
+	unsigned int sum=0;
+	for(int i=0;i<argc;i++) {
+		sum+=atoi(argv[i]);
+	}
+	memset(myarray,sum,sizeof(int)*size);
+	sum=0;
+	for(unsigned int l=0;l<size;l++) {
+		sum+=myarray[l];
+	}
+	printf("sum is %u\n",sum);
+	my_system("ls -l %s",argv[0]);
+	return(0);
+}
