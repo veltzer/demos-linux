78ead3415abcf9aa39386d8650b083c7512bd21d
Mark Veltzer <mark.veltzer@gmail.com>
2010-10-20 04:37:57 +0200

    improved the example

--- user_space/processes_and_stacks/stack.cc
+++ user_space/processes_and_stacks/stack.cc
@@ -1,10 +1,20 @@
 #include<stdio.h>
-#include<string.h>
+#include<string.h> // for memset(3)
+
+/*
+ * This example shows the stack limitations of a regular program
+ *
+ * Notes:
+ * - this is why it's a bad idea to store stuff on the stack.
+ * - not that without the memset there will be no problem.
+ *
+ * 			Mark Veltzer
+ */
 
 void func(void) {
 	const unsigned int m=10;
 	const unsigned int size=m*1024*1024;
-	char data[size];
+	char data[size] __attribute((unused));
 	memset(data,0,size);
 }
 
