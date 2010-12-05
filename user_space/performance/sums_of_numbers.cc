f200b9dae8f890373ae97ca7ece3b3fc135098ee
Mark Veltzer <mark.veltzer@gmail.com>
2010-12-06 06:32:30 +0800

    improved the documentation of the example

--- user_space/performance/sums_of_numbers.cc
+++ user_space/performance/sums_of_numbers.cc
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stdio.h> // for printf(3)
 #include <string.h>
 
 /*
@@ -6,11 +6,21 @@
  * of numbers at compile time and avoids making the loops at runtime at
  * all... Pretty good! (g++ 4.4.3)
  *
+ * Why is this important? Because you may be very confused when debugging
+ * an application and seeing that much of the code that you write just isn't
+ * there. The wisdom derived here is never underestimate the compilers wisdom
+ * to throw your code away or to morph it to something which will be very hard
+ * to recognize.
+ *
  * Use a disassembler to see the resulting machine code...
  *
  *              Mark Veltzer
  *
  * EXTRA_LIBS=
+ *
+ * TODO:
+ * - make this example disassemble itself and show what the compiler did, or maybe
+ *   morph it's own code to show that the sum is there ?
  */
 int main(int argc, char **argv, char **envp) {
 	int sum = 0;
