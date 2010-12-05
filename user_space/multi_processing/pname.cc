14a1688e63f2725818dfe0cb27c9da514d5b873a
Mark Veltzer <mark.veltzer@gmail.com>
2010-12-06 06:10:15 +0800

    added demo about changing the process name

---
+++ user_space/multi_processing/pname.cc
@@ -0,0 +1,68 @@
+#include <sys/prctl.h> // for prctl(2)
+#include <string.h> // for strncpy(3)
+#include <sys/types.h> // for getpid(2)
+#include <unistd.h> // for getpid(2)
+
+#include "us_helper.hh"
+
+/*
+ * This example shows how to use prctl to set/get the current process name.
+ * The name itself is up to 16 bytes long.
+ * It also shows that this is reflected in the output of tools like ps and in the /proc
+ * filesystem.
+ * Notice that the comm attribute is what you are setting getting (/proc/[pid]/comm, ps -o comm) and not other attributes like command, cmd and others from ps or from /proc.
+ *
+ * When would you want to use this?
+ * - when you're doing lots of multi processing (fork(2)) and want to distinguish between
+ *   the various processes.
+ * - when whatever it is that your doing depends on how you were run (envrionment,
+ *   directory, conditions that you find, time of day, argv[0], whatever...).
+ * - set names of threads in a multi threaded application (will be in a separate demo).
+ *
+ *                              Mark Veltzer
+ * EXTRA_LIBS=
+ *
+ * TODO:
+ * - add a fork(2) to this demo and show that parent and child can have different names.
+ */
+
+void print_my_name() {
+	const unsigned int size=16;
+	char name[size];
+	scie(prctl(PR_GET_NAME,name),"prctl");
+	TRACE("process name is [%s]",name);
+}
+
+void get_my_name(char* buffer,unsigned int bufsize) {
+	const unsigned int size=16;
+	char name[size];
+	scie(prctl(PR_GET_NAME,name),"prctl");
+	strncpy(buffer,name,bufsize);
+}
+
+void set_my_name(const char* newname) {
+	const unsigned int size=16;
+	char name[size];
+	strncpy(name,newname,size);
+	scie(prctl(PR_SET_NAME,name),"prctl");
+}
+
+void print_my_name_from_proc() {
+	my_system("cat /proc/%d/comm",getpid());
+}
+
+int main(int argc, char **argv, char **envp) {
+	TRACE("start");
+	const char* newname="newpname";
+	print_my_name();
+	print_my_name_from_proc();
+	char myname[256];
+	get_my_name(myname,256);
+	my_system("ps -o comm | grep %s | grep -v grep",myname);
+	set_my_name(newname);
+	print_my_name();
+	print_my_name_from_proc();
+	my_system("ps -o comm | grep %s | grep -v grep",newname);
+	TRACE("end");
+	return(0);
+}
