3b8a1615b66b963e5d299a942c0e069480ffb09a
Mark Veltzer <mark.veltzer@gmail.com>
2010-12-06 06:56:54 +0800

    adding more verbosity

--- user_space/threading/thr_names.cc
+++ user_space/threading/thr_names.cc
@@ -21,7 +21,17 @@
  * TODO:
  * - don't do the ugly sleep(3) here but rather wait on a condition in the threads
  *   and have the main thread signal that condition after doing the "ps" command.
+ * - also have the main thread wait on a barrier that will be released once the two threads
+ *   did their setup work (set their own names). Only then should the main thread actually
+ *   do the ps to find their names.
  */
+
+void get_thread_name(char* buffer,unsigned int bufsize) {
+	const unsigned int size=16;
+	char name[size];
+	scie(prctl(PR_GET_NAME,name),"prctl");
+	strncpy(buffer,name,bufsize);
+}
 
 void set_thread_name(const char* newname) {
 	const unsigned int size=16;
@@ -35,20 +45,23 @@ void print_thread_name_from_proc() {
 }
 
 void* doit(void* arg) {
+	char orig_name[256];
+	get_thread_name(orig_name,256);
+	TRACE("original thread name is [%s]",orig_name);
 	const char* name=(const char*)arg;
 	set_thread_name(name);
-	printf("gettid() is %d\n",gettid());
-	printf("getpid() is %d\n",getpid());
-	printf("pthread_self() is %u\n",(unsigned int)pthread_self());
+	TRACE("gettid() is %d",gettid());
+	TRACE("getpid() is %d",getpid());
+	TRACE("pthread_self() is %u",(unsigned int)pthread_self());
 	print_thread_name_from_proc();
 	sleep(10);
 	return NULL;
 }
 
 int main(int argc,char** argv,char** envp) {
-	printf("gettid() is %d\n",gettid());
-	printf("getpid() is %d\n",getpid());
-	printf("pthread_self() is %u\n",(unsigned int)pthread_self());
+	TRACE("gettid() is %d",gettid());
+	TRACE("getpid() is %d",getpid());
+	TRACE("pthread_self() is %u",(unsigned int)pthread_self());
 	pthread_t t1,t2;
 	CHECK_ZERO(pthread_create(&t1,NULL,doit,(void*)"thread one"));
 	CHECK_ZERO(pthread_create(&t2,NULL,doit,(void*)"thread two"));
