b99cb189e72c887b34f30d42a62041451ff1f59b
Mark Veltzer <mark.veltzer@gmail.com>
2010-12-08 14:23:24 +0800

    added simple demo

---
+++ user_space/simple/infinite.cc
@@ -0,0 +1,14 @@
+/*
+ * This is a demo of a program always wanting the CPU.
+ * If you look at it through ps like in:
+ * ps -o comm,pid,state -e | grep infinite
+ * you will see it in a running state.
+ *
+ * 				Mark Veltzer
+ */
+
+int main(int argc,char** argv,char** envp) {
+	while(1) {
+	}
+	return 0;
+}
