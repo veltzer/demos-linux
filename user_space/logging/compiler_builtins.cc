739d4bdb5daa6ebb7c70432c4058df118bd5beb5
Mark Veltzer <mark.veltzer@gmail.com>
2010-12-08 12:52:56 +0800

    added compiler builtins demos

--- user_space/logging/compiler_builtins.cc
+++ user_space/logging/compiler_builtins.cc
@@ -1,20 +1,67 @@
+#include <stdio.h> // for printf(3)
+
 #include "us_helper.hh"
 
 /*
  * This example explores compiler builtins like:
- * __FILE__, __LINE__, __FUNCTION__, __PRETTY_FUNCTION__ 
+ * __FILE__, __LINE__, __func__, __FUNCTION__, __PRETTY_FUNCTION__ 
  * Which can be used in order to produce better error/logging/debug/warning messages.
  *
+ * Notice that if you put the prints, even in an inline function, then you get the data
+ * for the ** inline function ** which is the calee and not for the caller. This is
+ * probably not what you want. A way to solve this is to use a macro (also demonstrated
+ * below).
+ *
+ * Notice that __FILE__ and __LINE__ are different from __func__, __FUNCTION__ and
+ * __PRETTY_FUNCTION__ in that the former are supplied by the pre processor and the latter
+ * are supplied by the compiler. This distinction however is not very important.
+ *
+ * A frequently asked question here is how to indent the printing to match the level
+ * of stack at which the printing occurs. This is not very easy as there is no variable
+ * "stacklevel" or some such which indicated the stack level and can facilitate this.
+ * However, you can use function entry and exit handlers, either explicit or implicit,
+ * to facilitate this.
+ *
  * 					Mark Veltzer
+ *
+ * TODO:
  */
+
+
+inline void print_builtins() {
+	printf("__BASE_FILE__ is %s\n",__BASE_FILE__);
+	printf("__FILE__ is %s\n",__FILE__);
+	printf("__LINE__ is %d\n",__LINE__);
+	printf("__func__ is %s\n",__func__);
+	printf("__FUNCTION__ is %s\n",__FUNCTION__);
+	printf("__PRETTY_FUNCTION__ is %s\n",__PRETTY_FUNCTION__);
+}
+// even this will NOT cause the printing to be of the caller...
+void print_builtins() __attribute__((always_inline));
+void print_builtins() __attribute__((flatten));
+//void print_builtins() __attribute__((artificial));
+
+#define PRINT_BUILTINS() \
+	printf("__BASE_FILE__ is %s\n",__BASE_FILE__);\
+	printf("__FILE__ is %s\n",__FILE__);\
+	printf("__LINE__ is %d\n",__LINE__);\
+	printf("__func__ is %s\n",__func__);\
+	printf("__FUNCTION__ is %s\n",__FUNCTION__);\
+	printf("__PRETTY_FUNCTION__ is %s\n",__PRETTY_FUNCTION__);
 
 class A {
 	public:
 		void thisMethod(int a, int b) {
+			PRINT_BUILTINS();
 		}
 };
 
 int main(int argc, char **argv, char **envp) {
-	printf("__FILE__ is %s\n",__FILE__);
+	printf("Here is a print using the macro:\n");
+	PRINT_BUILTINS();
+	printf("Here is a print using the inline function:\n");
+	print_builtins();
+	A a;
+	a.thisMethod(5,6);
 	return 0;
 }
---
+++ user_space/logging/compiler_builtins_2.cc
@@ -0,0 +1,65 @@
+#include <stdio.h> // for printf(3)
+#include <stdarg.h> // for va_start(3), va_end(3)
+
+#include "us_helper.hh"
+
+/*
+ * This is an expanshion of the previous example showing how to use compiler builtins
+ * and still enjoy the blessings of your own debugging facility.
+ *
+ * Notice:
+ * - the use of pre-processor var args type of macro.
+ * - the use of __attribute__((format)) for compile time type checking of arguments
+ *   to the tracer.
+ * - the use 
+ *
+ * 					Mark Veltzer
+ *
+ * TODO:
+ */
+
+// log levels, ideas copied shamelessly from syslog(3)
+typedef enum _tracelevel {
+	LEVEL_DEBUG,
+	LEVEL_INFO,
+	LEVEL_NOTICE,
+	LEVEL_WARNING,
+	LEVEL_ERR,
+	LEVEL_CRIT,
+	LEVEL_ALERT,
+	LEVEL_EMERG
+} tracelevel;
+
+inline void my_trace(tracelevel level,const char* file,const char* base_file, const int line, const char* func, const char* function, const char* pretty_function,const char* fmt,...) {
+	// the current implemention is quite simplistic...
+	printf("level is %d\n",level);
+	printf("file is %s\n",file);
+	printf("base_file is %s\n",base_file);
+	printf("line is %d\n",line);
+	printf("func is %s\n",func);
+	printf("function %s\n",function);
+	printf("pretty_function is %s\n",pretty_function);
+	// now lets print the actual message...
+	va_list args;
+	va_start(args, fmt);
+	vfprintf(stderr, fmt, args);
+	va_end(args);
+}
+inline void my_trace(tracelevel level,const char* file,const char* base_file,const int line, const char* func, const char* function, const char* pretty_function,const char* fmt,...) __attribute__((format(printf, 8, 9)));
+
+// now wrap this up to get the right compiler macros...
+// this is a pre-processor varargs type of tracer...
+#define MY_TRACE(level,fmt,args...) my_trace(level,__FILE__,__BASE_FILE__,__LINE__,__func__,__FUNCTION__,__PRETTY_FUNCTION__,fmt, ## args)
+
+class A {
+	public:
+		void thisMethod(int a, int b) {
+			MY_TRACE(LEVEL_DEBUG,"did you know that %d+%d=%d?\n",a,b,a+b);
+		}
+};
+
+int main(int argc, char **argv, char **envp) {
+	A a;
+	a.thisMethod(5,6);
+	return 0;
+}
