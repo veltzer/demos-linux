8d8ee2dd138548806460cab01b664a7878a44926
Mark Veltzer <mark.veltzer@gmail.com>
2010-12-10 07:48:57 +0800

    the example now works...

--- user_space/real_time/disable_malloc.cc
+++ user_space/real_time/disable_malloc.cc
@@ -13,43 +13,89 @@
  */
 
 #include <malloc.h> // for malloc(3) and the hooks
+//#define NDEBUG
 #include <assert.h> // for assert(3)
 #include <iostream> // for std::exception
+#include <stdlib.h> // for exit(3)
 
 #include "us_helper.hh"
 
 static bool malloc_allowed=true;
 
-static void* my_malloc_hook(size_t size, const void *caller) {
-	if(malloc_allowed) {
-		return malloc(size);
-	} else {
-		throw std::exception();
-	}
-}
+// Variables to save original hooks
+void* (*old_malloc_hook)(size_t, const void *);
+void* (*old_realloc_hook)(void*, size_t, const void *);
+void* (*old_memalign_hook)(size_t , size_t ,const void *);
+void (*old_free_hook)(void*, const void *);
 
-static void* my_realloc_hook(void *ptr, size_t size, const void *caller) {
-	throw std::exception();
-}
+// prototypes for my hooks
+void* my_malloc_hook(size_t size, const void *caller);
+void* my_realloc_hook(void *ptr, size_t size, const void *caller);
+void* my_memalign_hook(size_t alignment, size_t size,const void *caller);
+void my_free_hook(void *ptr, const void *caller);
 
-static void* my_memalign_hook(size_t alignment, size_t size,const void *caller) {
-	throw std::exception();
-}
-
-static void my_free_hook(void *ptr, const void *caller) {
-	throw std::exception();
-}
-
-/* Variables to save original hooks. */
-static void *(*old_malloc_hook)(size_t, const void *);
-
-static void my_init_hook(void) {
-	TRACE("start");
+static void save_hooks(void) {
 	old_malloc_hook=__malloc_hook;
+	old_realloc_hook=__realloc_hook;
+	old_memalign_hook=__memalign_hook;
+	old_free_hook=__free_hook;
+}
+
+static void setup_hooks(void) {
 	__malloc_hook=my_malloc_hook;
 	__realloc_hook=my_realloc_hook;
 	__memalign_hook=my_memalign_hook;
 	__free_hook=my_free_hook;
+}
+
+static void restore_hooks(void) {
+	__malloc_hook=old_malloc_hook;
+	__realloc_hook=old_realloc_hook;
+	__memalign_hook=old_memalign_hook;
+	__free_hook=old_free_hook;
+}
+
+void* my_malloc_hook(size_t size, const void *caller) {
+	if(malloc_allowed) {
+		restore_hooks();
+		void* result=malloc(size);
+		save_hooks();
+		setup_hooks();
+		return result;
+	} else {
+		// c alternative (termination)
+		//restore_hooks();
+		//TRACE("ERROR! Malloc is not allowed");
+		//exit(1);
+
+		// cpp alternative (exception throwing)
+		//restore_hooks();
+		//TRACE("ERROR! Malloc is not allowed");
+		//throw new std::exception();
+
+		// c alternative (returning NULL)
+		restore_hooks();
+		return NULL;
+	}
+}
+
+void* my_realloc_hook(void *ptr, size_t size, const void *caller) {
+	throw std::exception();
+}
+
+void* my_memalign_hook(size_t alignment, size_t size,const void *caller) {
+	throw std::exception();
+}
+
+void my_free_hook(void *ptr, const void *caller) {
+	throw std::exception();
+}
+
+static void my_init_hook(void) {
+	TRACE("start");
+	save_hooks();
+	setup_hooks();
+	TRACE("end");
 }
 
 /* Override initializing hook from the C library. */
@@ -71,5 +117,7 @@ int main(int argc,char** argv,char** envp) {
 	disable_malloc();
 	// this should fail...
 	p=malloc(200);
+	// we should never get to this line
+	assert(p!=NULL);
 	return 0;
 }
