16d5865d4e577e846bd493eb2cca67d4042bab4d
Mark Veltzer <mark.veltzer@gmail.com>
2010-12-21 06:07:07 +0200

    more changes

---
+++ user_space/error_types/write_over_data.cc
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/*
+ *
+ * 			Mark Veltzer
+ */
+
+void do_something_with_an_integer(int val) {
+	printf("val is %d\n",val);
+}
+
+int* do_something_with_intp(int* val) {
+	return val+7-7;
+}
+int t=1945;
+const volatile int a=6;
+
+int main(int argc,char** argv,char** envp) {
+	int* p=&t;
+	p++;
+	*p=7;
+	printf("a is %d\n",a);
+	printf("&a is %p\n",&a);
+	printf("&t is %p\n",&t);
+	printf("a is %d\n",a);
+	/*
+	// the next line will produce a compile time error...
+	//a=5;
+	int* p=(int*)&a;
+	*p=7;
+	__sync_synchronize();
+	printf("*p is %d\n",*p);
+	printf("*&a is %d\n",*&a);
+	int* foo=(int*)&a;
+	foo=do_something_with_intp(foo);
+	printf("*foo is %d\n",*foo);
+	do_something_with_an_integer(a);
+	*/
+	return(0);
+}
