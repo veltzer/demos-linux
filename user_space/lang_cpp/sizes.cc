d546dd0996004466337149ea57ef102facff5103
Mark Veltzer <mark.veltzer@gmail.com>
2011-01-11 11:18:14 +0200

    some script flex up

---
+++ user_space/lang_cpp/sizes.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+
+/*
+ * This example investigates the sizeof() of C++ object.
+ *
+ *                                              Mark Veltzer
+ * EXTRA_LIBS=
+ */
+
+class A {
+	public:
+		int a;
+		char b;
+		int c;
+		char d;
+
+		void dosomething() {
+			for(int i=0;i<4;i++) {
+				std::cout << "Hello from A, i is " << i << std::endl;
+			}
+		}
+};
+
+class B: public A {
+	public:
+		int e;
+		char f;
+		void dosomething() {
+			for(int i=0;i<4;i++) {
+				std::cout << "Hello from B, i is " << i << std::endl;
+			}
+		}
+};
+#define CppOffsetOf(className, FieldName) ((char *)(&(((className *)1)->FieldName)) - (char *)1)
+
+int main(int argc, char **argv, char **envp) {
+	std::cout << "sizeof(A) is " << sizeof(A) << std::endl;
+	std::cout << "sizeof(B) is " << sizeof(B) << std::endl;
+	//std::cout << "__builtin_offsetof(e,B) " << __builtin_offsetof(e,B) << std::endl;
+	std::cout << "CppOffsetOf(B,e) " << CppOffsetOf(B,e) << std::endl;
+	A* a=new A();
+	a->dosomething();
+	A* ab=new B();
+	ab->dosomething();
+	B* b=new B();
+	b->dosomething();
+	A anotherA;
+	anotherA.dosomething();
+	return(0);
+}
