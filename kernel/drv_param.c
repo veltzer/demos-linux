6c7187462b52e42b124c695ace139727e5279e59
Mark Veltzer <mark.veltzer@gmail.com>
2011-02-12 23:06:35 +0200

    finished the param stuff.

--- kernel/drv_param.c
+++ kernel/drv_param.c
@@ -38,6 +38,18 @@ MODULE_DESCRIPTION("demonstrate the use of kernel module parameters");
  * modinfo [module_name] will print them.
  * - load the module (insmod or modprobe it) and look at:
  * /sys/modules/[module_name]/parameters/
+ *
+ * Note:
+ * You should consider carefully whether or not your module can actually
+ * handle changes to a parameters value at runtime. This could be extremely
+ * delicate. The parameter system DOES NOT provide you with a callback
+ * whenever a parameter changes. If you want that you can use the /proc
+ * filesystem where you get a callback both on read and on write.
+ * In any case you will probably have to use some kind of locking to protect
+ * pieces of your code that implicitly assume that the value of the parameter
+ * does not change while they are running. The starting point for doing this is
+ * to atomically copy the parameters value before the piece of code and never
+ * look at it again and only use the copy.
  */
 
 /*
@@ -49,6 +61,8 @@ MODULE_DESCRIPTION("demonstrate the use of kernel module parameters");
  * We could do some "auto detect" or "smart" code in the initialization of
  * the module but that too should ultimately be controlled by these external
  * patameters.
+ * These should all have default values otherwise you should assume that they
+ * are junk.
  */
 static short int myshort = 1;
 static int myint = 420;
@@ -62,13 +76,23 @@ static char *mystring = "blah";
  * The second param is it's data type
  * The final argument is the permissions bits,
  * for exposing parameters in sysfs (if non-zero) at a later stage.
+ *
+ * If you want to see all the options see $KERNEL_HEADERS/linux/stat.h
  */
-module_param(myshort, short, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+// this is the most permissive mode available.
+// root can read write, group root can read write and others can read.
+// for security reasons others writing is prohibited. This means that you
+// will need root user or to be a member of group root to change kernel
+// modules parameters at runtime...
+module_param(myshort, short, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 MODULE_PARM_DESC(myshort, "A short integer");
+// only root and group root will be able to both read this and write this at runtime
 module_param(myint, int, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 MODULE_PARM_DESC(myint, "An integer");
+// only root will be able to read this one
 module_param(mylong, long, S_IRUSR);
 MODULE_PARM_DESC(mylong, "A long integer");
+// this will not appear at all since no one has permission to do anything with it...
 module_param(mystring, charp, 0000);
 MODULE_PARM_DESC(mystring, "A character string");
 
