1bb80f2a144c3aa71e04e85eec5529d2b71b38e5
Mark Veltzer <mark.veltzer@gmail.com>
2011-03-04 13:30:05 +0200

    pipe fixes

--- kernel_standalone/pipe/main.c
+++ kernel_standalone/pipe/main.c
@@ -9,11 +9,14 @@
 #include <linux/spinlock.h> // for spinlock_t and ops on it
 #include <linux/wait.h> // for wait_queue_head_t and ops on it
 
+#define DO_DEBUG
 #include "kernel_helper.h" // our own helper
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Mark Veltzer");
 MODULE_DESCRIPTION("A named pipe exercise");
+
+//#define DO_COPY
 
 /*
  * See the README.txt for the explanation of this exercise.
@@ -40,9 +43,10 @@ struct my_pipe {
 	struct device* pipe_device;
 };
 
-static struct my_pipe* pipes;
+static struct my_pipe* pipes=NULL;
 
-static inline void init_pipe(struct my_pipe* pipe) {
+// pipe constructur
+static inline void pipe_ctor(struct my_pipe* pipe) {
 	pipe->data=kzalloc(pipe_size,GFP_KERNEL);
 	pipe->size=pipe_size;
 	pipe->read_pos=0;
@@ -53,42 +57,105 @@ static inline void init_pipe(struct my_pipe* pipe) {
 	init_waitqueue_head(&pipe->write_queue);
 }
 
-static inline void del_pipe(struct my_pipe* pipe) {
+// pipe destructor
+static inline void pipe_dtor(struct my_pipe* pipe) {
 	kfree(pipe->data);
 }
 
-static inline bool have_data(struct my_pipe* pipe) {
+// do we have data in the pipe ?
+static inline bool pipe_have_data(const struct my_pipe* pipe) {
 	return pipe->read_pos!=pipe->write_pos;
 }
 
 // return the empty room of a pipe
-static inline int pipe_room(struct my_pipe* pipe) {
+static inline int pipe_room(const struct my_pipe* pipe) {
 	if(pipe->read_pos<pipe->write_pos) {
-		return pipe->size-pipe->write_pos+pipe->read_pos;
+		return pipe->size-pipe->write_pos+pipe->read_pos-1;
 	} else {
-		return pipe->read_pos-pipe->write_pos;
+		if(pipe->read_pos==pipe->write_pos) {
+			return pipe->size-1;
+		} else {
+			return pipe->read_pos-pipe->write_pos-1;
+		}
 	}
 }
 // return the occupied room of a pipe
-static inline int pipe_data(struct my_pipe* pipe) {
+static inline int pipe_data(const struct my_pipe* pipe) {
 	if(pipe->read_pos<pipe->write_pos) {
 		return pipe->write_pos-pipe->read_pos;
 	} else {
-		return pipe->size-pipe->read_pos+pipe->write_pos;
+		if(pipe->read_pos==pipe->write_pos) {
+			return 0;
+		} else {
+			return pipe->size-pipe->read_pos+pipe->write_pos-1;
+		}
 	}
 }
 
+// lock the pipe spinlock
 static inline void pipe_lock(struct my_pipe* pipe) {
 	spin_lock(&pipe->lock);
 }
+
+// unlock the pipe spinlock
 static inline void pipe_unlock(struct my_pipe* pipe) {
 	spin_unlock(&pipe->lock);
 }
+
+// wait on the pipes readers queue
 static inline int pipe_wait_read(struct my_pipe* pipe) {
 	return wait_event_interruptible(pipe->read_queue,1);
 }
+
+// wait on the pipes writers queue
 static inline int pipe_wait_write(struct my_pipe* pipe) {
 	return wait_event_interruptible(pipe->write_queue,1);
+}
+
+// wake up the readers
+static inline void pipe_wake_readers(struct my_pipe* pipe) {
+	wake_up_all(&pipe->read_queue);
+}
+
+// wake up the writers
+static inline void pipe_wake_writers(struct my_pipe* pipe) {
+	wake_up_all(&pipe->write_queue);
+}
+
+// read into the pipe
+static inline int pipe_copy_from_user(struct my_pipe* pipe,int size,const char** __user ubuf) {
+	#ifdef DO_COPY
+	int ret=copy_from_user(pipe->data+pipe->read_pos,*ubuf,size);
+	#else // DO_COPY
+	int ret=0;
+	INFO("size is %d, read_pos is %d, write_pos is %d",size,pipe->read_pos,pipe->write_pos);
+	#endif // DO_COPY
+	if(ret==0) {
+		pipe->read_pos+=size;
+		*ubuf+=size;
+		if(pipe->read_pos==pipe->size) {
+			pipe->read_pos=0;
+		}
+	}
+	return ret;
+}
+
+// read from the pipe
+static inline int pipe_copy_to_user(struct my_pipe* pipe,int size,char** __user ubuf) {
+	#ifdef DO_COPY
+	int ret=copy_to_user(ubuf,pipe->data+pipe->write_pos,size);
+	#else // DO_COPY
+	int ret=0;
+	INFO("size is %d, read_pos is %d, write_pos is %d",size,pipe->read_pos,pipe->write_pos);
+	#endif // DO_COPY
+	if(ret==0) {
+		pipe->write_pos+=size;
+		*ubuf+=size;
+		if(pipe->write_pos==pipe->size) {
+			pipe->write_pos=0;
+		}
+	}
+	return ret;
 }
 
 // these are the actual operations
@@ -101,9 +168,10 @@ static int open_pipe(struct inode * inode, struct file * file) {
 
 static ssize_t read_pipe(struct file * file, char __user * buf, size_t count, loff_t *ppos) {
 	struct my_pipe* pipe;
-	int data,size_to_read,first_chunk,second_chunk;
-	INFO("start");
-	pipe=pipes+(int)file->private_data;
+	int data,work_size,first_chunk,second_chunk,minor,ret;
+	DEBUG("start");
+	minor=(int)(file->private_data);
+	pipe=pipes+minor;
 	// lets sleep while there is no data in the pipe 
 	pipe_lock(pipe);
 	data=pipe_data(pipe);
@@ -115,34 +183,43 @@ static ssize_t read_pipe(struct file * file, char __user * buf, size_t count, lo
 		pipe_lock(pipe);
 		data=pipe_data(pipe);
 	}
+	DEBUG("data is %d",data);
 	// now data > 0
-	size_to_read=smin(data,count);
+	work_size=smin(data,count);
+	DEBUG("work_size is %d",work_size);
 	// copy_to_user data from the pipe
 	if(pipe->read_pos<pipe->write_pos) {
-		copy_to_user(buf,pipe->data+pipe->read_pos,size_to_read);
-		pipe->read_pos+=size_to_read;
+		if((ret=pipe_copy_to_user(pipe,work_size,&buf))) {
+			pipe_unlock(pipe);
+			return ret;
+		}
 	} else {
-		first_chunk=smin(size_to_read,pipe->size-pipe->read_pos);
-		copy_to_user(buf,pipe->data+pipe->read_pos,first_chunk);
-		pipe->read_pos+=first_chunk;
-		if(first_chunk<size_to_read) {
-			second_chunk=size_to_read-first_chunk;
-			copy_to_user(buf+first_chunk,pipe->data,second_chunk);
-			pipe->read_pos=second_chunk;
+		first_chunk=smin(work_size,pipe->size-pipe->read_pos);
+		if((ret=pipe_copy_to_user(pipe,first_chunk,&buf))) {
+			pipe_unlock(pipe);
+			return ret;
+		}
+		if(first_chunk<work_size) {
+			second_chunk=work_size-first_chunk;
+			if((ret=pipe_copy_to_user(pipe,second_chunk,&buf))) {
+				pipe_unlock(pipe);
+				return ret;
+			}
 		}
 	}
 	pipe_unlock(pipe);
-	*ppos+=size_to_read;
+	*ppos+=work_size;
 	// wake up the writers
-	wake_up_all(&pipe->write_queue);
-	return size_to_read;
+	pipe_wake_writers(pipe);
+	return work_size;
 }
 
 static ssize_t write_pipe(struct file * file, const char __user * buf, size_t count, loff_t *ppos) {
 	struct my_pipe* pipe;
-	int room, size_to_write,first_chunk,second_chunk,ret;
-	INFO("start");
-	pipe=pipes+(int)file->private_data;
+	int room,work_size,first_chunk,second_chunk,minor,ret;
+	DEBUG("start");
+	minor=(int)(file->private_data);
+	pipe=pipes+minor;
 	pipe_lock(pipe);
 	// lets check if we have room in the pipe
 	room=pipe_room(pipe);
@@ -154,27 +231,35 @@ static ssize_t write_pipe(struct file * file, const char __user * buf, size_t co
 		pipe_lock(pipe);
 		room=pipe_room(pipe);
 	}
+	DEBUG("room is %d",room);
 	// now room > 0
-	size_to_write=smin(room,count);
+	work_size=smin(room,count);
+	DEBUG("work_size is %d",work_size);
 	// copy_from_user data from the pipe
 	if(pipe->read_pos<pipe->write_pos) {
-		first_chunk=smin(size_to_write,pipe->size-pipe->write_pos);
-		copy_from_user(pipe->data+pipe->write_pos,buf,first_chunk);
-		pipe->write_pos+=first_chunk;
-		if(first_chunk<size_to_write) {
-			second_chunk=size_to_write-first_chunk;
-			copy_from_user(pipe->data,buf,second_chunk);
-			pipe->write_pos=second_chunk;
+		first_chunk=smin(work_size,pipe->size-pipe->write_pos);
+		if((ret=pipe_copy_from_user(pipe,first_chunk,&buf))) {
+			pipe_unlock(pipe);
+			return ret;
+		}
+		if(first_chunk<work_size) {
+			second_chunk=work_size-first_chunk;
+			if((ret=pipe_copy_from_user(pipe,second_chunk,&buf))) {
+				pipe_unlock(pipe);
+				return ret;
+			}
 		}
 	} else {
-		copy_from_user(pipe->data+pipe->write_pos,buf,size_to_write);
-		pipe->write_pos+=size_to_write;
+		if((ret=pipe_copy_from_user(pipe,work_size,&buf))) {
+			pipe_unlock(pipe);
+			return ret;
+		}
 	}
 	pipe_unlock(pipe);
-	*ppos+=size_to_write;
+	*ppos+=work_size;
 	// wake up the readers
-	wake_up_all(&pipe->read_queue);
-	return size_to_write;
+	pipe_wake_readers(pipe);
+	return work_size;
 }
 
 // this is the operations table
@@ -193,28 +278,29 @@ static dev_t first_dev;
 // this is our first minor
 static int first_minor=0;
 
-static int pipe_init(void) {
+static int __init pipe_init(void) {
 	int err=0;
 	int i;
+	INFO("started loading...");
 	// allocate all pipes
 	pipes=(struct my_pipe*)kmalloc(sizeof(struct my_pipe)*pipes_count,GFP_KERNEL);
 	// initialize all pipes
 	for(i=0;i<pipes_count;i++) {
-		init_pipe(pipes+i);
+		pipe_ctor(pipes+i);
 	}
 	// allocate our own range of devices
 	if((err=alloc_chrdev_region(&first_dev, first_minor, pipes_count, THIS_MODULE->name))) {
 		ERROR("cannot alloc_chrdev_region");
 		goto err_final;
 	}
-	INFO("allocated the region");
+	DEBUG("allocated the region");
 	// add the cdev structure
 	cdev_init(&cdev, &pipe_fops);
 	if((err=cdev_add(&cdev, first_dev, pipes_count))) {
 		ERROR("cannot cdev_add");
 		goto err_dealloc;
 	}
-	INFO("added the cdev");
+	DEBUG("added the cdev");
 	// this is creating a new class (/proc/devices)
 	my_class=class_create(THIS_MODULE,THIS_MODULE->name);
 	if(IS_ERR(my_class)) {
@@ -222,7 +308,7 @@ static int pipe_init(void) {
 		err=PTR_ERR(my_class);
 		goto err_cdev_del;
 	}
-	INFO("created the class");
+	DEBUG("created the class");
 	for(i=0;i<pipes_count;i++) {
 	// and now lets auto-create a /dev/ node
 		pipes[i].pipe_device=device_create(my_class, NULL, MKDEV(MAJOR(first_dev),first_minor+i),NULL,"%s%d",THIS_MODULE->name,i);
@@ -232,7 +318,7 @@ static int pipe_init(void) {
 			goto err_class;
 		}
 	}
-	INFO("created the device");
+	DEBUG("created the device");
 	INFO("loaded ok");
 	return 0;
 //err_device:
@@ -244,11 +330,16 @@ err_cdev_del:
 err_dealloc:
 	unregister_chrdev_region(first_dev, pipes_count);
 err_final:
+	for(i=0;i<pipes_count;i++) {
+		pipe_dtor(pipes+i);
+	}
+	kfree(pipes);
 	return err;
 }
 
-static void pipe_exit(void) {
+static void __exit pipe_exit(void) {
 	int i;
+	INFO("started unloading...");
 	for(i=0;i<pipes_count;i++) {
 		device_destroy(my_class,MKDEV(MAJOR(first_dev),i));
 	}
@@ -256,8 +347,9 @@ static void pipe_exit(void) {
 	cdev_del(&cdev);
 	unregister_chrdev_region(first_dev, pipes_count);
 	for(i=0;i<pipes_count;i++) {
-		del_pipe(pipes+i);
+		pipe_dtor(pipes+i);
 	}
+	kfree(pipes);
 	INFO("unloaded ok");
 }
 
