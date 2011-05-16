8616911585e23e9c5c8f6f2e013d2f43d7dd3ce1
Mark Veltzer <mark.veltzer@gmail.com>
2011-05-16 23:53:03 +0300

    fixed templates examples

--- user_space_standalone/templates_storage_2/use_a.cc
+++ user_space_standalone/templates_storage_2/use_a.cc
@@ -2,8 +2,8 @@
 #include "A.h"
 
 int main(int argc, char **argv, char **envp) {
-	//A<int> a;
-	//a.setT(7);
+	A<int> a;
+	a.setT(7);
 	//printf("a.t is %d\n",a.t);
 	printf("Hellol, World!\n");
 	return(0);
