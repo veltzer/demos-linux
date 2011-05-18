0eda1b5088e6074eba4687c7a3e2462f51b7add3
Mark Veltzer <mark.veltzer@gmail.com>
2011-05-18 04:02:01 +0300

    fixed compilation

--- user_space/ace/examples/Chapter-3/Thread1.cc
+++ user_space/ace/examples/Chapter-3/Thread1.cc
@@ -1,46 +1,66 @@
-#include <ace/Synch.h>
+#include <ace/Synch.h> // for the mutex
+#include <ace/Thread.h> // for thread functions
 
 /*
  * EXTRA_CMDS=pkg-config --cflags --libs ACE
  */
 
-//Arguments that are to be passed to the worker thread are passed through this class.
+//Arguments that are to be passed to the worker thread are instances of this class.
 class Args {
-public:
-	Args(int iterations) :
-		mutex_(), iterations_(iterations) {
-	}
-	ACE_Thread_Mutex mutex_;
-	int iterations_;
+	public:
+		Args(int iterations):mutex(),iterations(iterations) {
+		}
+		ACE_Thread_Mutex mutex;
+		int iterations;
 };
 
 //The starting point for the worker threads
-static void *
-worker(void *arguments) {
+static void * worker(void *arguments) {
 	Args *arg = (Args *)arguments;
 
-	for (int i = 0; i < arg->iterations_; i++) {
+	for (int i = 0; i < arg->iterations; i++) {
 		ACE_DEBUG((LM_DEBUG,"(%t) Trying to get a hold of this iteration\n"));
-		//This is our critical section
-		arg->mutex_.acquire();
+		// lock the mutex
+		arg->mutex.acquire();
 		ACE_DEBUG((LM_DEBUG, "(%t) This is iteration number %d\n", i));
-		//work
+		// simulate work
 		ACE_OS::sleep(2);
-		arg->mutex_.release();
+		// release the mutex
+		arg->mutex.release();
 	}
 	return(0);
 }
 
 int main(int argc, char *argv[]) {
 	if (argc < 2) {
-		ACE_OS::printf("Usage: egx <number_of_threads> <number_of_iterations>\n");
+		ACE_OS::printf("Usage: %s <number_of_threads> <number_of_iterations>\n",argv[0]);
 		ACE_OS::exit(1);
 	}
 	//Setup the arguments
 	Args arg(ACE_OS::atoi(argv[2]));
+	unsigned int thread_num=ACE_OS::atoi(argv[1]);
 
-	ACE_Thread::spawn_n(ACE_OS::atoi(argv[1]), (ACE_THR_FUNC)worker, (void *)&arg);
+	// create the data structures needed
+	ACE_thread_t* threads=new ACE_thread_t[thread_num];
+	ACE_hthread_t *threadHandles = new ACE_hthread_t[thread_num];
+
 	//Spawn the worker threads
-	while (ACE_Thread::join(NULL, NULL, NULL) == 0) {
+	if (ACE_Thread::spawn_n(
+		threads,//id's for each of the threads
+		thread_num,//number of threads to spawn
+		worker,//entry point for new thread
+		(void*)&arg,//args to worker
+		THR_JOINABLE | THR_NEW_LWP,//flags
+		ACE_DEFAULT_THREAD_PRIORITY,
+		NULL,
+		0,
+		threadHandles) != thread_num) {
+			ACE_DEBUG((LM_DEBUG, "Error in spawning thread\n"));
 	}
+	//Wait for all the threads to exit before you let the main fall through
+	//and have the process exit.
+	for (unsigned int i = 0; i < thread_num; i++) {
+		ACE_Thread::join(threads[i]);
+	}
+	return 0;
 }
