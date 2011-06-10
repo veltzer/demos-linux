58f273358f1bf1147b7d697bd14f2aa7242283d7
Mark Veltzer <mark.veltzer@gmail.com>
2011-06-10 10:04:13 +0300

    more stuff

--- user_space/compiler_internals/varargs.cc
+++ user_space/compiler_internals/varargs.cc
@@ -4,16 +4,19 @@
  *      This example shows how to use the __builtin_ varargs
  *      of gcc...
  *
- *      Things to notice:
- *      1. We don't need any special header files (we are using compiler built ins
+ *      Notes:
+ *      - this is not portable because other compilers do not have these
+ *      '__builtin_*' macros. If you want something portable use the 'stdarg'
+ *      header and it's macros which are provided by the libc library.
+ *      - We don't need any special header files (we are using compiler built ins
  *      	which are something akin to "sizeof").
- *      2. We use the __attribute__ feature to make sure the compiler checks that
+ *      - We use the __attribute__ feature to make sure the compiler checks that
  *      we are passing the arguments right (gcc feature).
- *      3. Notice we can only put the __attribute__ feature on function declaration
+ *      - Notice we can only put the __attribute__ feature on function declaration
  *      (that is why we have the funny declaration after the definition).
- *
- * TODO:
- * - add an example using the glibc wrappers to show the difference.
+ *      - these are all *compiler builtins*. They are not taken from any header
+ *      files. Compare this to using the glibc wrappers in a similar example
+ *      under glibc.
  *
  *              Mark Veltzer
  *
@@ -24,13 +27,12 @@ int trace(const char *fmt, ...) {
 
 	printf("%s: ", program_invocation_short_name);
 	__builtin_va_list args;
-	int ret;
 	__builtin_va_start(args, fmt);
+	int ret;
 	ret = vprintf(fmt, args);
 	__builtin_va_end(args);
 	return(ret);
 }
-
 
 int trace(const char *fmt, ...) __attribute__((format(printf, 1, 2)));
 
---
+++ user_space/lib_c/varargs.cc
@@ -0,0 +1,40 @@
+#include <stdio.h> // for vprintf(3)
+#include <stdarg.h> // for va_start(3), va_arg(3), va_end(3)
+
+/*
+ *      This example shows how to use the glibc varargs support.
+ *
+ *      Notes:
+ *      - we use the program_invocation_short_name which is an external name of
+ *      our own program. much like argv[0] only we don't need to pass argv until
+ *      the point we wish to use it.
+ *      - unlike the corresponding builtin va_* compiler internals we can use this
+ *      code on any system conforming to the 'stdarg' standard which means this
+ *      is quite portable.
+ *      - We use the __attribute__ feature to make sure the compiler checks that
+ *      we are passing the arguments right (gcc feature).
+ *
+ *              Mark Veltzer
+ *
+ * EXTRA_LIBS=
+ */
+int trace(const char *fmt, ...) {
+	extern char *program_invocation_short_name;
+
+	printf("%s: ", program_invocation_short_name);
+	va_list ap;
+	va_start(ap,fmt);
+	int ret;
+	ret = vprintf(fmt, ap);
+	va_end(ap);
+	return(ret);
+}
+
+int trace(const char *fmt, ...) __attribute__((format(printf, 1, 2)));
+
+int main(int argc, char **argv, char **envp) {
+	trace("%s %d %f\n", "Hello", 5, 3.14);
+	// the next line will produce a compile time error (passing int as string...)
+	//trace("%s %s %f\n","Hello",5,3.14);
+	return(0);
+}
