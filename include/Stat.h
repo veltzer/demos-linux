cd09f6a8f07dce79e33917ceab222a326b2bed31
Mark Veltzer <mark.veltzer@gmail.com>
2011-07-12 04:46:23 +0300

    better demo for rep

---
+++ include/Stat.h
@@ -0,0 +1,115 @@
+#ifndef __Stat_h
+#define __Stat_h
+
+#include<stdio.h>
+#include<math.h>
+#include<values.h>
+
+/*
+	Statistics collecting object
+*/
+
+class Stat {
+	private:
+		// these are supplied by the user
+		unsigned int binnum;
+		double binsize;
+		double mean;
+
+		// these are managed by us
+		unsigned int* bins;
+		unsigned int bin_max;
+		unsigned int bin_min;
+		double minbin;
+		double maxbin;
+		double max;
+		double min;
+		double maxabs;
+		double minabs;
+		double ex;
+		double ex2;
+		unsigned int counter;
+
+	public:
+		Stat(unsigned int ibinnum,double ibinsize,double imean) {
+			binnum=ibinnum;
+			binsize=ibinsize;
+			mean=imean;
+			bins=new unsigned int[binnum]; 
+			for(unsigned int i=0;i<binnum;i++) {
+				bins[i]=0;
+			}
+			minbin=imean-ibinsize*double(binnum)/2.0;
+			maxbin=imean+ibinsize*double(binnum)/2.0;
+			bin_max=0;
+			bin_min=0;
+			max=MINDOUBLE;
+			min=MAXDOUBLE;
+			maxabs=MINDOUBLE;
+			minabs=MAXDOUBLE;
+			ex=0;
+			ex2=0;
+			counter=0;
+		}
+		~Stat(void) {
+			delete bins;
+		}
+
+		void accept(double val) {
+			// calculate the bin and add one to it
+			if(val<minbin) {
+				bin_min++;
+			} else {
+				if(val>maxbin) {
+					bin_max++;
+				} else {
+					double distance=val-minbin;
+					unsigned int bin=(unsigned int)(floor(distance/binsize));
+					bins[bin]++;
+				}
+			}
+			// update min,max,minabs,maxabs
+			if(val>max) {
+				max=val;
+			}
+			if(val<min) {
+				min=val;
+			}
+			double valabs=fabs(val);
+			if(valabs>maxabs) {
+				maxabs=valabs;
+			}
+			if(valabs<minabs) {
+				minabs=valabs;
+			}
+			// handle stats
+			ex+=val;
+			ex2+=val*val;
+			counter++;
+		}
+		void print(void) {
+			printf("counter is %u\n",counter);
+			// print the bins
+			printf("bin_min is %u\n",bin_min);
+			double runner=minbin;
+			for(unsigned int i=0;i<binnum;i++) {
+				printf("bin[%u] (%lf,%lf) => %u\n",i,runner,runner+binsize,bins[i]);
+				runner+=binsize;
+			}
+			printf("bin_max is %u\n",bin_max);
+			printf("min is %lf\n",min);
+			printf("max is %lf\n",max);
+			printf("minabs is %lf\n",minabs);
+			printf("maxabs is %lf\n",maxabs);
+			ex/=counter;
+			ex2/=counter;
+			ex2-=ex;
+			printf("ex is %lf\n",ex);
+			printf("ex2 is %lf\n",ex2);
+		}
+		double getMaxAbs(void) {
+			return maxabs;
+		}
+};
+
+#endif // __Stat_h
--- user_space/floating_point/rep.cc
+++ user_space/floating_point/rep.cc
@@ -1,5 +1,8 @@
 #include<stdio.h> // for printf(3), snprintf(3)
 #include<stdlib.h> // for atof(3), atoi(3)
+#include<values.h> // for MAXINT, MININT, MAXFLOAT, MINFLOAT
+
+#include<Stat.h> // our own statistics gathering object
 
 // this example explores the representational issues of the floating point
 // system in the mathematical co-processor.
@@ -10,15 +13,86 @@
 // using the mantisa. After this number it goes into using the exponent and therefor
 // becomes un-exact.
 //
-// 				Mark Veltzer
+// Conclusions:
+// atof behaves differently from float=int.
+//
+// 	Mark Veltzer
+
+#define DO_FLOAT
+//#define DO_DOUBLE
+//#define DO_ATOF
 
 int main(int argc,char** argv,char** envp) {
-	float f=atof(argv[1]);
-	//int precision=atoi(argv[2]);
-	//const int fmtmax=128;
-	//char fmt[fmtmax];
-	//snprintf(fmt,fmtmax,"%.%dlf\n",precision);
-	//printf((const char*)fmt,f);
-	printf("%.70f\n",f);
+	// first lets print the limits
+	printf("MAXINT is %d\n",MAXINT);
+	printf("MININT is %d\n",MININT);
+	printf("MAXFLOAT is %f\n",MAXFLOAT);
+	printf("MINFLOAT is %f\n",MINFLOAT);
+	printf("MAXDOUBLE is %lf\n",MAXDOUBLE);
+	printf("MINDOUBLE is %lf\n",MINDOUBLE);
+	unsigned int num_bins=100;
+	unsigned long range=(unsigned long)MAXINT-(unsigned long)MININT;
+	double dbin_size=(double)range/(double)num_bins;
+	unsigned long bin_size=dbin_size;
+	printf("bin_size is %lu\n",bin_size);
+	unsigned int bin_counter=0;
+	// collectors - one for float and one for double
+#ifdef DO_FLOAT
+	Stat s_f(num_bins,dbin_size,0);
+#endif // DO_FLOAT
+#ifdef DO_DOUBLE
+	Stat s_d(num_bins,dbin_size,0);
+#endif // DO_DOUBLE
+#ifdef DO_ATOF
+	Stat s_atof(num_bins,dbin_size,0);
+#endif // DO_ATOF
+	for(int i=MININT;i<MAXINT;i++) {
+		#ifdef DO_FLOAT
+		// direct conversion of int to float
+		// the 'volatile' is necessary in order that the compiler
+		// will not optimize the next two assignments and will do away
+		// with the intermediate float...
+		volatile float f=i;
+		int back_i_f=f;
+		if(i!=back_i_f) {
+			s_f.accept(i);
+		}
+		#endif // DO_FLOAT
+		#ifdef DO_DOUBLE
+		// same thing for double although double covers the range of int
+		// pretty well...
+		volatile double d=i;
+		int back_i_d=d;
+		if(i!=back_i_d) {
+			s_d.accept(i);
+		}
+		#endif // DO_DOUBLE
+		#ifdef DO_ATOF
+		// now print i into a string buffer, atof it back to float and convert
+		char string_of_i[128];
+		snprintf(string_of_i,128,"%d",i);
+		float f2=atof(string_of_i);
+		int back_i_atof=f2;
+		if(i!=back_i_atof) {
+			s_atof.accept(i);
+		}
+		#endif // DO_ATOF
+		// progress report...
+		bin_counter++;
+		if(bin_counter==bin_size) {
+			printf("new bin %d\n",i);
+			// reset counters
+			bin_counter=0;
+		}
+	}
+	#ifdef DO_FLOAT
+	s_f.print();
+	#endif // DO_FLOAT
+	#ifdef DO_DOUBLE
+	s_d.print();
+	#endif // DO_DOUBLE
+	#ifdef DO_ATOF
+	s_atof.print();
+	#endif // DO_ATOF
 	return 0;
 }
