0651bf85605a36c3342a2475f9390aad4104847f
Mark Veltzer <mark.veltzer@gmail.com>
2011-07-28 20:19:50 +0300

    Revert "flexed up the exercise..."
    This reverts commit 9743635a8bb1e4bcd3bcaa5dcc7e83e8b92cddd6.

--- src/user_space/ace/exercises/Chapter-3/solution.cc
+++ src/user_space/ace/exercises/Chapter-3/solution.cc
@@ -1,131 +1,110 @@
-#define ACE_NTRACE 1
-#include <ace/Task.h> // for ACE_Task_Base
-#include <sched.h> // for sched_yield(2)
-#include <assert.h> // for assert(3)
+#define ACE_TRACE 0
 
-/*
- * This is a solution to the exercise.
- *
- * Try to run this with "taskset 1" and more to see the difference in performance.
- */
+#include <ace/Task.h>
 
 /*
  * EXTRA_CMDS=pkg-config --cflags --libs ACE
  */
-const int numAttempts=10000;
-// should the threads yield one for the other ?
-const bool yield=true;
-// should we write debug messages ?
-const bool debug=false;
 
 class SharedResource {
-	private:
-		// This is the ACE mutex that wraps the OS mutex which we will
-		// use to prevent simultaneous access to the resource.
-		ACE_Thread_Mutex mutex;
-		// This is the actual counter. We will initialize it in the constructor.
-		int LockedCounter;
-		// This is the attempt counter. We will initialize it in the constructor.
-		int AttemptCounter;
-		// Number of threads (to do the modulu...)
-		int numThreads;
-	public:
-		SharedResource(int inumThreads) {
-			LockedCounter = 0;
-			AttemptCounter = 0;
-			numThreads=inumThreads;
+private:
+	// This is the ACE mutex that wraps the OS mutex which we will
+	// use to prevent simultaneous access to the resource.
+	ACE_Thread_Mutex mutex;
+	// This is the actual counter. We will initialize it in the constructor.
+	int LockedCounter;
+	// This is the attempt counter. We will initialize it in the constructor.
+	int AttemptCounter;
+public:
+	SharedResource() {
+		LockedCounter = 0;
+		AttemptCounter = 0;
+	}
+
+
+	// No need for any locking for getting the value
+	int getLockedCounter() {
+		return(LockedCounter);
+	}
+
+
+	// No need for any locking for getting the value
+	int getAttemptCounter() {
+		return(AttemptCounter);
+	}
+
+
+	// This method only attempts an increase and returns whether the
+	// attempt succeeded or not.
+	bool attemptIncreaseValue(int value) {
+		bool result = false;
+
+		// wait until mutex is acquired
+		//ACE_DEBUG((LM_DEBUG, ACE_TEXT("(%t) waiting for lock\n")));
+		mutex.acquire();
+		AttemptCounter++;
+		//ACE_DEBUG((LM_DEBUG, ACE_TEXT("(%t) Trying to update variable modulu=%d value=%d\n"), LockedCounter % 3, value));
+		if (LockedCounter % 3 == value) {
+			//ACE_DEBUG((LM_DEBUG, ACE_TEXT("(%t) increasing counter\n")));
+			LockedCounter++;
+			result = true;
 		}
+		mutex.release();
+		//ACE_DEBUG((LM_DEBUG, ACE_TEXT("(%t) released lock\n")));
+		ACE_OS::thr_yield();
+		return(result);
+	}
 
 
-		// No need for any locking for getting the value
-		int getLockedCounter() {
-			return(LockedCounter);
+	// This method will keep on trying until it succeeds. Watch out! Given
+	// the right set of cirumstances this method will loop forever.
+	void increaseValue(int value) {
+		while (!attemptIncreaseValue(value)) {
 		}
-
-		// No need for any locking for getting the value
-		int getAttemptCounter() {
-			return(AttemptCounter);
-		}
-
-		// This method only attempts an increase and returns whether the
-		// attempt succeeded or not.
-		bool attemptIncreaseValue(int value) {
-			bool result = false;
-
-			if(debug) {
-				ACE_DEBUG((LM_DEBUG, ACE_TEXT("(%t) waiting for lock\n")));
-			}
-			// wait until mutex is acquired
-			mutex.acquire();
-			AttemptCounter++;
-			if(debug) {
-				ACE_DEBUG((LM_DEBUG, ACE_TEXT("(%t) Trying to update variable modulu=%d value=%d\n"), LockedCounter % numThreads, value));
-			}
-			if (LockedCounter % numThreads == value) {
-				if(debug) {
-					ACE_DEBUG((LM_DEBUG, ACE_TEXT("(%t) increasing counter\n")));
-				}
-				LockedCounter++;
-				result = true;
-			}
-			mutex.release();
-			if(debug) {
-				ACE_DEBUG((LM_DEBUG, ACE_TEXT("(%t) released lock\n")));
-			}
-			if(yield) {
-				//ACE_OS::thr_yield();
-				int ret=sched_yield();
-				assert(ret!=-1);
-			}
-			return(result);
-		}
-
-		// This method will keep on trying until it succeeds. Watch out! Given
-		// the right set of cirumstances this method will loop forever.
-		void increaseValue(int value) {
-			while (!attemptIncreaseValue(value)) {
-			}
-		}
+	}
 };
 
 class HA_CommandHandler : public ACE_Task_Base {
-	private:
-		SharedResource&  sharedResource;
-		const int value;
-	public:
-		HA_CommandHandler(SharedResource & sharedResource, const int value) : sharedResource(sharedResource), value(value) {
-		}
+private:
+	static const int numAttempts = 1000;
+	SharedResource&  sharedResource;
+	const int value;
 
-		// The real body of the thread
-		virtual int svc(void) {
-			ACE_DEBUG((LM_DEBUG, ACE_TEXT("(%t) start\n")));
-			for (int i = 0; i < numAttempts; i++) {
-				sharedResource.attemptIncreaseValue(value);
-				// Sleep for 1 sec to let the other thread try to aquire the lock
-				// What if we remove this sleep period? Will the program work ?
-				//ACE_OS::sleep(1);
-			}
-			return(0);
+public:
+	HA_CommandHandler(SharedResource & sharedResource, const int value) : sharedResource(sharedResource), value(value) {
+	}
+
+	// The real body of the thread
+	virtual int svc(void) {
+		ACE_DEBUG((LM_DEBUG, ACE_TEXT("(%t) Handler Thread running\n")));
+		for (int i = 0; i < numAttempts; i++) {
+			//while(LockedCounter<25)
+			//bool res=sharedResource.attemptIncreaseValue(value);
+			sharedResource.attemptIncreaseValue(value);
+			//if(!res) {
+			//	ACE_OS::thr_yield();
+			//}
+			// Sleep for 1 sec to let the other thread try to aquire the lock
+			// What if we remove this sleep period? Will the program work ?
+			//ACE_OS::sleep(1);
 		}
+		return(0);
+	}
 };
 
-int ACE_TMAIN(int argc, ACE_TCHAR ** argv) {
-	int num_threads=3;
-	if(argc>1) {
-		num_threads=atoi(argv[1]);
-	}
-	SharedResource sharedResource(num_threads);
+int ACE_TMAIN(int, ACE_TCHAR *[]) {
+	SharedResource sharedResource;
 
-	HA_CommandHandler* handlers[num_threads];
-	for(int i=0;i<num_threads;i++) {
-		handlers[i]=new HA_CommandHandler(sharedResource, i);
-	}
-	for(int i=0;i<num_threads;i++) {
-		handlers[i]->activate();
-	}
-	for(int i=0;i<num_threads;i++) {
-		handlers[i]->wait();
-	}
+	HA_CommandHandler handler1(sharedResource, 0);
+	HA_CommandHandler handler2(sharedResource, 1);
+	HA_CommandHandler handler3(sharedResource, 2);
+
+	handler1.activate();
+	handler2.activate();
+	handler3.activate();
+	handler1.wait();
+	handler2.wait();
+	handler3.wait();
 	ACE_DEBUG((LM_DEBUG, ACE_TEXT("(%t) LockedCounter=%d\n"), sharedResource.getLockedCounter()));
 	ACE_DEBUG((LM_DEBUG, ACE_TEXT("(%t) AttemptCounter=%d\n"), sharedResource.getAttemptCounter()));
 	return(0);
