206e261023a65fcaa512337b1bf38a0d3a701530
Mark Veltzer <mark.veltzer@gmail.com>
2011-09-13 00:16:54 +0300

    fixed up this example

--- src/user_space/libs/hellomysqlpp.cc
+++ src/user_space/libs/hellomysqlpp.cc
@@ -1,41 +1,25 @@
-#include <iostream>
-#include <iomanip>
-#include <mysql++.h>
+#include <iostream> // for std::cout, std::cerr, std::endl
+#include <mysql++.h> // for mysqlpp::*
 
 /*
+ * You need all of these folders added with -I because of how the h files
+ * for mysql++ are written.
  * EXTRA_LIBS=-I/usr/include/mysql++ -I/usr/include/mysql -lmysqlpp
  */
 
 /* This is a mysql++ demo program */
 
-using namespace mysqlpp;
-using namespace std;
-
-int main(int, char **, char **) {
-	try {
-		Connection con("mysql", "database", "master", "master", true);
-
-		Query query = con.query("select item from stock");
-//		Query query=con.query();
-//		query << "select * from user";
-		StoreQueryResult res = query.store("select * from user");
-		StoreQueryResult::iterator i;
-		int j = 0;
-		for (i = res.begin(); i != res.end(); i++) {
-			Row row = *i;
-			cout << j << ":" << row[0] << "\n";
-			j++;
-		}
-		cout << "Records Found: " << res.size() << endl;
-	} catch (...) {
-		cerr << "exception\n";
-		return(-1);
+int main(int argc,char** argv, char** envp) {
+	// parameters are: schema, host, user, pass, use exceptions
+	mysqlpp::Connection con("myworld", "localhost", "mark", "", true);
+	mysqlpp::Query query=con.query("select id,name from TbOrganization order by id");
+	mysqlpp::StoreQueryResult res=query.store();
+	unsigned int j = 0;
+	std::cout << "Records Found: " << res.size() << std::endl;
+	for (mysqlpp::StoreQueryResult::iterator i = res.begin(); i != res.end(); i++) {
+		mysqlpp::Row row = *i;
+		std::cout << j << ":" << row["name"] << std::endl;
+		j++;
 	}
 	return(0);
 }
-
-
-//unfortunate template stuff
-
-//template const_subscript_container<MysqlRow, mysql_ColData<const_string>, mysql_ColData<const_string> const, unsigned, int>;
-//template char* std::basic_string<char, std::char_traits<char>, std::allocator<char> >::_S_construct<char const*>(char const*, char const*, std::allocator<char> const&, std::forward_iterator_tag);
