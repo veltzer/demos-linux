f8f252f359595a9d2d7f4860fa272d92f2c31192
Mark Veltzer <mark.veltzer@gmail.com>
2011-09-18 13:36:02 +0300

    many fixes

--- src/user_space/io/bad_fd.cc
+++ src/user_space/io/bad_fd.cc
@@ -1,13 +1,14 @@
 #include <stdio.h>  // for perror(3)
-#include <unistd.h> // for read(2)
+#include <unistd.h> // for read(2), getdtablesize(2)
 
 /*
  *      This is an example of what happens when you read from a bad fd.
  *      The important thing to note is that you do not SEGFAULT. Technically
  *      you called a syscall with the wrong parameter and what you get back
  *      is an error.
- *      Note yet that read(2) return ssize_t which is SIGNED as opposed to size_t
- *      which is UNSIGNED. The idea here is the ability to return -1.
+ *      Note yet that read(2) return type is ssize_t which is SIGNED as opposed to size_t
+ *      which is UNSIGNED. The idea here is the ability to return -1 and report errors
+ *      this way.
  *
  *              Mark Veltzer
  *
@@ -15,7 +16,7 @@
  */
 int main(int argc, char **argv, char **envp) {
 	const size_t SIZE = 5;
-	const int BAD_FD = 11;
+	const int BAD_FD = getdtablesize()+1;
 	char buf[SIZE];
 	ssize_t s = read(BAD_FD, buf, SIZE);
 
