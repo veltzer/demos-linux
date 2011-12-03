feeb2ab51c825033399cfd1e9fd55f7b6c0305f4
Mark Veltzer <mark.veltzer@gmail.com>
2011-12-03 03:57:49 +0200

    fixed a small bug

--- src/user_space/lang_c/union_issues.cc
+++ src/user_space/lang_c/union_issues.cc
@@ -29,10 +29,16 @@ typedef union _LARGE_INTEGER {
 int main(int argc, char **argv, char **envp) {
 	// this is how you would use such a union...
 	LARGE_INTEGER li;
-
-	li.LowPart = 15;
-	li.HighPart = 16;
-	li.QuadPart = 17;
+	li.LowPart=15;
+	li.HighPart=16;
+	printf("li.LowPart is %u\n",li.LowPart);
+	printf("li.HighPart is %u\n",li.HighPart);
+	printf("li.QuadPart is %lli\n",li.QuadPart);
+	// trampling the quad part tramples both low and high parts
+	li.QuadPart=17;
+	printf("li.LowPart is %u\n",li.LowPart);
+	printf("li.HighPart is %u\n",li.HighPart);
+	printf("li.QuadPart is %lli\n",li.QuadPart);
 	// this is the offsets of it's relevant parts...
 	printf("offset of LowPart is %d\n", OffsetOf(LARGE_INTEGER, LowPart));
 	printf("offset of HighPart is %d\n", OffsetOf(LARGE_INTEGER, HighPart));
