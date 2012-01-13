78d9645b9b21c805772edc0a457de6d0f72cf9d7
Mark Veltzer <mark.veltzer@gmail.com>
2012-01-14 01:08:38 +0200

    package installation script

---
+++ scripts/ubuntu_install.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+# this scrip will install all the required packages that you need on
+# ubuntu to compile and work with this package.
+
+import subprocess
+
+packs=[
+	'libncurses5',
+	'libncurses5-dev',
+	'libncursesw5',
+	'libncursesw5-dev',
+	'ncurses-doc',
+	'libproc-dev',
+	'libgnomeui-dev',
+	'libsigc++-dev',
+	'libgnomeuimm-2.6-dev',
+	'libpq-dev',
+	'liblog4cpp5-dev',
+	'binutils-dev',
+	'libmysqlclient-dev',
+	'libcwd',
+	'libqt4-dev',
+	'libwxgtk2.8-dev',
+	'libmysql++-dev',
+	'libsdl1.2-dev',
+	'dialog',
+	'libace-dev',
+	'iptables-dev',
+]
+
+args=['sudo','apt-get','install']
+args.extend(packs)
+subprocess.check_call(args)
