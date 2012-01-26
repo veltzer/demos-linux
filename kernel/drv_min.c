a4e80d04103468f656e158b056fb7b69136dc5d2
Mark Veltzer <mark@veltzer.net>
2012-01-26 02:01:01 +0200

    fixes for new kernel

--- kernel/drv_min.c
+++ kernel/drv_min.c
@@ -1,4 +1,6 @@
+#define DEBUG
 #include <linux/module.h> // for MODULE_*. module_*
+#include <linux/printk.h> // for pr_*
 
 #include "kernel_helper.h" // our own helper
 
@@ -12,13 +14,15 @@ MODULE_DESCRIPTION("Demo module for testing");
 
 // our own functions
 static int __init mod_init(void) {
-	INFO("start");
+	pr_debug("init");
+	PR_INFO("init");
 	return(0);
 }
 
 
 static void __exit mod_exit(void) {
-	INFO("start");
+	PR_INFO("exit");
+	pr_debug("exit");
 }
 
 // declaration of init/cleanup functions of this module
