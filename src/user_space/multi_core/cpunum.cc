5b2b6ede5d9504ac8da3c35a6fe33a158996a065
Mark Veltzer <mark@veltzer.net>
2012-01-27 20:00:09 +0200

    more samples

---
+++ src/user_space/multi_core/cpunum.cc
@@ -0,0 +1,19 @@
+#include <stdio.h> // for printf(3)
+#include <unistd.h> // for sysconf(3)
+
+#include "us_helper.hh"
+
+/*
+ * This is a simple example showing how to get the number of current cpus which are online...
+ * Remmember that this number can change at any minute.
+ *
+ *		Mark Veltzer
+ *
+ * EXTRA_LIBS=
+ */
+int main(int argc, char **argv, char **envp) {
+	int ncpus;
+	sc((ncpus=sysconf(_SC_NPROCESSORS_ONLN)));
+	printf("sysconf(_SC_NPROCESSORS_ONLN)=%d\n",ncpus);
+	return(0);
+}
