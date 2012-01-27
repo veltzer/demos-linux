c9ad33b2b53acc5c67f93666140cc5d9aaf41fd9
Mark Veltzer <mark@veltzer.net>
2012-01-27 04:27:06 +0200

    fixed up the test script a bit

--- kernel_standalone/pipe/test_speed.sh
+++ kernel_standalone/pipe/test_speed.sh
@@ -3,6 +3,7 @@
 # this will test the speed of my pipe device
 
 fuser -k /dev/mypipe0
-dd if=/dev/zero of=/dev/mypipe0 &
+cat /dev/zero > /dev/mypipe0 &
+#dd if=/dev/zero of=/dev/mypipe0 &
 pipemeter < /dev/mypipe0
 fuser -k /dev/mypipe0
