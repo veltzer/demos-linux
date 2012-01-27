1e98d7ff282eb1cc2b7c8440885c6e07a09cf43b
Mark Veltzer <mark@veltzer.net>
2012-01-27 14:10:45 +0200

    factor out

--- kernel/mod_open_close_release.c
+++ kernel/mod_open_close_release.c
@@ -1,6 +1,6 @@
 //#define DEBUG
 #include <linux/module.h> // for MODULE_*
-#include <linux/printk.h> // for printk, pr_*
+#include <linux/device.h>  // for dev_[warn|info|..]
 #include <linux/fs.h> // for fops
 
 //#define DO_DEBUG
@@ -16,22 +16,24 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Mark Veltzer");
 MODULE_DESCRIPTION("module with open and release");
 
+// static data
+static struct device* my_device;
+
 // fops
 
 /*
  * The open implementation. Currently this does nothing
  */
 static int kern_open(struct inode *inode, struct file *filp) {
-	pr_info("start");
+	dev_info(my_device,"start");
 	return(0);
 }
-
 
 /*
  * The release implementation. Currently this does nothing
  */
 static int kern_release(struct inode *inode, struct file *filp) {
-	pr_info("start");
+	dev_info(my_device,"start");
 	return(0);
 }
 
