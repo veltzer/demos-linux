db6453f918b767d20df8b508c22b3bc6d1c8902a
Mark Veltzer <mark@veltzer.net>
2012-01-28 18:54:28 +0200

    removed even more includes

--- kernel/mod_get_free_pages.c
+++ kernel/mod_get_free_pages.c
@@ -1,19 +1,6 @@
 //#define DEBUG
 #include <linux/module.h> // for MODULE_*
-/*
-#include <linux/kernel.h>
-#include <linux/pci.h>
-#include <linux/init.h>
-#include <linux/io.h>
-#include <linux/interrupt.h>
-#include <linux/cdev.h>
-#include <linux/uaccess.h>
-#include <linux/device.h>
-#include <linux/types.h>
-#include <linux/proc_fs.h>
-#include <linux/mm.h>
-#include <linux/slab.h>
-*/
+#include <linux/slab.h> // for memory allocation API
 
 //#define DO_DEBUG
 #include "kernel_helper.h" // our own helper
