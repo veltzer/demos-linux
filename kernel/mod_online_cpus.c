9f479cf3251562a3cb7639cfb3feca146a5ea796
Mark Veltzer <mark@veltzer.net>
2012-01-28 07:44:04 +0200

    a module for showing the number of online cpus

--- kernel/mod_online_cpus.c
+++ kernel/mod_online_cpus.c
@@ -1,29 +1,27 @@
 //#define DEBUG
 #include <linux/module.h> // for MODULE_*. module_*
 #include <linux/printk.h> // for pr_*
+#include <linux/cpumask.h> // for num_online_cpus
 
 //#define DO_DEBUG
 #include "kernel_helper.h" // our own helper
 
 /*
- *      This is a minimal module doing just init and cleanup
+ * This is a minimal module for showing the number of current online cpus
  */
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Mark Veltzer");
-MODULE_DESCRIPTION("Demo module for testing");
+MODULE_DESCRIPTION("Module showing the number of online cpus");
 
 // our own functions
 static int __init mod_init(void) {
-	pr_info("start");
-	PR_INFO("start");
+	pr_info("number of online cpus is %d",num_online_cpus());
 	return(0);
 }
 
 
 static void __exit mod_exit(void) {
-	PR_INFO("start");
-	pr_info("start");
 }
 
 // declaration of init/cleanup functions of this module
