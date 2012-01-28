247b721a61cd5991ed6e1456d67aa98482bc1cfe
Mark Veltzer <mark.veltzer@gmail.com>
2012-01-28 17:37:27 +0200

    using the common infra

--- kernel/mod_spin_lock.c
+++ kernel/mod_spin_lock.c
@@ -1,16 +1,9 @@
 //#define DEBUG
-#include <linux/kernel.h>
-#include <linux/module.h>
-#include <linux/pci.h>
-#include <linux/init.h>
-#include <linux/io.h>
-#include <linux/interrupt.h>
-#include <linux/cdev.h>
-#include <linux/uaccess.h>
-#include <linux/device.h>
-#include <linux/types.h>
-#include <linux/proc_fs.h>
-#include <linux/mm.h>
+#include <linux/module.h> // for MODULE_*
+#include <linux/fs.h> // for fops
+#include <linux/device.h> // for struct device
+#include <linux/spinlock.h> // for the spin lock API
+#include <linux/slab.h> // for the kmalloc API
 
 //#define DO_DEBUG
 #include "kernel_helper.h" // our own helper
@@ -26,39 +19,16 @@ MODULE_DESCRIPTION("Showing how to use spin locks in the kernel");
  * http://www.kernel.org/pub/linux/kernel/people/rusty/kernel-locking/index.html
  */
 
-// parameters for this module
-
-static int chrdev_alloc_dynamic = 1;
-static int first_minor = 0;
-static int kern_major = 253;
-static int kern_minor = 0;
-
-// constants for this module
-
-// number of files we expose via the chr dev
-static const int MINORS_COUNT = 1;
-
-// first the structures
-
-struct kern_dev {
-	// pointer to the first device number allocated to us
-	dev_t first_dev;
-	// cdev structures for the char devices we expose to user space
-	struct cdev cdev;
-};
-
 // static data
-static struct kern_dev *pdev;
-static struct class* my_class;
 static struct device* my_device;
+DEFINE_SPINLOCK(mr_lock); // unlocked by default...
+static spinlock_t *lock_t;
 
-// now the functions
+// fops
 
 /*
  * This is the ioctl implementation.
  */
-DEFINE_SPINLOCK(mr_lock); // unlocked by default...
-spinlock_t *lock_t;
 static long kern_unlocked_ioctll(struct file *filp, unsigned int cmd, unsigned long arg) {
 	unsigned long flags;
 
@@ -95,95 +65,4 @@ static struct file_operations my_fops = {
 	.unlocked_ioctl = kern_unlocked_ioctll,
 };
 
-int register_dev(void) {
-	// create a class
-	my_class = class_create(THIS_MODULE, THIS_MODULE->name);
-	if (IS_ERR(my_class)) {
-		goto goto_nothing;
-	}
-	PR_DEBUG("created the class");
-	// alloc and zero
-	pdev = kmalloc(sizeof(struct kern_dev), GFP_KERNEL);
-	if (pdev == NULL) {
-		goto goto_destroy;
-	}
-	memset(pdev, 0, sizeof(struct kern_dev));
-	PR_DEBUG("set up the structure");
-	if (chrdev_alloc_dynamic) {
-		if (alloc_chrdev_region(&pdev->first_dev, first_minor, MINORS_COUNT, THIS_MODULE->name)) {
-			PR_DEBUG("cannot alloc_chrdev_region");
-			goto goto_dealloc;
-		}
-	} else {
-		pdev->first_dev = MKDEV(kern_major, kern_minor);
-		if (register_chrdev_region(pdev->first_dev, MINORS_COUNT, THIS_MODULE->name)) {
-			PR_DEBUG("cannot register_chrdev_region");
-			goto goto_dealloc;
-		}
-	}
-	PR_DEBUG("allocated the device");
-	// create the add the sync device
-	cdev_init(&pdev->cdev, &my_fops);
-	pdev->cdev.owner = THIS_MODULE;
-	pdev->cdev.ops = &my_fops;
-	kobject_set_name(&pdev->cdev.kobj, THIS_MODULE->name);
-	if (cdev_add(&pdev->cdev, pdev->first_dev, 1)) {
-		PR_DEBUG("cannot cdev_add");
-		goto goto_deregister;
-	}
-	PR_DEBUG("added the device");
-	// now register it in /dev
-	my_device = device_create(
-		my_class,/* our class */
-		NULL,/* device we are subdevices of */
-		pdev->first_dev,
-		NULL,
-		THIS_MODULE->name,
-		0
-	);
-	if (my_device == NULL) {
-		PR_DEBUG("cannot create device");
-		goto goto_create_device;
-	}
-	PR_DEBUG("did device_create");
-	return(0);
-
-	//goto_all:
-	//	device_destroy(my_class,pdev->first_dev);
-goto_create_device:
-	cdev_del(&pdev->cdev);
-goto_deregister:
-	unregister_chrdev_region(pdev->first_dev, MINORS_COUNT);
-goto_dealloc:
-	kfree(pdev);
-goto_destroy:
-	class_destroy(my_class);
-goto_nothing:
-	return(-1);
-}
-
-
-void unregister_dev(void) {
-	device_destroy(my_class, pdev->first_dev);
-	cdev_del(&pdev->cdev);
-	unregister_chrdev_region(pdev->first_dev, MINORS_COUNT);
-	kfree(pdev);
-	class_destroy(my_class);
-}
-
-
-// our own functions
-static int __init mod_init(void) {
-	return(register_dev());
-}
-
-
-static void __exit mod_exit(void) {
-	unregister_dev();
-}
-
-
-// declaration of init/cleanup functions of this module
-
-module_init(mod_init);
-module_exit(mod_exit);
+#include "device.inc"
