0445e5433fa76b4091a92d09843ffaa1976f6068
Mark Veltzer <mark@veltzer.net>
2012-01-28 18:51:41 +0200

    fixed up compilation issues

--- kernel/mod_rammap.c
+++ kernel/mod_rammap.c
@@ -1,19 +1,7 @@
 //#define DEBUG
 #include <linux/module.h> // for MODULE_*
-/*
-#include <linux/kernel.h>
-#include <linux/pci.h>
-#include <linux/init.h>
-#include <linux/io.h>
-#include <linux/interrupt.h>
-#include <linux/cdev.h>
-#include <linux/uaccess.h>
-#include <linux/device.h>
-#include <linux/types.h>
-#include <linux/proc_fs.h>
-#include <linux/mm.h>
-#include <asm/e820.h>
-*/
+#include <linux/page-flags.h> // for PG_* constants
+#include <linux/mm.h> // for page_* functions
 
 //#define DO_DEBUG
 #include "kernel_helper.h" // our own helper
