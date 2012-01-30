de99eef6091f38769196b0b1bc2c01e97897882a
Mark Veltzer <mark@veltzer.net>
2012-01-30 17:25:53 +0200

    improved this example

--- src/user_space/performance/syscalls_comparison.cc
+++ src/user_space/performance/syscalls_comparison.cc
@@ -2,6 +2,8 @@
 #include <sys/time.h> // for gettimeofday(2)
 #include <sys/types.h> // for getpid(2), gettid(2)
 #include <unistd.h> // for getpid(2)
+#include <stdlib.h> // for free(3), malloc(3)
+#include <pthread.h> // for pthread_key_create(3), pthread_setspecific(3), pthread_getspecific(3)
 
 #include "us_helper.hh" // for micro_diff
 
@@ -12,15 +14,43 @@
  * 		the system time.
  * 	- gettid(2). intermediate. not cached.
  * 	- getpid(2). best since it is cached by the kernel.
+ * we also show the performance of gettid_cached which is a TLS cached version
+ * of gettid and performs much better.
  *
  * How do I know that gcc actually calls getpid or gettid? I see it in the disassemly.
  * (gettimeofday is obviously called)
  *
  * TODO:
- * - do caching of gettid() using TLS.
  *
  * 		Mark Veltzer
+ * EXTRA_LIBS=-lpthread
  */
+
+pthread_key_t tid_key;
+typedef struct _cached_tid {
+	pid_t val;
+} cached_tid;
+
+void gettid_cache_delete(void* ptr) {
+	free(ptr);
+}
+
+pid_t gettid_cached() {
+	void* ptr=pthread_getspecific(tid_key);
+	if(ptr==NULL) {
+		cached_tid* ctid=(cached_tid*)malloc(sizeof(cached_tid));
+		ctid->val=gettid();
+		sc(pthread_setspecific(tid_key,ctid));
+		return ctid->val;
+	} else {
+		cached_tid* ctid=(cached_tid*)ptr;
+		return ctid->val;
+	}
+}
+
+void gettid_cache_init() {
+	sc(pthread_key_create(&tid_key,gettid_cache_delete));
+}
 
 int main(int argc, char **argv, char** envp) {
 	struct timeval t1, t2;
@@ -47,6 +77,15 @@ int main(int argc, char **argv, char** envp) {
 	sc(gettimeofday(&t1, NULL));
 	for (unsigned int i = 0;i < loop;i++) {
 		gettid();
+	}
+	sc(gettimeofday(&t2, NULL));
+	printf("time in micro of one op: %lf\n", micro_diff(&t1,&t2)/(double)loop);
+
+	gettid_cache_init();
+	printf("doing %d gettid_cached\n",loop);
+	sc(gettimeofday(&t1, NULL));
+	for (unsigned int i = 0;i < loop;i++) {
+		gettid_cached();
 	}
 	sc(gettimeofday(&t2, NULL));
 	printf("time in micro of one op: %lf\n", micro_diff(&t1,&t2)/(double)loop);
