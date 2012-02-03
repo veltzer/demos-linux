57f387f137a0c46a521cf53dd66a5b65439cdbee
Mark Veltzer <mark.veltzer@gmail.com>
2012-02-03 14:51:12 +0200

    more code

--- src/kernel_standalone/pipe/test_linux_pipe_speed.sh
+++ src/kernel_standalone/pipe/test_linux_pipe_speed.sh
@@ -3,4 +3,5 @@
 # This script tests the speed of the regular linux pipe...
 # Just break this script with 'CTRL+C'.
  
-cat /dev/zero | pipemeter > /dev/null
+#cat /dev/zero | pipemeter > /dev/null
+dd if=/dev/zero bs=50000 count=100000 | pipemeter > /dev/null
