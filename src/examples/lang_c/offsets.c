73c00a84f1822754c6b6cffef66d9f22a99fb93c
Mark Veltzer <mark@veltzer.net>
2012-02-03 19:50:30 +0200

    more notes

--- src/examples/lang_c/offsets.c
+++ src/examples/lang_c/offsets.c
@@ -2,11 +2,12 @@
 #include <stddef.h> // for offsetof(3)
 
 /*
- *      watch these macro and see if you can figure out what it does.
- *      we use '1' instead of '0' in these next lines because gcc is smart
- *      enough to understand that using 0 will mean that we use the NULL
- *      object and not let us do anything with it including taking fields
- *      or doing pointer arithmetic...
+ *	This is an example of finding out how far a field is from the begining
+ *	of the structure it is embedded in.
+ *      In a similar example in C++ (using g++ instead of gcc) we had to
+ *      use '1' instead of '0' in these next macros because g++ is smarter
+ *      than gcc and translated the 0 to NULL and disallowed it use.
+ *      In this example, since we are compiling to C we can use 0 freely.
  *      Notice that with my macro, offsetof and __builtin_offsetof you can use the
  *      "struct" prefix to the structure name or drop it.
  *
@@ -14,7 +15,7 @@
  * EXTRA_LIBS=
  */
 
-#define myoffsetof(structname,fieldname) ((char *)(&(((structname *)1)->fieldname)) - (char *)1)
+#define myoffsetof(structname,fieldname) ((char *)(&(((structname *)0)->fieldname)) - (char *)0)
 
 // our own struct for the demo...
 typedef struct _mystruct {
@@ -26,15 +27,15 @@ typedef struct _mystruct {
 int main(int argc,char** argv,char** envp) {
 	// using our own macro
 	printf("field_a is in offset %d\n",myoffsetof(mystruct,field_a));
-	printf("field_b is in offset %d\n",myoffsetof(mystruct,field_b));
+	printf("field_b is in offset %d\n",myoffsetof(struct _mystruct,field_b));
 	printf("field_c is in offset %d\n",myoffsetof(mystruct,field_c));
 	// using a libc macro
 	printf("field_a is in offset %d\n",offsetof(mystruct,field_a));
-	printf("field_b is in offset %d\n",offsetof(mystruct,field_b));
+	printf("field_b is in offset %d\n",offsetof(struct _mystruct,field_b));
 	printf("field_c is in offset %d\n",offsetof(mystruct,field_c));
 	// using a gcc builtin
 	printf("field_a is in offset %d\n",__builtin_offsetof(mystruct,field_a));
-	printf("field_b is in offset %d\n",__builtin_offsetof(mystruct,field_b));
+	printf("field_b is in offset %d\n",__builtin_offsetof(struct _mystruct,field_b));
 	printf("field_c is in offset %d\n",__builtin_offsetof(mystruct,field_c));
 	return(0);
 }
--- src/exercises/io/ls.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h> // for printf(3)
-
-int main(int argc,char** argv,char** envp) {
-	printf("Hello, World!\n");
-	return 0;
-}
