8077ca1f1af59b621c27ffe5509ccc83aa8d04f0
Mark Veltzer <mark.veltzer@gmail.com>
2012-02-05 10:52:09 +0200

    minor fixes

--- src/kernel_standalone/pipe/test_our_pipe_speed.sh
+++ src/kernel_standalone/pipe/test_our_pipe_speed.sh
@@ -4,6 +4,8 @@
 
 fuser -k /dev/pipe0
 dd if=/dev/zero bs=50000 count=1000000 of=/dev/pipe0 &
+# this is to make sure that the writing starts before the reading.
+sleep 1
 #dd if=/dev/zero of=/dev/pipe0 &
 pipemeter < /dev/pipe0 > /dev/null &
 # wait for the two background processes to be over
