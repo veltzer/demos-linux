735f34419e8bda88531552f079ec141c67129f81
Mark Veltzer <mark.veltzer@gmail.com>
2012-02-12 16:19:02 +0200

    better examples for multi targets

--- tools/makefiles/Makefile.multiple_targets2.mk
+++ tools/makefiles/Makefile.multiple_targets2.mk
@@ -1,20 +1,40 @@
 # This makefile shows that writing rules of the form:
 # a b: c
 # 	[cmd]
-# will run the rule for a and b once.
-# this is what you want if the command in question produces
-# both a and b.
+# could potentialy run the rule once for a and once for b.
+# You write the rule using the automatic varialble $@
+# and so you know which target you are creating.
+#
+# The problem with this approach is that the GNU make automatic $@ variable
+# does not really refer to the whole set of targets but only to the one
+# currently asked to be created.
+#
+# This means that writing a multi-target rule in make is just a short hand
+# for writing two rules which means that make doesn't REALLY support multiple
+# targets at a deep level. This also explains why there is no automatic variable
+# in GNU make which refers to ALL the targets.
+#
+# If you create both targets in a 2 target rule then it will be run once since
+# after the first run (that creates both targets) GNU make will check the last
+# modification times of the relevant files and will see that the second target
+# should not be created.
 
-SOURCE=source.junk
+SOURCE=source.file
 TG1=file1.junk
 TG2=file2.junk
+TG3=file3.junk
+TG4=file4.junk
 
 .PHONY: all
-all: $(TG1) $(TG2)
+all: $(TG1) $(TG2) $(TG3) $(TG4)
 
 $(TG1) $(TG2): $(SOURCE)
-	echo $@
-	touch $(TG1) $(TG2)
+	$(info doing $@)
+	touch $@
+
+$(TG3) $(TG4): $(SOURCE)
+	$(info doing $@)
+	touch $(TG3) $(TG4)
 
 .PHONY: clean
 clean:
