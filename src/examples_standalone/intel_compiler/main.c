17722e4a3445d77dc519f24046d21bd65e9813e6
Mark Veltzer <mark@veltzer.net>
2012-02-20 04:57:16 +0200

    added intel based PGO

--- src/examples_standalone/intel_compiler/main.c
+++ src/examples_standalone/intel_compiler/main.c
@@ -2,15 +2,33 @@
 
 int main(int argc,char** argv,char** envp) {
 	// don't remove the prints, it make the disassembly easier to understand...
-	printf("start\n");
+	printf("===================================\n");
+
 	// this is a compiler reordering barrier in the intel compiler...
 	// this does not emit any assembly...
 	__memory_barrier();
-	printf("here\n");
+
+	printf("===================================\n");
+
 	// this is a machine reordering barrier, which is also a compiler barrier
 	// (because the documentation says it is, not because every machine barrier
 	// is also a compiler reordering barrier)
 	__sync_synchronize();
-	printf("end\n");
+
+	printf("===================================\n");
+
+	// here is a compare and swap that succeeds...
+	int a=4;
+	int old_val=__sync_val_compare_and_swap(&a,4,5);
+	printf("old_val is %d and a is %d\n",old_val,a);
+
+	printf("===================================\n");
+
+	// here is a compare and swap that fails...
+	a=17;
+	old_val=__sync_val_compare_and_swap(&a,4,5);
+	printf("old_val is %d and a is %d\n",old_val,a);
+
+	printf("===================================\n");
 	return 0;
 }
---
+++ src/examples_standalone/intel_compiler_pgo/main.cc
@@ -0,0 +1,6 @@
+#include<stdio.h> // for printf(3)
+
+int main(int argc,char** argv,char** envp) {
+	printf("Hello, Intel PGO!\n");
+	return(0);
+}
