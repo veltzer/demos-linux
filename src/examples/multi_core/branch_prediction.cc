f7dc199b8a92ba93a501696e8abce3edec2935f4
Mark Veltzer <mark.veltzer@gmail.com>
2012-02-20 02:46:11 +0200

    more code

--- src/examples/multi_core/branch_prediction.cc
+++ src/examples/multi_core/branch_prediction.cc
@@ -7,6 +7,8 @@
 // the identity function.
 #define likely(x)       __builtin_expect((x),1)
 #define unlikely(x)     __builtin_expect((x),0)
+//#define likely(x)	x
+//#define unlikely(x)     x
 
 /*
  * This is an example of giving hints to the compiler about branchings.
@@ -19,12 +21,19 @@
  * Where can this help?
  * It can allow the compiler to reorder the control-flow graph to reduce the number of branches taken for the 'likely' path. This can have a marked improvement in loops where you're checking multiple exit cases.
  *
+ * TODO:
+ * - on the command line using time(1) this example runs the same with branch prediction and without.
+ *   Modify this to have a marked effect on performance and measure itself so that I can show when
+ *   this is effective.
+ *
  *		Mark Veltzer
  */
 int main(int argc, char **argv, char **envp) {
 	int x=0;
 	long long sum=0;
-	while(likely(x<100000000)) {
+	//while(likely(x<100000000)) {
+	// mispredict on purpose...
+	while(unlikely(x<100000000)) {
 		// to force the compiler to actually DO the loop.
 		// moving this line to sum+=x will actually mean that the compiler
 		// will calculate the whole loop in advance!!!
