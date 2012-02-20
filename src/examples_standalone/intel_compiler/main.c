e630eca8e9e68acb933d008ab3899637173d7884
Mark Veltzer <mark.veltzer@gmail.com>
2012-02-20 04:29:49 +0200

    more examples

--- src/examples_standalone/intel_compiler/main.c
+++ src/examples_standalone/intel_compiler/main.c
@@ -1,6 +1,16 @@
 #include <stdio.h> // for printf(3)
 
 int main(int argc,char** argv,char** envp) {
-	printf("Hello, World!\n");
+	// don't remove the prints, it make the disassembly easier to understand...
+	printf("start\n");
+	// this is a compiler reordering barrier in the intel compiler...
+	// this does not emit any assembly...
+	__memory_barrier();
+	printf("here\n");
+	// this is a machine reordering barrier, which is also a compiler barrier
+	// (because the documentation says it is, not because every machine barrier
+	// is also a compiler reordering barrier)
+	__sync_synchronize();
+	printf("end\n");
 	return 0;
 }
