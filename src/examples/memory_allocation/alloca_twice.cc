e759e08475a4b93e7a04f69df96488bc7c0ea434
Mark Veltzer <mark.veltzer@gmail.com>
2012-02-21 18:43:26 +0200

    added another example of alloca(3)

---
+++ src/examples/memory_allocation/alloca_twice.cc
@@ -0,0 +1,25 @@
+#include <strings.h> // for bzero(3)
+#include <alloca.h> // for alloca(3)
+#include <stdio.h> // for printf(3)
+
+#include "us_helper.hh"
+
+/*
+ * This example investigates what happens if you call alloca(3) twice...
+ *
+ * Results:
+ * - you can call alloca more than once.
+ * - as a result you CANNOT free memory obtained by alloca(3).
+ * - alloca rounds stuff up by 8 bytes or some such.
+ *
+ * 	Mark Veltzer
+ */
+
+int main(int argc,char** argv,char** envp) {
+	for(int i=0;i<10;i++) {
+		char* p=(char*)alloca(10);
+		bzero(p,10);
+		printf("p1 is %p\n",p);
+	}
+	return(0);
+}
