1b0f96097ddba67bef16780c1da33f4f22104c70
Mark Veltzer <mark@veltzer.net>
2012-02-22 10:55:58 +0200

    added malloc_stats to malloc demo

--- src/examples/memory_allocation/malloc_lazy.cc
+++ src/examples/memory_allocation/malloc_lazy.cc
@@ -2,6 +2,7 @@
 #include <unistd.h> // for sleep(3), getpagesize(2)
 #include <stdlib.h> // for malloc(3), atoi(3), exit(3)
 #include <sys/mman.h> // for mlockall(2), munlockall(2)
+#include <malloc.h> // for malloc_stats(3)
 
 #include "us_helper.hh"
 
@@ -48,9 +49,10 @@ int main(int argc, char **argv, char **envp) {
 		printf("5) allocate more ram (with touching it)\n");
 		printf("6) call mlockall (no future)\n");
 		printf("7) call mlockall (with future)\n");
-		printf("9) call munlockall\n");
+		printf("8) call munlockall\n");
 		printf("9) just print stats\n");
-		printf("10) exit\n");
+		printf("10) stats\n");
+		printf("11) exit\n");
 		unsigned int bufsize=256;
 		char buf[bufsize];
 		char *r=fgets(buf,bufsize,stdin);
@@ -110,6 +112,9 @@ int main(int argc, char **argv, char **envp) {
 				print_stats();
 				break;
 			case 10:
+				malloc_stats();
+				break;
+			case 11:
 				over=true;
 				break;
 			default:
