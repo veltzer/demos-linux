d4d8cdef9bfdb970ff4e377dd2ddb67054bfcd08
Mark Veltzer <mark.veltzer@gmail.com>
2012-03-04 00:12:11 +0200

    some more examples

---
+++ src/examples/lang_cpp/hello.cc
@@ -0,0 +1,12 @@
+#include <iostream> // for std::cout, std::endl
+
+/*
+ * A simple hello world application for C++.
+ *
+ *		Mark Veltzer
+ */
+
+int main(int argc, char **argv, char **envp) {
+	std::cout << "Hello, World!" << std::endl;
+	return(0);
+}
