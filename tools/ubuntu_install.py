266678428f5ec8e42d11cacbf1e5b6eafb794f26
Mark Veltzer <mark.veltzer@gmail.com>
2012-03-19 12:54:51 +0200

    more scripts

---
+++ tools/ubuntu_install.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+# this scrip will install all the required packages that you need on
+# ubuntu to compile and work with this package.
+
+import subprocess
+
+packs=[
+	'htop',
+]
+
+args=['sudo','apt-get','install']
+args.extend(packs)
+subprocess.check_call(args)
