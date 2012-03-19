63bd8f61a4379d9539c3c64fda3ec4e6dc3c168f
Mark Veltzer <mark.veltzer@gmail.com>
2012-03-19 17:34:29 +0200

    more code

---
+++ src/examples/performance/memcpy_comparison.cc
@@ -0,0 +1,47 @@
+#include <stdio.h> // for printf(3)
+#include <sys/time.h> // for gettimeofday(2)
+#include <stdlib.h> // for malloc(3)
+#include <string.h> // for malloc(3)
+
+#include "us_helper.hh" // for micro_diff
+
+/*
+ * This example compares memcpy(3) to copy by loop...
+ * TODO:
+ *
+ * 		Mark Veltzer
+ * EXTRA_LIBS=
+ */
+
+int main(int argc, char **argv, char** envp) {
+	struct timeval t1, t2;
+	const unsigned int loop=10000000;
+	const unsigned int size=200;
+	char* buf1=(char*)malloc(size);
+	char* buf2=(char*)malloc(size);
+	/*
+	if(rand()<RAND_MAX) {
+		buf1=NULL;
+	}
+	*/
+
+	printf("doing %d memcpy\n",loop);
+	CHECK_NOT_M1(gettimeofday(&t1, NULL));
+	for (unsigned int i = 0;i < loop;i++) {
+		memcpy(buf1,buf2,size);
+	}
+	CHECK_NOT_M1(gettimeofday(&t2, NULL));
+	printf("time in micro of one op: %lf\n", micro_diff(&t1,&t2)/(double)loop);
+
+	printf("doing %d copy int by int\n",loop);
+	CHECK_NOT_M1(gettimeofday(&t1, NULL));
+	for (unsigned int i = 0;i < loop;i++) {
+		for(unsigned int j=0;j<size/sizeof(int);j++) {
+			((int*)buf1)[j]=((int*)buf2)[j];
+		}
+	}
+	CHECK_NOT_M1(gettimeofday(&t2, NULL));
+	printf("time in micro of one op: %lf\n", micro_diff(&t1,&t2)/(double)loop);
+
+	return 0;
+}
