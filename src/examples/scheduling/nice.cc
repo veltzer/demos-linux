eb618732abcbbeb428930bbbbf63681f22ab99f6
Mark Veltzer <mark.veltzer@gmail.com>
2012-03-21 14:53:50 +0200

    more demos

---
+++ src/examples/scheduling/nice.cc
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/*
+ * This examples shows the effect of the nice scheduling system.
+ *
+ * It should fork two versions of heavy work each with a different nice level.
+ *
+ * References:
+ * man 2 nice
+ *
+ * 	Mark Veltzer
+
+int main(int argc,char** argv,char** envp) {
+	float sum;
+	for(unsigned int i=0;i<1000000;i++) {
+		for(unsigned int j=0;j<100000000;j++) {
+			sum+=j;
+		}
+		printf("i is %d\n",i);
+	}
+	return 0;
+}
