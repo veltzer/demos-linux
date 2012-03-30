70f8b409309d6b53cd6e15f2d7fa40e17c21776e
Mark Veltzer <mark@veltzer.net>
2012-03-30 23:35:40 +0300

    better exercise

--- src/exercises/cpp_threads/MyThread.cc
+++ src/exercises/cpp_threads/MyThread.cc
@@ -1,8 +1,11 @@
-#include <pthread.h>
-#include <iostream>
+#include <pthread.h> // for pthread_create(3), pthread_join(3)
 
 #include "MyThread.hh"
-#include "us_helper.hh"
+#include "us_helper.hh" // for CHECK_ZERO
+ 
+/*
+ * EXTRA_LIBS=-lpthread
+ */
 
 void* MyThread::realsvc(void* arg) {
 	MyThread* t=(MyThread*)arg;
@@ -19,4 +22,38 @@ void MyThread::start() {
 
 void MyThread::join() {
 	CHECK_ZERO(pthread_join(myid,NULL));
+}
+
+#include<iostream> // for std::cout, std::endl
+#include<unistd.h> // for sleep(3)
+
+#include "us_helper.hh" // for gettid()
+
+class ImpThread:public MyThread {
+	private:
+		int limit;
+		int sleep_time;
+
+	public:
+		ImpThread(int ilimit,int isleep_time):limit(ilimit),sleep_time(isleep_time) {}
+	protected:
+		virtual void svc() {
+			pid_t tid=gettid();
+			std::cout << "thread " << tid << " starting" << std::endl;
+			for(int i=0;i<limit;i++) {
+				std::cout << "Hello from thread " << tid << " num is " << i << std::endl;
+				sleep(sleep_time);
+			}
+			std::cout << "thread " << tid << " ending" << std::endl;
+		}
+};
+
+int main(int argc,char** argv,char** envp) {
+	ImpThread thr1(10,1);
+	ImpThread thr2(5,2);
+	thr1.start();
+	thr2.start();
+	thr1.join();
+	thr2.join();
+	return 0;
 }
