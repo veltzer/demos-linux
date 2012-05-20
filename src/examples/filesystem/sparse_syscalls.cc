9ef30ae640395525e21cbfa6557390da2aa0604a
Mark Veltzer <mark.veltzer@gmail.com>
2012-05-19 19:49:46 -0700

    removed perror from one of the examples

--- src/examples/filesystem/sparse_syscalls.cc
+++ src/examples/filesystem/sparse_syscalls.cc
@@ -24,38 +24,20 @@
 #include<sys/stat.h> // for open(2)
 #include<fcntl.h> // for open(2)
 #include<unistd.h> // for write(2), lseek(2), close(2)
-#include<stdlib.h> // for exit(3), EXIT_SUCCESS, EXIT_FAILURE
+#include<stdlib.h> // for EXIT_SUCCESS
+
+#include"us_helper.h" // for CHECK_NOT_M1()
 
 /*
 * This example explores sparse files on a UNIX system.
-*
-* TODO:
-* - use my CHECK macros here.
 */
 
 int main(int argc,char** argv,char** envp) {
+	int fd;
+	CHECK_NOT_M1(fd=open("sparse_file", O_CREAT | O_RDWR, 0666));
+	CHECK_NOT_M1(lseek(fd, 1000000, SEEK_CUR));
 	const char *buf = "hello";
-	int fd = open("sparse_file", O_CREAT | O_RDWR, 0666);
-	int res = fd;
-
-	if (res == -1) {
-		perror("error in open");
-		exit(EXIT_FAILURE);
-	}
-	res = lseek(fd, 1000000, SEEK_CUR);
-	if (res == -1) {
-		perror("error in seek");
-		exit(EXIT_FAILURE);
-	}
-	res = write(fd, buf, 5);
-	if (res == -1) {
-		perror("error in write");
-		exit(EXIT_FAILURE);
-	}
-	res = close(fd);
-	if (res == -1) {
-		perror("error in close");
-		exit(EXIT_FAILURE);
-	}
+	CHECK_NOT_M1(write(fd,buf,strlen(buf)));
+	CHECK_NOT_M1(close(fd));
 	return EXIT_SUCCESS;
 }
