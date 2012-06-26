644bee5686196dbc2b560928e1a8422120368d93
Mark Veltzer <mark@veltzer.net>
2012-06-26 15:09:07 +0600

    more code

---
+++ src/examples/performance/cache_line_pad.cc
@@ -0,0 +1,44 @@
+/*
+	This file is part of the linuxapi project.
+	Copyright (C) 2011, 2012 Mark Veltzer <mark.veltzer@gmail.com>
+
+	The linuxapi package is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Lesser General Public
+	License as published by the Free Software Foundation; either
+	version 2.1 of the License, or (at your option) any later version.
+
+	The linuxapi package is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+	Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public
+	License along with the GNU C Library; if not, write to the Free
+	Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+	02111-1307 USA.
+*/
+
+#include<firstinclude.h>
+#include<stdlib.h> // for EXIT_SUCCESS
+#include<stdio.h> // for printf(3)
+
+/*
+* This example shows how to pad a structure to cache line size
+*/
+
+typedef struct _barestruct {
+	int field1;
+	char foo;
+} barestruct;
+
+typedef struct _mystruct {
+	int field1;
+	char foo;
+	char padding[64-sizeof(barestruct)];
+} mystruct;
+
+int main(int argc,char** argv,char** envp) {
+	printf("sizeof(barestruct)=%d\n",sizeof(barestruct));
+	printf("sizeof(mystruct)=%d\n",sizeof(mystruct));
+	return EXIT_SUCCESS;
+}
