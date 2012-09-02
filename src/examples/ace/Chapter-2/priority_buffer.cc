3e4ded0c1fda65c3b34af63f358bc18058a65a04
Mark Veltzer <mark@veltzer.net>
2012-09-02 17:09:54 +0300

    more code

--- src/examples/ace/Chapter-2/priority_buffer.cc
+++ src/examples/ace/Chapter-2/priority_buffer.cc
@@ -35,9 +35,6 @@
 * EXTRA_CMDS=pkg-config --cflags --libs ACE
 */
 
-// Global thread manager.
-static ACE_Thread_Manager thr_mgr;
-
 // Make the queue be capable of being *very* large.
 static const long max_queue=LONG_MAX;
 
@@ -45,33 +42,27 @@ static const long max_queue=LONG_MAX;
 // the message to the stderr stream, and deletes the message. The
 // producer sends a 0-sized message to inform the consumer to stop
 // reading and exit.
-static void *consumer(ACE_Message_Queue<ACE_MT_SYNCH> *msg_queue) { // Keep looping, reading a message out of the queue, until we
+static void *consumer(ACE_Message_Queue<ACE_MT_SYNCH> *msg_queue) {
+	// Keep looping, reading a message out of the queue, until we
 	// timeout or get a message with a length == 0, which signals us to
 	// quit.
-
 	while(true) {
 		ACE_Message_Block *mb;
-
 		if(msg_queue->dequeue_head(mb) == -1) {
 			break;
 		}
-
 		size_t length=mb->length();
-
 		if(length>0) {
 			ACE_OS::puts(mb->rd_ptr());
 		}
-
 		// Free up the buffer memory and the Message_Block.
 		ACE_Allocator::instance()->free(mb->rd_ptr());
 		mb->release();
-
 		if(length==0) {
 			break;
 		}
 	}
-
-	return(0);
+	return(NULL);
 }
 
 
@@ -82,29 +73,21 @@ static void *consumer(ACE_Message_Queue<ACE_MT_SYNCH> *msg_queue) { // Keep loop
 // know when to exit.
 static void *producer(ACE_Message_Queue<ACE_MT_SYNCH> *msg_queue) {
 	ACE_Read_Buffer rb(ACE_STDIN);
-
 	// Keep reading stdin, until we reach EOF.
-
 	while(true) {
 		// Allocate a new buffer.
 		char *buffer=rb.read('\n');
-
 		ACE_Message_Block *mb;
-
 		if(buffer==0) {
 			// Send a 0-sized shutdown message to the other thread and
 			// exit.
-
 			ACE_NEW_RETURN(mb, ACE_Message_Block((size_t)0), 0);
-
 			if(msg_queue->enqueue_tail(mb)==-1) {
 				ACE_ERROR((LM_ERROR, "(%t) %p\n", "put_next"));
 			}
 			break;
-		}
-
-		// Enqueue the message in priority order.
-		else {
+		} else {
+			// Enqueue the message in priority order.
 			// Allocate a new message, but have it "borrow" its memory
 			// from the buffer.
 			ACE_NEW_RETURN(mb, ACE_Message_Block(rb.size(), ACE_Message_Block::MB_DATA, 0, buffer), 0);
@@ -120,23 +103,41 @@ static void *producer(ACE_Message_Queue<ACE_MT_SYNCH> *msg_queue) {
 			}
 		}
 	}
-
 	// Now read all the items out in priority order (i.e., ordered by
 	// the size of the lines!).
-	consumer(msg_queue);
-
-	return(0);
+	//consumer(msg_queue);
+	return(NULL);
 }
+
+// Global thread manager.
+static ACE_Thread_Manager thr_mgr;
 
 // Spawn off one thread that copies stdin to stdout in order of the
 // size of each line.
 int ACE_TMAIN(int argc,ACE_TCHAR** argv,ACE_TCHAR** envp) {
-	// Message queue.
+	// first way to run this app is this way:
+	/*
+	ACE_Message_Queue<ACE_MT_SYNCH> msg_queue(max_queue);
+	producer(&msg_queue);
+	*/
+	// second way to run this app - one thread in the background:
+	/*
 	ACE_Message_Queue<ACE_MT_SYNCH> msg_queue(max_queue);
 	if(thr_mgr.spawn(ACE_THR_FUNC(producer),(void *)&msg_queue,THR_NEW_LWP|THR_DETACHED)==-1) {
 		ACE_ERROR_RETURN((LM_ERROR, "%p\n", "spawn"), 1);
 	}
 	// Wait for producer and consumer threads to exit.
 	thr_mgr.wait();
+	*/
+	// third way: two threads in the background
+	ACE_Message_Queue<ACE_MT_SYNCH> msg_queue(max_queue);
+	if(thr_mgr.spawn(ACE_THR_FUNC(producer),(void *)&msg_queue,THR_NEW_LWP|THR_DETACHED)==-1) {
+		ACE_ERROR_RETURN((LM_ERROR, "%p\n", "spawn"), 1);
+	}
+	if(thr_mgr.spawn(ACE_THR_FUNC(consumer),(void *)&msg_queue,THR_NEW_LWP|THR_DETACHED)==-1) {
+		ACE_ERROR_RETURN((LM_ERROR, "%p\n", "spawn"), 1);
+	}
+	thr_mgr.wait();
+	// Wait for producer and consumer threads to exit.
 	return EXIT_SUCCESS;
 }
