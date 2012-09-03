3e955d4750fc7670eefff1e06973709bf69a2607
Mark Veltzer <mark.veltzer@gmail.com>
2012-09-03 10:33:06 +0300

    better solutions

---
+++ src/exercises/ace/Chapter-1/solution0.cc
@@ -0,0 +1,41 @@
+/*
+	This file is part of the linuxapi project.
+	Copyright (C) 2011, 2012 Mark Veltzer <mark.veltzer@gmail.com>
+
+	The linuxapi package is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Lesser General Public
+	License as published by the Free Software Foundation; either
+	version 2.1 of the License, or (at your option) any later version.
+
+	The linuxapi package is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+	Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public
+	License along with the GNU C Library; if not, write to the Free
+	Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+	02111-1307 USA.
+*/
+
+#include<firstinclude.h>
+#include<ace/Log_Msg.h> // for ACE_DEBUG and ACE_ERROR
+#include<ace/OS_NS_stdio.h> // for the printf function
+#include<ace/streams.h> // for C++ streams interface
+#include<stdlib.h> // for EXIT_SUCCESS
+
+/*
+* solution to the first exercise.
+*
+* EXTRA_CMDS=pkg-config --cflags --libs ACE
+*/
+
+// Notice the use of the ACE version of main
+int ACE_TMAIN(int argc,ACE_TCHAR** argv,ACE_TCHAR** envp) {
+	printf("It seems that printf is working\n");
+	cout << "It seems like C++ streams are working without std:: prefix" << endl;
+	// Notice that ACE_DEBUG takes one argument
+	ACE_DEBUG((LM_DEBUG, ACE_TEXT("ACE_DEBUG(pid=%P Time:%T)\n")));
+	ACE_ERROR((LM_ERROR, ACE_TEXT("ACE_ERROR(pid=%P File:%N Line:%l Date+Time:%D)\n")));
+	return EXIT_SUCCESS;
+}
