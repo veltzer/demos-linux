ac2c7e1de75614906de8e59d29351e5c1c4eccdd
Mark Veltzer <mark.veltzer@gmail.com>
2012-11-15 13:17:36 +0200

    minor changes

---
+++ src/examples/compiler_internals/step_over_const.cc
@@ -0,0 +1,40 @@
+/*
+	This file is part of the linuxapi project.
+	Copyright (C) 2011, 2012 Mark Veltzer <mark.veltzer@gmail.com>
+
+	The linuxapi package is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Lesser General Public
+	License as published by the Free Software Foundation; either
+	version 2.1 of the License, or (at your option) any later version.
+
+	The linuxapi package is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+	Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public
+	License along with the GNU C Library; if not, write to the Free
+	Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+	02111-1307 USA.
+*/
+
+#include<firstinclude.h>
+#include<stdio.h> // for printf(3)
+
+/*
+* This example proves that const data segment data is stored in a non write
+* section
+*/
+
+const int j=1024;
+
+int main(int argc,char** argv,char** envp) {
+	int* p=(int*)&j;
+	for(int i=0;i<1000;i++) {
+		if(i%10==0) {
+			(*p)++;
+		}
+		printf("j is %d\n",*p);
+	}
+	return 0;
+}
