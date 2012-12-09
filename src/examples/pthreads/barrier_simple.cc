a1582dd07e04522e47623532d9e5a7662fdab6f7
Mark Veltzer <mark.veltzer@gmail.com>
2012-12-09 22:47:58 +0200

    better barrier example

--- src/examples/pthreads/barrier_simple.cc
+++ src/examples/pthreads/barrier_simple.cc
@@ -19,70 +19,68 @@
 */
 
 #include <firstinclude.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h> // for EXIT_SUCCESS, EXIT_FAILURE
-#include <pthread.h>
-#include <unistd.h> // for sysconf(3)
+#include <stdlib.h> // for EXIT_SUCCESS, EXIT_FAILURE, rand(3)
+#include <pthread.h> // for pthread_t, pthread_attr_t, pthread_barrier_t, pthread_create(3), 
+#include <unistd.h> // for sysconf(3), usleep(3), sleep(3)
+#include <sched.h> // for cpu_set_t, CPU_ZERO(3), CPU_SET(3)
 #include <us_helper.h> // for CHECK_ZERO(), CHECK_ONEOFTWO(), TRACE(), print_cpu_set()
 
 /*
-* This is a demo which shows atomic add using the
-* __sync_add_and_fetch gcc function.
-* see
-* http://gcc.gnu.org/onlinedocs/gcc/Atomic-Builtins.html
-* for more details...
-*
-* The idea here is to try to break this function by having multiple
-* thread try to increment the counter at once.
+* This demo shows off pthread barriers which are a way to synchronize a number of threads.
 *
 * EXTRA_LIBS=-lpthread
-*
-* TODO: have each thread print on which cpu it is currently running.
 */
 
-// data to be passed to each thread...
 typedef struct _thread_data {
 	int num;
+	pthread_barrier_t* barrier;
 	int attempts;
-	int* value;
+	int max_sleep_time;
+	int min_sleep_time;
 } thread_data;
 
 static void *worker(void *p) {
 	thread_data* td=(thread_data*)p;
-	TRACE("start thread %d",td->num);
+	TRACE("start thread (%d)",td->num);
 	for(int i=0;i<td->attempts;i++) {
-		__sync_add_and_fetch(td->value, 1);
+		// block on the barrier
+		CHECK_ONEOFTWO(pthread_barrier_wait(td->barrier),0, PTHREAD_BARRIER_SERIAL_THREAD);
+		// give a chance for other thread to print that they stopped sleeping (hack)
+		//usleep(1000);
+		TRACE("doing work (%d)",td->num);
+		// sleep for a random time
+		sleep(rand()%td->max_sleep_time+td->min_sleep_time);
+		TRACE("stopped_sleeping (%d)",td->num);
 	}
 	TRACE("end thread %d",td->num);
 	return(NULL);
 }
 
 int main(int argc,char** argv,char** envp) {
-	if(argc<3) {
-		fprintf(stderr,"%s: usage: %s [attempts] [core] [core] [core..]\n",argv[0],argv[0]);
+	if(argc!=1) {
+		fprintf(stderr,"%s: usage: %s\n",argv[0],argv[0]);
 		return EXIT_FAILURE;
 	}
-	// find the number of cores
+	const int thread_num=10;
 	const int cpu_num=sysconf(_SC_NPROCESSORS_ONLN);
-	// the counter to be incremented by all threads
-	int counter=0;
-	int attempts=atoi(argv[1]);
-	const int thread_num=argc-2;
-	TRACE("cpu_num is %d",cpu_num);
-	TRACE("attempts is %d",attempts);
-	TRACE("thread_num is %d",thread_num);
+	const int attempts=10;
+	const int max_sleep_time=10;
+	const int min_sleep_time=2;
 	pthread_t* threads=new pthread_t[thread_num];
 	pthread_attr_t* attrs=new pthread_attr_t[thread_num];
 	thread_data* data=new thread_data[thread_num];
 	cpu_set_t* cpu_sets=new cpu_set_t[thread_num];
 	void** rets=new void*[thread_num];
+	pthread_barrier_t bar;
 
+	CHECK_ZERO(pthread_barrier_init(&bar, NULL, thread_num));
 	TRACE("start creating threads");
 	for(int i=0;i<thread_num;i++) {
 		data[i].num=i;
 		data[i].attempts=attempts;
-		data[i].value=&counter;
+		data[i].barrier=&bar;
+		data[i].max_sleep_time=max_sleep_time;
+		data[i].min_sleep_time=min_sleep_time;
 		CPU_ZERO(cpu_sets+i);
 		CPU_SET(i%cpu_num,cpu_sets+i);
 		//print_cpu_set(stderr,cpu_sets + i);
@@ -96,7 +94,7 @@ int main(int argc,char** argv,char** envp) {
 		CHECK_ZERO(pthread_join(threads[i],rets+i));
 	}
 	TRACE("joined threads");
-	TRACE("counter is %d and should be %d",counter,thread_num*attempts);
+	CHECK_ZERO(pthread_barrier_destroy(&bar));
 	TRACE("ended");
 	return EXIT_SUCCESS;
 }
