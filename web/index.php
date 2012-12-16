0a6936f2a19de85320ff10d4c84b057825c2ba67
Mark Veltzer <mark.veltzer@gmail.com>
2012-12-16 05:28:05 +0200

    better web page

--- web/index.php
+++ web/index.php
@@ -1,7 +1,15 @@
 <html>
+	<head>
+		<meta charset="UTF-8">
+		<title>LinuxAPI</title>
+		<link rel="shortcut icon" href="favicon.ico"/>
+		<meta name="Description" content="LinuxAPI is a project to demo and explore the Linux user space C/C++ API"/>
+		<meta name="Keywords" content="Mark Veltzer, veltzer, linux, API, C, C++, kernel, examples, samples, demos"/>
+	</head>
 	<body>
+		<h1>Welcome to the LinuxAPI web site</h1>
 		<p>
-		This is the web page for the linuxapi project.
+		This is the web page for the LinuxAPI project.
 		Read more about this project from it's
 		<a href="https://github.com/veltzer/linuxapi">github page</a>.
 		</p>
