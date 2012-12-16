403cfa59a5527a845158be502e1091c557e664bc
Mark Veltzer <mark@veltzer.net>
2012-12-16 04:32:40 +0200

    improved this example

--- src/kernel/printk_sysctl.cc
+++ src/kernel/printk_sysctl.cc
@@ -18,32 +18,29 @@
 	02111-1307 USA.
 */
 
+/*
+* This example shows how to control kernel logging level using a user space program.
+* You can also use /proc or sysctl via the command line to do the same thing
+*/
+
 #include <firstinclude.h>
 #include <stdio.h> // for printf(3), perror(3)
-#include <errno.h> // for perror(3)
 #include <sys/sysctl.h> // for sysctl(3)
 //#include <unistd.h> // for sysctl(2)
 //#include <linux/sysctl.h> // for sysctl(2)
-#include <stdlib.h> // for exit(3), EXIT_SUCCESS, EXIT_FAILURE
+#include <stdlib.h> // for EXIT_SUCCESS, EXIT_FAILURE
+#include <us_helper.h> // for CHECK_NOT_M1()
 
 #define SIZE(a) (sizeof(a)/sizeof((a)[0]))
 int name[]={CTL_KERN,KERN_PRINTK};
 int printk_params[4];
 int new_params[4];
 
-/*
-* This example shows how to control kernel logging level using a user space program.
-* You can also use /proc or sysctl via the command line to do the same thing
-*/
-
 int main(int argc, char** argv, char** envp) {
 	size_t paramlth=sizeof(printk_params);
 	if(argc==1) {
 		/* report */
-		if (sysctl(name, SIZE(name), printk_params, &paramlth, 0, 0)) {
-			perror("sysctl");
-			exit(EXIT_FAILURE);
-		}
+		CHECK_NOT_M1(sysctl(name, SIZE(name), printk_params, &paramlth, 0, 0));
 		printf("got %d bytes:\n", paramlth);
 		printf("console_loglevel: %d\n", printk_params[0]);
 		printf("default_message_loglevel: %d\n", printk_params[1]);
@@ -53,14 +50,11 @@ int main(int argc, char** argv, char** envp) {
 		for (int i=0;i<4;i++)
 			new_params[i]=atoi(argv[i+1]);
 		/* set */
-		if (sysctl(name, SIZE(name), 0, 0, new_params, sizeof(new_params))) {
-			perror("sysctl");
-			exit(EXIT_FAILURE);
-		}
+		CHECK_NOT_M1(sysctl(name, SIZE(name), 0, 0, new_params, sizeof(new_params)));
 		printf("set new printk parameters\n");
 	} else {
 		fprintf(stderr, "Call: %s [N N N N]\n", argv[0]);
-		exit(EXIT_FAILURE);
+		return EXIT_FAILURE;
 	}
 	return EXIT_SUCCESS;
 }
