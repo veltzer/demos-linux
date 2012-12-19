f71e97fb2f5fb5b1f64b82b4e3604c072caa62e0
Mark Veltzer <mark.veltzer@gmail.com>
2012-12-19 17:10:55 +0200

    more code

--- src/examples_standalone/making_so_executable/add.c
+++ src/examples_standalone/making_so_executable/add.c
@@ -22,6 +22,8 @@
 #include <stdio.h> // for printf(3)
 #include <unistd.h> // for write(2), _exit(2)
 #include <stdlib.h> // for EXIT_SUCCESS, EXIT_FAILURE
+#include <sys/syscall.h> // for syscall(2)
+#include <string.h> // for strlen(3)
 
 int func(int a, int b) {
 	return(a + b);
@@ -34,14 +36,68 @@ void do_init(void) {
 }
 */
 
-int my_entry(int argc,char** argv,char** envp) {
+
+//int main(int argc,char** argv,char** envp) __attribute__((weak,noreturn)); 
+//int main(int argc,char** argv,char** envp) __attribute__((weak)); 
+//int mymain(int argc,char** argv,char** envp) __attribute__((noreturn)); 
+int mymain(int argc,char** argv,char** envp) {
+	static const char buf_data[]="Hello world from the lib\n";
+	// this fails
+	//printf("hello from the lib...\n");
+	// this succeeds
 	/*
-	_exit(EXIT_FAILURE);
+	volatile int i=0;
+	while(1) {
+		i++;
+	}
+	*/
+	// lets try a write syscall to file descriptor 1...
+	//strcpy(mybuf.buf,buf_data);
+	const int len=strlen(buf_data);
+	const char* mybuf=buf_data;
+	long res;
+		// SYS_write is 4 (from /usr/include/i386-linux-gnu/asm/unistd_32.h)
+		// file descriptor is 1
+		// buffer
+		// size of buffer
+		// enter kernel mode
+	__asm__ volatile(								\
+		"int $0x80"								\
+		: "=a" (res)								\
+		: "0" (4),"b" ((long)(1)),"c" ((long)(mybuf+1)),			\
+		"d" ((long)(len))							\
+	);
+		// SYS_exit is 1 (from /usr/include/i386-linux-gnu/asm/unistd_32.h)
+		// argument is in ebx, it is 0
+		//"xor %ebx,%ebx\n\t"
+		// enter kernel mode
+	__asm__ volatile(								\
+		"mov $1,%eax\n\t"							\
+		"mov $7,%ebx\n\t"							\
+		"int $0x80\n\t"								\
+	);
+	// this works!!!
+	/*
+	asm(
+		// SYS_exit is 1 (from /usr/include/i386-linux-gnu/asm/unistd_32.h)
+		"mov $1,%eax;"
+		// argument is in ebx, it is 0
+		"xor %ebx,%ebx;"
+		// enter kernel mode
+		"int $0x80;"
+	);
+	*/
+	// call syscall using assembly code
+	// this fails
+	//syscall(SYS_exit);
+	// this fails
+	//_exit(EXIT_FAILURE);
+	/*
 	int ret=write(1,"hello\n",6);
 	if(ret!=6) {
 		_exit(EXIT_FAILURE);
 	}
-	//printf("hello from the lib...");
 	*/
+	// even this fails
 	return EXIT_SUCCESS;
 }
