3576ea56e575f3d6f373481f6543cf72c5b5f841
Mark Veltzer <mark.veltzer@gmail.com>
2013-02-09 20:09:20 +0200

    checking failings for pthreads

--- src/examples/test/fail_pthread_create.cc
+++ src/examples/test/fail_pthread_create.cc
@@ -19,14 +19,34 @@
 */
 
 #include <firstinclude.h>
+#include <pthread.h> // for pthread_create(3). pthread_attr_init(3), pthread_attr_setinheritsched(3), pthread_attr_setschedpolicy(3), pthread_attr_setschedparam(3), pthread_attr_t, pthread_t
 #include <stdlib.h> // for EXIT_SUCCESS
 #include <us_helper.h> // for CHECK_ZERO()
 
 /*
 * This program calls pthread_create and tries to fail on purpse to see the error
 * messages for this function.
+*
+* EXTRA_LINK_FLAGS=-lpthread
 */
 
+void* myfunc(void*) {
+	return NULL;
+}
+
 int main(int argc,char** argv,char** envp) {
+	no_params(argc,argv);
+
+	pthread_attr_t myattr;
+	pthread_attr_init(&myattr);
+	pthread_attr_setinheritsched(&myattr, PTHREAD_EXPLICIT_SCHED);
+	pthread_attr_setschedpolicy(&myattr, SCHED_FIFO);
+	struct sched_param myparam;
+	// this is the bad argument to pthread_create(3)
+	myparam.sched_priority=101;
+	pthread_attr_setschedparam(&myattr, &myparam);
+
+	pthread_t mythread;
+	CHECK_ZERO(pthread_create(&mythread, &myattr, myfunc, NULL));
 	return EXIT_SUCCESS;
 }
