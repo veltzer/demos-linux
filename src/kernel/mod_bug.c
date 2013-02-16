2fea997370ddaa27479dbaef86781b77c68cbded
Mark Veltzer <mark@veltzer.net>
2013-02-16 20:43:10 +0200

    started using checkpatch.pl for checking my code

--- src/kernel/mod_bug.c
+++ src/kernel/mod_bug.c
@@ -18,10 +18,10 @@
 	02111-1307 USA.
 */
 
-//#define DEBUG
-#include <linux/module.h> // for the MODULE_*, module_*
-//define DO_DEBUG
-#include "kernel_helper.h" // our own helper
+/* #define DEBUG */
+#include <linux/module.h> /* for the MODULE_*, module_* */
+/* define DO_DEBUG */
+#include "kernel_helper.h" /* our own helper */
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Mark Veltzer");
@@ -29,8 +29,10 @@ MODULE_DESCRIPTION("Module showing how to use BUG()");
 
 /*
 *	Take note of the following facts:
-*	- after a driver does a BUG() it cannot be unloaded (reboot will be needed).
-*	- BUG() generates a snipplet in the kernel log that can be used to report
+*	- after a driver does a BUG() it cannot be unloaded
+*	(reboot will be needed).
+*	- BUG() generates a snipplet in the kernel log that can be used to
+*	report
 *		bugs to kernel maintainers.
 *	- BUG() generates a stack trace that you can use for debugging.
 *
@@ -38,14 +40,16 @@ MODULE_DESCRIPTION("Module showing how to use BUG()");
 *	- examin the behaviour in each of these and describe it.
 *	- add open and ioctl support.
 */
-static int __init mod_init(void) {
+static int __init mod_init(void)
+{
 	BUG();
-	BUG_ON(1!=2);
+	BUG_ON(1 != 2);
 	panic("there is nothing I can do");
-	return(0);
+	return 0;
 }
 
-static void __exit mod_exit(void) {
+static void __exit mod_exit(void)
+{
 }
 
 module_init(mod_init);
