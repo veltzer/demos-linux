bee304692b89724ea7f4c268f3de0603f041a4a0
Mark Veltzer <mark@veltzer.net>
2013-02-23 16:09:16 +0200

    added comments to this example

--- src/examples/stl/modification_while_iterating.cc
+++ src/examples/stl/modification_while_iterating.cc
@@ -24,31 +24,82 @@
 #include <vector> // for std::list<T>, std::list<T>::iterator
 #include <stdlib.h> // for EXIT_SUCCESS, EXIT_FAILURE;
 
-int main(int argc,char** argv,char** envp) {
-	if(argc!=4) {
-		std::cerr << argv[0] << ": usage " << argv[0] << " [size] [postoact] [postoremove]" << std::endl;
-		return EXIT_FAILURE;
+/*
+* This example shows that doing modification on an STL data structure while
+* iterating it could produce quite interesting results which are hard to
+* predict or understand. It does not only depend on what it is that you
+* are trying to do but also on what data structure you are actually using:
+* list or vector. In this example we explore both of these and show that
+* there are differences in behaviour between them:
+* - vector dumps core when trying to remove elements which are not in it
+* using its 'erase' method.
+* - list forgives trying to remove elements which are not there using its
+* 'remove' method.
+* - vector is ok with removing the current element.
+* - list dumps core when removing the current element.
+* - many other weird side effects happend. Play around with the numbers and
+* you will see it.
+*
+* Note that all of these behaviours are particular to the GNU implementation
+* of STL and are not part of the STL standard. A different STL implementation
+* may yield totally different outcomes.
+*/
+
+static int listorvector,size,postoact,postoremove,numtoremove;
+
+void do_list() {
+	std::list<int> l;
+	for(int i=0;i<size;i++) {
+		l.push_back(i);
 	}
-	int size=atoi(argv[1]);
-	int postoact=atoi(argv[2]);
-	int postoremove=atoi(argv[3]);
-	//std::list<int> l;
+	// remove an element in a position already passed
+	std::list<int>::iterator i;
+	int counter=0;
+	for(i=l.begin();i!=l.end();i++) {
+		if(counter==postoact) {
+			for(int j=0;j<numtoremove;j++) {
+				l.remove(postoremove+j);
+			}
+		}
+		std::cout << "visiting " << *i << std::endl;
+		counter++;
+	}
+}
+
+void do_vector() {
 	std::vector<int> l;
 	for(int i=0;i<size;i++) {
 		l.push_back(i);
 	}
-
 	// remove an element in a position already passed
-	//std::list<int>::iterator i;
 	std::vector<int>::iterator i;
 	int counter=0;
 	for(i=l.begin();i!=l.end();i++) {
 		if(counter==postoact) {
-			//l.remove(postoremove);
-			l.erase(l.begin()+postoremove);
+			for(int j=0;j<numtoremove;j++) {
+				l.erase(l.begin()+postoremove);
+			}
 		}
 		std::cout << "visiting " << *i << std::endl;
 		counter++;
+	}
+}
+
+int main(int argc,char** argv,char** envp) {
+	if(argc!=6) {
+		std::cerr << argv[0] << ": usage: " << argv[0] << " [list or vector (1 for list)] [size] [postoact] [postoremove] [numtoremove]" << std::endl;
+		std::cerr << argv[0] << ": example: " << argv[0] << " 1 10 5 5 1" << std::endl;
+		return EXIT_FAILURE;
+	}
+	listorvector=atoi(argv[1]);
+	size=atoi(argv[2]);
+	postoact=atoi(argv[3]);
+	postoremove=atoi(argv[4]);
+	numtoremove=atoi(argv[5]);
+	if(listorvector) {
+		do_list();
+	} else {
+		do_vector();
 	}
 	return EXIT_SUCCESS;
 }
