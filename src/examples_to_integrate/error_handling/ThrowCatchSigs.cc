c109e2573a1c3216fdd20b49e3e7f26ecff77c04
Mark Veltzer <mark.veltzer@gmail.com>
2013-02-24 18:05:30 +0200

    uncrustify phase 2

--- src/examples_to_integrate/error_handling/ThrowCatchSigs.cc
+++ src/examples_to_integrate/error_handling/ThrowCatchSigs.cc
@@ -104,7 +104,7 @@ int main(int argc, char** argv, char** envp) {
 		std::cerr << "c is " << c << std::endl;
 		try {
 			doBadCode(c%2);
-		}catch(std::exception e)  {
+		}catch(std::exception e) {
 			std::cerr << "Got exception, lets continue anyway" << std::endl;
 		}
 	}
