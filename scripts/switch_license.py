57493e130486bf8bb323c60517e3a30b13a570c5
Mark Veltzer <mark@veltzer.net>
2013-02-24 18:23:22 +0200

    before big license replace

---
+++ scripts/switch_license.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+import sys # for sys.argv
+
+old_lic=open("support/old_license.txt").read()
+new_lic=open("support/license.txt").read()
+
+#print old_lic
+#print new_lic
+#sys.exit(0)
+
+for x in sys.argv[1:]:
+	in_f=open(x)
+	f=in_f.read()
+	in_f.close()
+	#print f, old_lic
+	if f.startswith(old_lic):
+		f=new_lic+f[len(old_lic):]
+		#print f
+		in_f=open(x,"wb")
+		in_f.write(f)
+		in_f.close()
+		print "file",x,"was license replaced..."
+	else:
+		print "file",x,"does not start with the right license"
