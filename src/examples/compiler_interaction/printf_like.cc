8efe5cde13202c77eca30d963d9ac9cb997991a9
Mark Veltzer <mark.veltzer@gmail.com>
2013-05-06 16:33:20 +0300

    more examples

--- src/examples/compiler_interaction/printf_like.cc
+++ src/examples/compiler_interaction/printf_like.cc
@@ -19,21 +19,28 @@
  */
 
 #include <firstinclude.h>
-#include <omp.h>// for #pragma omg parallel, omp_get_thread_num(3), omp_get_num_threads(3)
-#include <stdio.h>	// for printf(3)
+#include <stdarg.h>	// for va_start(3), va_end(3), va_list(3)
+#include <stdio.h>	// for vfprintf(3)
 #include <stdlib.h>	// for EXIT_SUCCESS
 
 /*
- * This is a minimal openmp program.
- * Notice that openmp automatically adjusts to the number of cores that you have.
- * You can ofcourse, change that (reduce or increase number of threads)
- *
- * EXTRA_COMPILE_FLAGS=-fopenmp
- * EXTRA_LINK_FLAGS=-fopenmp
+ * An exampple of using the compiler to inforce 'printf' like correct
+ * argument type passing...
  */
 
+void my_trace(const char* fmt, ...) __attribute__((format(printf, 1, 2)));
+
+void my_trace(const char* fmt, ...) {
+	va_list args;
+	va_start(args, fmt);
+	vfprintf(stderr, fmt, args);
+	va_end(args);
+}
+
 int main(int argc, char** argv, char** envp) {
-	#pragma omp parallel
-	printf("Hello from thread %d, nthreads %d\n", omp_get_thread_num(), omp_get_num_threads());
+	// this will not compile
+	//my_trace("2+2 is %d\n","4");
+	// this one does compile
+	my_trace("2+2 is %d\n",4);
 	return EXIT_SUCCESS;
 }
