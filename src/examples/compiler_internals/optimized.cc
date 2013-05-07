6b877776c23b447953d9f7ad1eba3fc0c36b582a
Mark Veltzer <mark@veltzer.net>
2013-05-07 23:45:35 +0300

    better optimized example

--- src/examples/compiler_internals/optimized.cc
+++ src/examples/compiler_internals/optimized.cc
@@ -19,23 +19,51 @@
  */
 
 #include <firstinclude.h>
-#include <stdio.h>	// for printf(3)
+#include <stdio.h>	// for printf(3), fopen(3)
 #include <stdlib.h>	// for EXIT_SUCCESS
+#include <us_helper.h>	// for my_system()
 
 /*
  * This example shows that the compiler is very aggresive on optimization.
  * It doesn't even unroll the loop! It simply calculates (1+99)*99/2=4950
  * and puts the value in the code.
  * Conclusion: DO NOT UNDERESTIMATE THE COMPILER!
+ *
+ * this is to make sure that source code interleaving in the disassembly works
+ * out well...
+ * EXTRA_COMPILE_FLAGS=-g3
  */
 
-int main(int argc, char** argv, char** envp) {
-	unsigned int sum __attribute__((unused))=0;
+FILE* out;
+// first lets try a loop without optimization
+#pragma GCC push_options
+#pragma GCC optimize ("O0")
+void f1() __attribute__((noinline));
+void f1() {
+	unsigned int sum1=0;
 	for(unsigned int i=0; i<100; i++) {
-		printf("i is %d\n", i);
-		sum+=i;
-		// __sync_synchronize();
+		sum1+=i;
 	}
-	printf("sum is %u\n", sum);
+	fprintf(out,"sum1 is %u\n", sum1);
+}
+#pragma GCC pop_options
+
+int main(int argc, char** argv, char** envp) {
+	out=fopen("/dev/null","w");
+	f1();
+	// now with optimization
+	unsigned int sum2=0;
+	for(unsigned int i=0; i<100; i++) {
+		sum2+=i;
+	}
+	fprintf(out,"sum2 is %u\n", sum2);
+	// now with printing
+	unsigned int sum3=0;
+	for(unsigned int i=0; i<100; i++) {
+		fprintf(out,"i is %d\n", i);
+		sum3+=i;
+	}
+	fprintf(out,"sum3 is %u\n", sum3);
+	my_system("objdump --disassemble --source %s", argv[0]);
 	return EXIT_SUCCESS;
 }
