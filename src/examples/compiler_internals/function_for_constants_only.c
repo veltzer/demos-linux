f5bb224c92b3100eb05d8c17fdd5cdd7421e6c49
Mark Veltzer <mark.veltzer@gmail.com>
2013-05-08 10:51:30 +0300

    some work on the examples

--- src/examples/compiler_internals/function_for_constants_only.c
+++ src/examples/compiler_internals/function_for_constants_only.c
@@ -20,11 +20,12 @@
 
 #include <firstinclude.h>
 #include <stdio.h>	// for printf(3)
+#include <stdlib.h>	// for atoi(3)
 
 /*
  * This example shows how to write a function that only works for constants.
  * This file is a C file because g++ does not have this feature yet.
- * A quick check in the internet shows that a patch to have this in g++ is on the way.
+ * A quick check on the internet shows that a patch to have this in g++ is on the way.
  * Notice that the trick is to create a compile time error and is based on returning
  * a void* instead of an int.
  */
@@ -32,27 +33,35 @@ static inline int _sum(int i) {
 	return(i *(i + 1) / 2);
 }
 
-#define sum(i) \
+#define sum1(i) \
 	__builtin_choose_expr( \
 	__builtin_constant_p(i), \
 	_sum(i), \
-	(void*)0)
+	(void)0)
+
+#define sum2(i) \
+	(__builtin_constant_p(i) ? _sum(i) : 0 )
 
 /*
  * This inline version does not work because of bug in gcc whereby __builtin_constant_p(i) is not a constant
  */
 
-/*
- * static __attribute__((always_inline)) int sum(int i) {
- *      return __builtin_choose_expr(__builtin_constant_p(i),_sum(i),(void*)0);
- * }
- */
+static inline int sum3(int i) {
+	return __builtin_constant_p(i) ? _sum(i) : 0;
+}
 
 int main(int argc, char** argv, char** envp) {
-	printf("sum for 100 is %d\n", sum(100));
-	printf("sum for 5 is %d\n", sum(5));
-	// these next 2 lines will create a compile time error...
-	// volatile int j=25;
-	// printf("sum for j is %d\n",sum(j));
+	// everything is ok for constants
+	printf("sum1 for 100 is %d\n", sum1(100));
+	printf("sum2 for 5 is %d\n", sum2(5));
+	printf("sum3 for 7 is %d\n", sum3(7));
+	// we make sure that this is indeed a variable...:)
+	int j=atoi("25");
+	// only this creates a compile time error
+	//printf("sum1 for j is %d\n",sum1(j));
+	// this one just returns bad value
+	printf("sum2 for j is %d\n",sum2(j));
+	// this one just returns bad value
+	printf("sum3 for j is %d\n",sum3(j));
 	return(0);
 }
