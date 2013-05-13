be6efe6a42ecab5c2ebd0ad39a376dc25083d7b1
Mark Veltzer <mark.veltzer@gmail.com>
2013-05-13 20:23:28 +0300

    factored out common code

--- src/examples/multi_processing/vfork.cc
+++ src/examples/multi_processing/vfork.cc
@@ -28,6 +28,7 @@
 #include <sys/types.h>	// for vfork(2)
 #include <unistd.h>	// for vfork(2)
 #include <us_helper.h>	// for CHECK_NOT_M1()
+#include <multi_processing.h> // for print_code(), print_status()
 
 /*
  * An example of using vfork(2)
@@ -42,38 +43,6 @@
  * for sure.
  * - the results of using vfork incorrectly (not for exec) are indeed undefined as the manual page says.
  */
-void print_status(int status) {
-	if (WIFEXITED(status)) {
-		TRACE("child exited normally with status %d", WEXITSTATUS(status));
-	}
-	if (WIFSTOPPED(status)) {
-		TRACE("child was stopped with signal %s", strsignal(WSTOPSIG(status)));
-	}
-	if (WIFSIGNALED(status)) {
-		TRACE("child was signaled with signal %s", strsignal(WTERMSIG(status)));
-	}
-}
-
-void print_code(int code) {
-	switch (code) {
-	case CLD_EXITED:
-		TRACE("child exited of it's own accord");
-		break;
-
-	case CLD_KILLED:
-		TRACE("child was killed");
-		break;
-
-	case CLD_STOPPED:
-		TRACE("child was stopped");
-		break;
-
-	case CLD_CONTINUED:
-		TRACE("child was continued");
-		break;
-	}
-}
-
 int global_data=42;
 
 int main(int argc, char** argv, char** envp) {
---
+++ src/include/multi_processing.h
@@ -0,0 +1,60 @@
+/*
+ * This file is part of the linuxapi project.
+ * Copyright (C) 2011-2013 Mark Veltzer <mark.veltzer@gmail.com>
+ *
+ * The linuxapi package is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * The linuxapi package is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with the GNU C Library; if not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307 USA.
+ */
+
+#ifndef __multi_processing_h
+#define __multi_processing_h
+
+#include <us_helper.h>
+
+/* THIS IS A C FILE, NO C++ here */
+
+static inline void print_status(int status) {
+	if (WIFEXITED(status)) {
+		TRACE("child exited normally with status %d", WEXITSTATUS(status));
+	}
+	if (WIFSTOPPED(status)) {
+		TRACE("child was stopped with signal %s", strsignal(WSTOPSIG(status)));
+	}
+	if (WIFSIGNALED(status)) {
+		TRACE("child was signaled with signal %s", strsignal(WTERMSIG(status)));
+	}
+}
+
+static inline void print_code(int code) {
+	switch (code) {
+	case CLD_EXITED:
+		TRACE("child exited of it's own accord");
+		break;
+
+	case CLD_KILLED:
+		TRACE("child was killed");
+		break;
+
+	case CLD_STOPPED:
+		TRACE("child was stopped");
+		break;
+
+	case CLD_CONTINUED:
+		TRACE("child was continued");
+		break;
+	}
+}
+
+#endif /* !__us_helper_h */
