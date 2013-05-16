730ff05de7892202ac4f347e1b1c37a21003a0b8
Mark Veltzer <mark.veltzer@gmail.com>
2013-05-16 15:22:33 +0300

    more 64bit fixes

--- src/examples_standalone/embed_files_in_binary/main.cc
+++ src/examples_standalone/embed_files_in_binary/main.cc
@@ -19,15 +19,38 @@
  */
 
 #include <firstinclude.h>
-#include <stdio.h>	// for putchar(3)
+#include <stdio.h>	// for putchar(3), printf(3)
 #include <stdlib.h>	// for EXIT_SUCCESS
+#include <us_helper.h>	// for CHECK_NOT_M1()
+#include <sys/types.h>	// for open(2)
+#include <sys/stat.h>	// for open(2)
+#include <fcntl.h>	// for open(2)
+
+/*
+ * This example shows how to read bindy data from an attached object file.
+ *
+ * TODO:
+ * - take the code here that reads a whole file and add it to my utils.
+ */
 
 extern "C" {
-extern char _binary_data_txt_start;
-extern char _binary_data_txt_end;
+	extern char _binary_data_txt_start;
+	extern char _binary_data_txt_end;
 }
 
 int main(int argc, char** argv, char** envp) {
+	const size_t bufsize=16384;
+	char buffer[bufsize];
+	int fd=CHECK_NOT_M1(open("data.txt",O_RDONLY));
+	char* ptr=buffer;
+	size_t left=bufsize;
+	ssize_t read_size=CHECK_NOT_M1(read(fd,ptr,left));
+	while(read_size>0) {
+		ptr+=read_size;
+		left-=read_size;
+		read_size=CHECK_NOT_M1(read(fd,ptr,left));
+	}
+	printf("The next line should say [%s]\n",buffer);
 	char *p=&_binary_data_txt_start;
 	while(p!=&_binary_data_txt_end)
 		putchar(*p++);
