caaad88ea1092333c2b8c6f825966ababd6429ec
Mark Veltzer <mark.veltzer@gmail.com>
2013-06-07 15:59:11 +0300

    added a select(2) based file copy example

--- src/examples/io/zero_copy/copy_file_select.cc
+++ src/examples/io/zero_copy/copy_file_select.cc
@@ -36,30 +36,147 @@
  * the buffer in a circular way. No need for any synchronization in this implementation
  * since only one thread is doing all the work.
  *
+ * The advantage of this implementation over a simple read and write implementation is that
+ * requests for read(2) and write(2) could be issued by this app * at the same time *.
+ * This is especially true for read(2) calls which are synchroneous and blocking. In the
+ * simple read+write implementation a write will never be issued while a read is runnig
+ * which in this implementation we could issue a write while a read is going on. This is
+ * especially useful when you are copying files between two different hard disks.
+ *
  * TODO:
  * - implement this example.
  */
 
+class Pipe {
+	private:
+		char* buf;
+		size_t size;
+		size_t pos_read;
+		size_t pos_write;
+	public:
+		inline Pipe(const size_t isize) {
+			size=isize;
+			pos_read=0;
+			pos_write=0;
+			buf=(char*)CHECK_NOT_NULL(malloc(isize));
+		}
+		inline ~Pipe() {
+			free(buf);
+		}
+		/* return the empty room of a pipe */
+		inline size_t room() {
+			if (pos_read <= pos_write)
+				return size-pos_write+pos_read-1;
+			else
+				return pos_read-pos_write-1;
+		}
+		/* return the occupied room of a pipe */
+		inline size_t data() {
+			if (pos_read <= pos_write)
+				return pos_write-pos_read;
+			else
+				return size-pos_read+pos_write;
+		}
+		inline bool haveData() {
+			return data()>0;
+		}
+		inline bool haveRoom() {
+			return room()>0;
+		}
+		inline bool canRead() {
+			return haveRoom();
+		}
+		inline bool canWrite() {
+			return haveData();
+		}
+		/* read data into the buffer (at pos_write) */
+		inline bool doRead(int fd) {
+			ssize_t len;
+			if (pos_read <= pos_write) {
+				len=CHECK_NOT_M1(read(fd,buf+pos_write,size-pos_write));
+			} else {
+				len=CHECK_NOT_M1(read(fd,buf+pos_write,pos_write-pos_read));
+			}
+			pos_write+=len;
+			pos_write%=size;
+			return len==0;
+		}
+		/* write data from the buffer (at pos_read) */
+		inline void doWrite(int fd) {
+			ssize_t len;
+			if (pos_read <= pos_write) {
+				len=CHECK_NOT_M1(write(fd,buf+pos_read,pos_write-pos_read));
+			} else {
+				len=CHECK_NOT_M1(write(fd,buf+pos_read,size-pos_read));
+			}
+			pos_read+=len;
+			pos_read%=size;
+		}
+};
+
+class Selector {
+	private:
+		fd_set rfds;
+		fd_set wfds;
+		int fd_max;
+	public:
+		inline Selector() {
+		}
+		inline void null() {
+			FD_ZERO(&rfds);
+			FD_ZERO(&wfds);
+			fd_max=0;
+		}
+		inline void addReadFd(int fd) {
+			FD_SET(fd, &rfds);
+			if(fd>fd_max) {
+				fd_max=fd;
+			}
+		}
+		inline void addWriteFd(int fd) {
+			FD_SET(fd, &wfds);
+			if(fd>fd_max) {
+				fd_max=fd;
+			}
+		}
+		inline bool isReadActive(int fd) {
+			return FD_ISSET(fd,&rfds);
+		}
+		inline bool isWriteActive(int fd) {
+			return FD_ISSET(fd,&wfds);
+		}
+		inline void doSelect() {
+			CHECK_NOT_M1(select(fd_max+1, &rfds, &wfds, NULL, NULL));
+		}
+};
+
 void copy_file(const char* filein, const char* fileout, const unsigned int bufsize) {
-	char buf[bufsize];
+	Pipe p(bufsize);
 	int fdin=CHECK_NOT_M1(open(filein, O_RDONLY|O_LARGEFILE, 0666));
 	int fdout=CHECK_NOT_M1(open(fileout, O_WRONLY|O_CREAT|O_TRUNC|O_LARGEFILE, 0666));
-	// this is the main copy loop
-	// we go out of the loop because of error or eof
-	// >0: would have kept us in the loop
-	// =0: that is ok - it is end of file
-	// -1: error
-	ssize_t read_bytes;
+	Selector s;
+	bool eof=false;
 	do {
-		read_bytes=CHECK_NOT_M1(read(fdin, buf, bufsize));
-		char* p=buf;
-		ssize_t len=read_bytes;
-		while(len>0) {
-			size_t written_bytes=CHECK_NOT_M1(write(fdout, p, len));
-			len-=written_bytes;
-			p+=written_bytes;
+		s.null();
+		if(p.canRead() && !eof) {
+			s.addReadFd(fdin);
 		}
-	} while(read_bytes>0);
+		if(p.canWrite()) {
+			s.addWriteFd(fdout);
+		}
+		s.doSelect();
+		// It doesn't matter who we handle first: if we handle the read first then
+		// we will have more to write. If we handle the write first then we will
+		// have more room to read into.
+		if (s.isReadActive(fdin)) {
+			if(p.doRead(fdin)) {
+				eof=true;
+			}
+		}
+		if (s.isWriteActive(fdout)) {
+			p.doWrite(fdout);
+		}
+	} while (!eof || p.canWrite());
 	CHECK_NOT_M1(close(fdin));
 	CHECK_NOT_M1(close(fdout));
 }
