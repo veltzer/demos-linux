33471a21e906b7e7562afd9c9ab6a78551e36a37
Mark Veltzer <mark.veltzer@gmail.com>
2013-06-08 12:56:37 +0300

    more examples

---
+++ src/examples/syscalls/hello_world_using_write.cc
@@ -0,0 +1,33 @@
+/*
+ * This file is part of the linuxapi project.
+ * Copyright (C) 2011-2013 Mark Veltzer <mark.veltzer@gmail.com>
+ *
+ * The linuxapi package is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * The linuxapi package is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with the GNU C Library; if not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307 USA.
+ */
+
+#include <firstinclude.h>
+#include <unistd.h>	// for write(2)
+#include <us_helper.h>	// for CHECK_NOT_M1()
+
+/*
+ * This is an example of how to do the classic "Hello, World!\n"
+ * program using the write(2) system call...
+ */
+
+int main(int argc, char** argv, char** envp) {
+	CHECK_NOT_M1(write(1,"Hello, World!\n",14));
+	return EXIT_SUCCESS;
+}
