9e53fc7821efdf0107d8687b97bda34a1ce2e09e
Mark Veltzer <mark.veltzer@gmail.com>
2013-06-09 13:53:21 +0300

    solution to one of the exercises

---
+++ src/exercises/mutex_parametrized/mymutex.hh
@@ -0,0 +1,67 @@
+/*
+ * This file is part of the linuxapi project.
+ * Copyright (C) 2011-2013 Mark Veltzer <mark.veltzer@gmail.com>
+ *
+ * linuxapi is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * linuxapi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with linuxapi. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __mymutex_h
+#define __mymutex_h
+
+#include <firstinclude.h>
+#include <pthread.h>	// for pthread_mutex_init(3), pthread_mutex_destroy(3)
+#include <us_helper.h>	// for CHECK_ZERO_ERRNO()
+
+class MT_SYNCH {
+};
+class MT_NOSYNCH {
+};
+
+template<typename T> class MyMutex {
+};
+
+template<> class MyMutex<MT_SYNCH> {
+private:
+	pthread_mutex_t mylock;
+
+public:
+	MyMutex() {
+		CHECK_ZERO_ERRNO(pthread_mutex_init(&mylock, NULL));
+	}
+	~MyMutex() {
+		CHECK_ZERO_ERRNO(pthread_mutex_destroy(&mylock));
+	}
+	void lock() {
+		CHECK_ZERO_ERRNO(pthread_mutex_lock(&mylock));
+	}
+	void unlock() {
+		CHECK_ZERO_ERRNO(pthread_mutex_unlock(&mylock));
+	}
+};
+
+template<> class MyMutex<MT_NOSYNCH> {
+private:
+
+public:
+	MyMutex() {
+	}
+	~MyMutex() {
+	}
+	void lock() {
+	}
+	void unlock() {
+	}
+};
+
+#endif	// __mymutex_h
---
+++ src/exercises/mutex_parametrized/usage.cc
@@ -0,0 +1,71 @@
+/*
+ * This file is part of the linuxapi project.
+ * Copyright (C) 2011-2013 Mark Veltzer <mark.veltzer@gmail.com>
+ *
+ * linuxapi is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * linuxapi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with linuxapi. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <firstinclude.h>
+#include <unistd.h>	// for sleep(3)
+#include <pthread.h>	// for pthread_create(3), pthread_join(3)
+#include <us_helper.h>	// for TRACE(), CHECK_ZERO_ERRNO()
+#include "mymutex.hh"	// for MyMutex<T>::Object
+
+/*
+ * This demos pthread mutex usage.
+ * Note that these are actually futexes.
+ *
+ * EXTRA_LINK_FLAGS=-lpthread
+ */
+
+static MyMutex<MT_SYNCH> mylock;
+
+static void *worker(void *p) {
+	unsigned int id=*(unsigned int*)p;
+	int counter=0;
+	while(counter<10) {
+		TRACE("%d before lock", id);
+		mylock.lock();
+		CHECK_ZERO(sleep(1));
+		// this simulates a bug. see what this thread is doing using
+		// strace on the process or thread id.
+		if(id==1 && counter==10) {
+			CHECK_ZERO(sleep(1000));
+		}
+		mylock.unlock();
+		counter++;
+		TRACE("%d after lock", id);
+		CHECK_ZERO(sleep(1));
+	}
+	return(NULL);
+}
+
+int main(int argc, char** argv, char** envp) {
+	TRACE("initialized the lock...");
+	const unsigned int num=2;
+	pthread_t threads[num];
+	unsigned int ids[num];
+	void* rets[num];
+	TRACE("starting threads...");
+	for(unsigned int i=0; i<num; i++) {
+		ids[i]=i;
+		CHECK_ZERO_ERRNO(pthread_create(threads + i, NULL, worker, ids + i));
+	}
+	TRACE("finished creating threads, joining them...");
+	for(unsigned int i=0; i<num; i++) {
+		CHECK_ZERO_ERRNO(pthread_join(threads[i], rets + i));
+	}
+	TRACE("joined all threads, destroying the lock...");
+	return EXIT_SUCCESS;
+}
