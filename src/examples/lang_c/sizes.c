ee34a3e51dd4b21bd6ac0b1c49336a8d0f87dba6
Mark Veltzer <mark.veltzer@gmail.com>
2013-06-18 20:25:02 +0300

    factored out common code

--- src/examples/lang_c/sizes.c
+++ src/examples/lang_c/sizes.c
@@ -21,25 +21,40 @@
 #include <sys/utsname.h>// for uname(2)
 #include <stdbool.h>	// for bool
 #include <stdlib.h>	// for EXIT_SUCCESS
-#include <us_helper.h>	// for CHECK_NOT_M1(), __stringify()
+#include <us_helper.h>	// for CHECK_NOT_M1(), __stringify(), PRINT_SIZEOF()
 
 /*
- * This is a demo showing the sizes of variables on the architecture
+ * This is a demo showing the sizes of basic types on the architecture
  * it is running on (it also prints the name of the architecture).
  *
  * This is intended for a GNU/linux system.
  *
+ * Notes:
+ * - bool is just a macro for _Bool as the print clearly shows.
+ *
  * TODO:
- * Show the results inside this source code for 32bit and 64bit linux.
+ * - Show the results inside this source code for 32bit and 64bit linux.
  */
 
-#define PRINT_SIZEOF(type) printf("size of " __stringify(type) " is %zd\n", sizeof(type))
+typedef struct _struct_empty {
+} struct_empty;
+typedef struct _struct_emptyarray {
+	char a[0];
+} struct_emptyarray;
+typedef struct _struct_char {
+	char c;
+} struct_char;
+typedef struct _struct_charintchar {
+	char c1;
+	int i;
+	char c2;
+} struct_charintchar;
 
 int main(int argc, char** argv, char** envp) {
 	struct utsname buf;
-
 	CHECK_NOT_M1(uname(&buf));
 	printf("architecture is [%s]\n", buf.machine);
+
 	PRINT_SIZEOF(bool);
 	PRINT_SIZEOF(char);
 	PRINT_SIZEOF(unsigned char);
@@ -54,22 +69,15 @@ int main(int argc, char** argv, char** envp) {
 	PRINT_SIZEOF(float);
 	PRINT_SIZEOF(double);
 	// this is not allowed in C
-	// float* x={ 5, 3, 2, 1 };
+	// float* fourfloatarray={ 5, 3, 2, 1 };
 	// but this is...
-	float x[]={ 5, 3, 2, 1 };
-	printf("size of x is %zd\n", sizeof(x));
-	typedef struct _empty {
-	} empty;
-	PRINT_SIZEOF(empty);
-	typedef struct _array {
-		char a[0];
-	} array;
-	PRINT_SIZEOF(array);
-	typedef struct _charintchar {
-		char c1;
-		int i;
-		char c2;
-	} charintchar;
-	PRINT_SIZEOF(charintchar);
+	float fourfloatarray[]={ 5, 3, 2, 1 };
+	printf("size of fourfloatarray is %zd\n", sizeof(fourfloatarray));
+	float zerofloatarray[]={};
+	printf("size of zerofloatarray is %zd\n", sizeof(zerofloatarray));
+	PRINT_SIZEOF(struct_empty);
+	PRINT_SIZEOF(struct_emptyarray);
+	PRINT_SIZEOF(struct_char);
+	PRINT_SIZEOF(struct_charintchar);
 	return EXIT_SUCCESS;
 }
