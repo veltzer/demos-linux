d38067d6ffd5adb512c1642276bcfa156be55740
Mark Veltzer <mark.veltzer@gmail.com>
2013-06-23 16:35:09 +0300

    got ridd of __BASE_FILE__

--- src/examples/logging/combine.cc
+++ src/examples/logging/combine.cc
@@ -42,16 +42,13 @@
 
 const bool log_stderr=true;
 
-inline void my_trace(int level, const char* file, const char* base_file, const int line, const char* func, const char* function, const char* pretty_function, const char* fmt, ...) {
+inline void my_trace(int level, const char* file, const int line, const char* func, const char* fmt, ...) {
 	if(log_stderr) {
 		// the current implemention is quite simplistic...
 		fprintf(stderr, "level is %d\n", level);
 		fprintf(stderr, "file is %s\n", file);
-		fprintf(stderr, "base_file is %s\n", base_file);
 		fprintf(stderr, "line is %d\n", line);
 		fprintf(stderr, "func is %s\n", func);
-		fprintf(stderr, "function %s\n", function);
-		fprintf(stderr, "pretty_function is %s\n", pretty_function);
 		// now lets print the actual message...
 		va_list args;
 		va_start(args, fmt);
@@ -61,11 +58,8 @@ inline void my_trace(int level, const char* file, const char* base_file, const i
 		// the current implemention is quite simplistic...
 		syslog(level, "level is %d\n", level);
 		syslog(level, "file is %s\n", file);
-		syslog(level, "base_file is %s\n", base_file);
 		syslog(level, "line is %d\n", line);
 		syslog(level, "func is %s\n", func);
-		syslog(level, "function %s\n", function);
-		syslog(level, "pretty_function is %s\n", pretty_function);
 		// now lets print the actual message...
 		va_list args;
 		va_start(args, fmt);
@@ -73,11 +67,11 @@ inline void my_trace(int level, const char* file, const char* base_file, const i
 		va_end(args);
 	}
 }
-inline void my_trace(int level, const char* file, const char* base_file, const int line, const char* func, const char* function, const char* pretty_function, const char* fmt, ...) __attribute__((format(printf, 8, 9)));
+inline void my_trace(int level, const char* file, const int line, const char* func, const char* fmt, ...) __attribute__((format(printf, 5, 6)));
 
 // now wrap this up to get the right compiler macros...
 // this is a pre-processor varargs type of tracer...
-#define MY_TRACE(level, fmt, args ...) my_trace(level, __FILE__, __BASE_FILE__, __LINE__, __func__, __FUNCTION__, __PRETTY_FUNCTION__, fmt, ## args)
+#define MY_TRACE(level, fmt, args ...) my_trace(level, __FILE__, __LINE__, __func__, fmt, ## args)
 
 class A {
 public:
--- src/kernel/kernel_helper.h
+++ src/kernel/kernel_helper.h
@@ -21,18 +21,21 @@
 
 // do we want debug information ?!?
 //#define DO_DEBUG
+
+// Priting macros. If any of these spans more than one statement it should
+// be surrounded with do { ... } while(0)
 // TODO - the macros here could (and should?!?) be turned into inline functions...
 
 #ifdef DO_DEBUG
-#define PR_DEBUG(fmt, args...) printk(KERN_DEBUG "DEBUG %s %s %s %d: " fmt "\n", THIS_MODULE->name, __BASE_FILE__, __FUNCTION__, __LINE__, ## args)
+#define PR_DEBUG(fmt, args...) printk(KERN_DEBUG "DEBUG %s %s %s %d: " fmt "\n", THIS_MODULE->name, __FILE__, __FUNCTION__, __LINE__, ## args)
 #else
 #define PR_DEBUG(fmt, args...)
 #endif
 // errors are always shown
-#define PR_ERROR(fmt, args...) printk(KERN_ERR "ERROR %s %s %s %d: " fmt "\n", THIS_MODULE->name, __BASE_FILE__, __FUNCTION__, __LINE__, ## args)
+#define PR_ERROR(fmt, args...) printk(KERN_ERR "ERROR %s %s %s %d: " fmt "\n", THIS_MODULE->name, __FILE__, __FUNCTION__, __LINE__, ## args)
 
 // prints are always shown
-#define PR_INFO(fmt, args...) printk(KERN_INFO "INFO %s %s %s %d: " fmt "\n", THIS_MODULE->name, __BASE_FILE__, __FUNCTION__, __LINE__, ## args)
+#define PR_INFO(fmt, args...) printk(KERN_INFO "INFO %s %s %s %d: " fmt "\n", THIS_MODULE->name, __FILE__, __FUNCTION__, __LINE__, ## args)
 
 static inline int memcheck(void *buf, char val, unsigned int size) {
 	unsigned int i;
