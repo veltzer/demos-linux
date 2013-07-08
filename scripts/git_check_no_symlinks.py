04e26422b0422eeec3be7ab73456ee99149dab91
Mark Veltzer <mark@veltzer.net>
2013-07-08 14:35:16 +0300

    got ridd of sym links

---
+++ scripts/git_check_no_symlinks.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+This script runs "git ls-files" and checks that no files
+are symbolic links
+"""
+
+import subprocess # for check_output
+import os.path # for islink
+
+process = subprocess.Popen(['git','ls-files'], stdout=subprocess.PIPE)
+for line in process.stdout:
+	current=line.decode('utf-8').rstrip()
+	if os.path.islink(current):
+		raise ValueError("found a symlink (this is bad) "+current)
