feed7f5f8dea06ade7bd83e29375a2763ebe0c22
Mark Veltzer <mark.veltzer@gmail.com>
2013-07-11 16:29:32 +0300

    improved the read_a_lot.cc example

--- src/examples/filesystem/read_a_lot.cc
+++ src/examples/filesystem/read_a_lot.cc
@@ -17,38 +17,50 @@
  */
 
 #include <firstinclude.h>
-#include <stdio.h>	// for fprintf(3), fopen(3)
-#include <stdlib.h>	// for malloc(3), EXIT_SUCCESS, EXIT_FAILURE
-#include <unistd.h>	// for pause(2), getpid(2), sleep(3), read(2)
-#include <sys/types.h>	// for getpid(2), open(2)
-#include <sys/mman.h>	// for mprotect(2)
+#include <stdio.h>	// for printf(3), fprintf(3), stderr
+#include <stdlib.h>	// for EXIT_SUCCESS, EXIT_FAILURE, atoi(3)
+#include <string.h>	// for strlen(3)
+#include <unistd.h>	// for sleep(3), read(2), write(2), fsync(2)
+#include <sys/types.h>	// for open(2)
 #include <sys/stat.h>	// for open(2)
 #include <fcntl.h>	// for open(2)
-#include <us_helper.h>	// for CHECK_NOT_M1(), CHECK_INT(), CHECK_ZERO()
+#include <us_helper.h>	// for CHECK_NOT_M1(), CHECK_INT(), CHECK_ZERO(), INFO()
 
 /*
  * This example shows that you can continue to use a file after it has
  * been removed from the hard disk. So: as long as a process is using
  * a file its content will not be deleted from the hard drive.
  *
- * You must create the /tmp/myfile.data file, make sure it has
- * 3 bytes and a number int. Remove it as this process runs to
- * see it in action.
+ * The example creates a file, puts content to it and proceeds to read
+ * the content every second. While this is going on you can remove
+ * the file from the command line and see the program continue to
+ * run.
  */
 
 int main(int argc, char** argv, char** envp) {
-	int fd=CHECK_NOT_M1(open("/tmp/myfile.data", O_RDONLY));
-	const unsigned int readsize=3;
-	char buffer[1024];
+	if(argc!=3) {
+		fprintf(stderr, "%s: usage: %s [filename] [content]\n", argv[0], argv[0]);
+		fprintf(stderr, "%s: example: %s /tmp/foo mycontent\n", argv[0], argv[0]);
+		return EXIT_FAILURE;
+	}
+	// parameters
+	const char* filename=argv[1];
+	const char* content=argv[2];
+	size_t len=strlen(content);
+	INFO("creating file [%s]", filename);
+	int fd=CHECK_NOT_M1(open(filename, O_RDWR));
+	CHECK_INT(write(fd, content, len), len);
+	CHECK_NOT_M1(fsync(fd));
+	INFO("file [%s] is ready, see it, and delete it while I am running...", filename);
+	char buffer[len+1];
+	buffer[len]='\0';
 	unsigned int counter=0;
 	while(true) {
-		CHECK_INT(read(fd, buffer, sizeof(buffer)), readsize);
-		int a;
-		CHECK_INT(sscanf(buffer, "%d", &a), 1);
-		fprintf(stderr, "number is %d, counter is %d\n", a, counter);
+		CHECK_NOT_M1(lseek(fd, 0, SEEK_SET));
+		CHECK_INT(read(fd, buffer, len), len);
+		printf("content is %s, counter is %d\n", buffer, counter);
 		counter++;
 		CHECK_ZERO(sleep(1));
-		CHECK_NOT_M1(lseek(fd, 0, SEEK_SET));
 	}
 	CHECK_NOT_M1(close(fd));
 	return EXIT_SUCCESS;
