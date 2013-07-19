c93cc23aac9d0c89ec526f3255050587f6a7e5d5
Mark Veltzer <mark@veltzer.net>
2013-07-20 02:05:33 +0300

    finished with the memory barriers

---
+++ src/kernel/mod_mb.c
@@ -0,0 +1,56 @@
+/*
+ * This file is part of the linuxapi package.
+ * Copyright (C) 2011-2013 Mark Veltzer <mark.veltzer@gmail.com>
+ *
+ * linuxapi is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * linuxapi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with linuxapi. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* #define DEBUG */
+#include <linux/module.h> /* for MODULE_*. module_* */
+#include <linux/printk.h> /* for pr_* */
+/* #define DO_DEBUG */
+#include "kernel_helper.h" /* our own helper */
+
+/*
+ * This is a minimal module to help disassemble linux kernel
+ * memory barriers.
+ *
+ * This example proves that mb(), rmb() and wmb() translate
+ * all to the same "lock addl $0x0,(%esp)" instruction on
+ * x86.
+*/
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Mark Veltzer");
+MODULE_DESCRIPTION("Module for mb(), rmb(), wmb() disassembly");
+
+/* our own functions */
+static int __init mod_init(void)
+{
+	mb();
+	rmb();
+	wmb();
+	return 0;
+}
+
+
+static void __exit mod_exit(void)
+{
+	PR_INFO("start\n");
+	pr_info("start\n");
+}
+
+/* declaration of init/cleanup functions of this module */
+module_init(mod_init);
+module_exit(mod_exit);
