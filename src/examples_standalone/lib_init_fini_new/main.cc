a3fde46955dfa6c9256c27744c423f30069a922d
Mark Veltzer <mark.veltzer@gmail.com>
2013-07-19 21:13:51 +0300

    more compile fixes

--- src/examples_standalone/lib_init_fini_new/main.cc
+++ src/examples_standalone/lib_init_fini_new/main.cc
@@ -21,8 +21,8 @@
 #include <unistd.h>	// for fork(2), _exit(2), sleep(3)
 #include <sys/wait.h>	// for waitpid(2)
 #include <pthread.h>	// for pthread_create(3), pthread_join(3)
-#include <err_utils.h>	// for CHECK_ZERO_ERRNO()
-#include <us_helper.h>	// for TRACE()
+#include <err_utils.h>	// for CHECK_ZERO()
+#include <trace_utils.h>	// for TRACE()
 
 /*
  * EXTRA_LINK_FLAGS=-lpthread
