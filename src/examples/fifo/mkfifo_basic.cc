7a185a88b39f6234077100614546dcf25bf83695
Mark Veltzer <mark.veltzer@gmail.com>
2013-08-06 10:49:23 +0300

    some basic fixes

---
+++ src/examples/fifo/mkfifo_basic.cc
@@ -0,0 +1,43 @@
+/*
+ * This file is part of the linuxapi package.
+ * Copyright (C) 2011-2013 Mark Veltzer <mark.veltzer@gmail.com>
+ *
+ * linuxapi is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * linuxapi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with linuxapi. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <firstinclude.h>
+#include <stdlib.h>	// for EXIT_SUCCESS
+#include <sys/types.h>	// for mkfifo(2)
+#include <sys/stat.h>	// for mkfifo(2)
+#include <sys/types.h>	// for open(2)
+#include <sys/stat.h>	// for open(2)
+#include <fcntl.h>	// for open(2)
+#include <err_utils.h>	// for CHECK_NOT_M1()
+#include <stdio.h>	// for printf(3)
+
+/*
+ * This example shows basic usage of mkfifo(2)
+ */
+
+int main(int argc, char** argv, char** envp) {
+	const char* filename="/tmp/myfifo";
+	CHECK_NOT_M1(mkfifo(filename, 0666));
+	int fd=CHECK_NOT_M1(open(filename, O_RDONLY));
+	while(true) {
+		char buf[256];
+		ssize_t ret=CHECK_NOT_M1(read(fd, buf, sizeof(buf)));
+		printf("got %d bytes...\n", ret);
+	}
+	return EXIT_SUCCESS;
+}
