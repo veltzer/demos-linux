dcf63cc57c1f876b4904943ce06596bc14f9f048
Mark Veltzer <mark.veltzer@gmail.com>
2013-09-07 16:49:20 +0800

    renames

--- src/examples/pthreads/core/working.cc
+++ src/examples/pthreads/core/working.cc
@@ -18,23 +18,27 @@
 
 #include <firstinclude.h>
 #include <pthread.h>	// for pthread_t, pthread_create(3), pthread_join(3), pthread_self(3)
-#include <unistd.h>	// for sleep(3)
 #include <trace_utils.h>// for TRACE()
 #include <err_utils.h>	// for CHECK_ZERO_ERRNO(), CHECK_ZERO()
+#include <work_utils.h>	// for do_work()
 
 /*
  * This is a demo of a multi-threaded application which does some work.
- * Watch it using top(1) and the 'H' button to see the work.
+ * Watch it using top(1) and the 'H' button which shows you threads
+ * to see the thread actually doing the work.
+ * Note that without the 'H' view top will actually show you the accumulated
+ * CPU time that all threads in this process take which, on a multi-core
+ * machine, may be greater than 100%.
  *
  * EXTRA_LINK_FLAGS=-lpthread
  */
 void *worker(void *p) {
-	int num=*(int *)p;
-	TRACE("starting thread %d", num);
-	pthread_t t=pthread_self();
-	TRACE("pthread_self is %lu", t);
-	CHECK_ZERO(sleep(60));
-	TRACE("ending thread %d", num);
+	//int num=*(int *)p;
+	//TRACE("starting thread %d", num);
+	//pthread_t t=pthread_self();
+	//TRACE("pthread_self is %lu", t);
+	do_work(30);
+	//TRACE("ending thread %d", num);
 	return NULL;
 }
 
@@ -44,15 +48,15 @@ int main(int argc, char** argv, char** envp) {
 	int ids[num];
 	void* rets[num];
 
-	TRACE("main starting");
+	//TRACE("main starting");
 	for(int i=0; i<num; i++) {
 		ids[i]=i;
 		CHECK_ZERO_ERRNO(pthread_create(threads + i, NULL, worker, ids + i));
 	}
-	TRACE("main ended creating threads");
+	//TRACE("main ended creating threads");
 	for (int i=0; i < num; i++) {
 		CHECK_ZERO_ERRNO(pthread_join(threads[i], rets + i));
 	}
-	TRACE("main ended");
+	//TRACE("main ended");
 	return EXIT_SUCCESS;
 }
