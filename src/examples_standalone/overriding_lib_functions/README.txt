ae9f832e9c14c1c872ca2faf8c631783f036294b
Mark Veltzer <mark.veltzer@gmail.com>
2013-09-08 01:13:53 +0800

    added another standalone linking example which doesnt work

---
+++ src/examples_standalone/overriding_lib_functions/README.txt
@@ -0,0 +1,3 @@
+This example shows how you can use the linker to override some functions in a shared object.
+This even goes further to define your own version of the shared object.
+In this example we override the cos(3) function provided by the mathematical library.
---
+++ src/examples_standalone/overriding_lib_functions/main.c
@@ -0,0 +1,33 @@
+/*
+ * This file is part of the linuxapi package.
+ * Copyright (C) 2011-2013 Mark Veltzer <mark.veltzer@gmail.com>
+ *
+ * linuxapi is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * linuxapi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with linuxapi. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <firstinclude.h>
+#include <math.h>	// for cos(3)
+#include <stdio.h>	// for printf(3), fprintf(3), stderr:object
+#include <stdlib.h>	// for EXIT_SUCCESS, EXIT_FAILURE, atof(3)
+
+int main(int argc,char** argv,char** envp) {
+	if(argc!=2) {
+		fprintf(stderr, "%s: usage: %s x\n", argv[0], argv[0]);
+		return EXIT_FAILURE;
+	}
+	double x=atof(argv[1]);
+	printf("cos(%lf) is %lf\n", x, cos(x));
+	//printf("sin(%lf) is %lf\n", x, sin(x));
+	return EXIT_SUCCESS;
+}
