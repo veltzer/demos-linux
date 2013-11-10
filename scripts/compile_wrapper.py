0d0093a343834c38e81525b12b649e54557f88e0
Mark Veltzer <mark.veltzer@gmail.com>
2013-11-10 15:20:09 +0200

    mucho grande fixes to the compiler wrapper script

--- scripts/compile_wrapper.py
+++ scripts/compile_wrapper.py
@@ -6,22 +6,33 @@ You must pass the compiler and the flags to it.
 It will scan the source file in order to find specific flags
 to be added to the compilation or linkage.
 """
+from __future__ import print_function
 
-import sys # for exit
-import subprocess # for Popen
-import codecs
+import sys # for exit, argv
+import subprocess # for Popen, PIPE, stderr, stdout
+
+##############
+# parameters #
+##############
+doDebug=False
+
+#############
+# functions #
+#############
 
 def system_check_output(cmd, input=None, cwd=None, env=None): 
-	pipe=subprocess.Popen(cmd, shell=True, cwd=cwd, env=env, stdout=subprocess.PIPE, stderr=subprocess.STDOUT) 
-	(output,errout)=pipe.communicate(input=input) 
+	pipe=subprocess.Popen(cmd, shell=True, cwd=cwd, env=env, stdout=subprocess.PIPE, stderr=subprocess.PIPE) 
+	(out_stdout,out_stderr)=pipe.communicate(input=input) 
 	status=pipe.returncode 
 	if status:
 		#raise ValueError('error in executing',cmd)
+		print('could not run [%s]' % (cmd), file=sys.stderr)
+		print('error follows...', file=sys.stderr)
+		print(out_stderr.decode(), file=sys.stderr)
+		print('output follows...', file=sys.stderr)
+		print(out_stdout.decode(), file=sys.stderr)
 		sys.exit(status)
-	return output.decode()
-
-# parameters
-debug=False
+	return out_stdout.decode()
 
 # start of code
 script=sys.argv.pop(0)
@@ -31,7 +42,7 @@ link=int(sys.argv.pop(0))
 source=sys.argv.pop(0)
 target=sys.argv.pop(0)
 args=sys.argv
-if debug:
+if doDebug:
 	print('script is',script)
 	print('source is',source)
 	print('target is',target)
@@ -40,7 +51,7 @@ for line in open(source):
 	if link:
 		f=line.find('EXTRA_LINK_CMDS=')
 		if f!=-1:
-			line.strip()
+			line=line.strip()
 			f=line.find('EXTRA_LINK_CMDS=')+len('EXTRA_LINK_CMDS=')
 			cmd=line[f:]
 			cmd=cmd.replace('SOURCE',source)
@@ -51,7 +62,7 @@ for line in open(source):
 			args.extend(out)
 		f=line.find('EXTRA_LINK_FLAGS=')
 		if f!=-1:
-			line.strip()
+			line=line.strip()
 			f=line.find('EXTRA_LINK_FLAGS=')+len('EXTRA_LINK_FLAGS=')
 			cmd=line[f:]
 			cmd=cmd.replace('SOURCE',source)
@@ -61,19 +72,19 @@ for line in open(source):
 	else:
 		f=line.find('EXTRA_COMPILE_CMDS=')
 		if f!=-1:
-			line.strip()
+			line=line.strip()
 			f=line.find('EXTRA_COMPILE_CMDS=')+len('EXTRA_COMPILE_CMDS=')
 			cmd=line[f:]
 			cmd=cmd.replace('SOURCE',source)
 			cmd=cmd.replace('TARGET',target)
 			out=system_check_output(cmd)
-			if debug:
+			if doDebug:
 				print('out is',out)
 			out=out.split()
 			args.extend(out)
 		f=line.find('EXTRA_COMPILE_FLAGS=')
 		if f!=-1:
-			line.strip()
+			line=line.strip()
 			f=line.find('EXTRA_COMPILE_FLAGS=')+len('EXTRA_COMPILE_FLAGS=')
 			cmd=line[f:]
 			cmd=cmd.replace('SOURCE',source)
@@ -81,7 +92,7 @@ for line in open(source):
 			args[1:1]=cmd.split()
 		f=line.find('EXTRA_COMPILE_FLAGS_AFTER=')
 		if f!=-1:
-			line.strip()
+			line=line.strip()
 			f=line.find('EXTRA_COMPILE_FLAGS_AFTER=')+len('EXTRA_COMPILE_FLAGS_AFTER=')
 			cmd=line[f:]
 			cmd=cmd.replace('SOURCE',source)
