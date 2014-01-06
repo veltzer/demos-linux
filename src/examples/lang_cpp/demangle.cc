3e9a74a8d8b11edc79a0e3b1371629df1cef660c
Mark Veltzer <mark.veltzer@gmail.com>
2014-01-06 12:51:22 +0200

    the demangle example is done...

--- src/examples/lang_cpp/demangle.cc
+++ src/examples/lang_cpp/demangle.cc
@@ -17,18 +17,52 @@
  */
 
 #include <firstinclude.h>
-#include <stdlib.h>	// for EXIT_SUCCESS
+#define HAVE_DECL_BASENAME 1
 #include <demangle.h>	// for cplus_demangle(3) 
+#include <stdlib.h>	// for EXIT_SUCCESS
 #include <stdio.h>	// for printf(3)
+#include <us_helper.h>	// for ARRAY_SIZEOF()
 
 /*
- * Demangle c++ symbols using libelf
+ * Demangle c++ symbols using libiberly (which is -liberly when linking on
+ * the command line...)
  *
  * EXTRA_LINK_FLAGS=-liberty
  */
 
+typedef struct _dmgl_val_and_name {
+	int val;
+	const char* name;
+} dmgl_val_and_name;
+
+#define entry(x) { x, # x }
+static dmgl_val_and_name dmgl_tbl[]={
+	entry(DMGL_NO_OPTS),
+	entry(DMGL_PARAMS),
+	entry(DMGL_ANSI),
+	entry(DMGL_JAVA),
+	entry(DMGL_VERBOSE),
+	entry(DMGL_TYPES),
+	entry(DMGL_RET_POSTFIX),
+	entry(DMGL_RET_DROP),
+	entry(DMGL_AUTO),
+	entry(DMGL_GNU),
+	entry(DMGL_LUCID),
+	entry(DMGL_ARM),
+	entry(DMGL_HP),
+	entry(DMGL_EDG),
+	entry(DMGL_GNU_V3),
+	entry(DMGL_GNAT),
+	entry(DMGL_STYLE_MASK),
+};
+
 int main(int argc, char** argv, char** envp) {
-	char* p=cplus_demangle("this is a mangled string", DMGL_GNU);
-	printf("result is [%p]\n", p);
+	const char* mangled="_ZN1A3addEii";
+	for(unsigned int i=0;i<ARRAY_SIZEOF(dmgl_tbl); i++) {
+		const int val=dmgl_tbl[i].val;
+		const char* name=dmgl_tbl[i].name;
+		char* s=cplus_demangle(mangled, val);
+		printf("mangled [%s] and demangled is [%s] ,name is [%s]\n", mangled, s, name);
+	}
 	return EXIT_SUCCESS;
 }
