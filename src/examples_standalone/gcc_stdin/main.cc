c8a12b18c48a7ae41fc37eaa2032f9fea9744677
Mark Veltzer <mark.veltzer@gmail.com>
2014-02-02 11:59:02 +0100

    example of gcc standard input

---
+++ src/examples_standalone/gcc_stdin/main.cc
@@ -0,0 +1,7 @@
+// note this file has no include for <stdio.h>
+#include <stdlib.h> // for EXIT_SUCCESS
+
+int main(int argc, char** argv, char** envp) {
+	printf("Hello, World!\n");
+	return EXIT_SUCCESS;
+}
