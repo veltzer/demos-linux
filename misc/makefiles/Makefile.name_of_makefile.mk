0dee5a0e8698893b99d10e5d896bb1768ff197fc
Mark Veltzer <mark@veltzer.net>
2014-06-17 14:40:22 +0300

    more examples for make

---
+++ misc/makefiles/Makefile.name_of_makefile.mk
@@ -0,0 +1,8 @@
+# This example shows how to know the name of the makefile you are in
+# if you activate this makefile using "make -C" then CURDIR will
+# be the folder that you told make(1) to cd into...
+
+.PHONY: all
+all:
+	$(info MAKEFILE_LIST=$(MAKEFILE_LIST))
+	$(info CURDIR=$(CURDIR))
