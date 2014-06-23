73a96c241e2cd6b1574cfaa1330deb7c29d6e5a4
Mark Veltzer <mark.veltzer@gmail.com>
2014-06-23 13:18:09 +0300

    fixed up two examples

--- src/examples/io/default_open_files.cc
+++ src/examples/io/default_open_files.cc
@@ -19,8 +19,10 @@
 #include <firstinclude.h>
 #include <stdio.h>	// for printf(3), fileno(3), stdin, stdout, stderr
 #include <stdlib.h>	// EXIT_SUCCESS
-#include <unistd.h>	// getdtablesize(2), sysconf(3)
+#include <unistd.h>	// getdtablesize(2), sysconf(3), fcntl(2)
 #include <sys/resource.h>	// for getrlimit(2)
+#include <sys/time.h>	// for getrlimit(2)
+#include <fcntl.h>	// for fcntl(2)
 #include <err_utils.h>	// for CHECK_NOT_M1()
 
 /*
@@ -30,7 +32,25 @@
  * NOTES:
  * - getdtablesize() returns the MAXIMUM number of open files, not the current
  * number of open files.
+ *
+ * TODO:
+ * - where do I get the _POSIX_OPEN_MAX and FOPEN_MAX macros
  */
+
+/*
+	A function to find the number of *really* open files...
+*/
+int get_openfds() {
+	int count = 0;
+	for (int fd = 0; fd < getdtablesize(); fd++) {
+		int res=fcntl(fd, F_GETFD, 0);
+		if(res==-1) {
+			continue;
+		}
+		count++;
+	}
+	return count;
+}
 
 int main(int argc, char** argv, char** envp) {
 	printf("getdtablesize() returned %d\n", CHECK_NOT_M1(getdtablesize()));
@@ -39,8 +59,11 @@ int main(int argc, char** argv, char** envp) {
 	CHECK_NOT_M1(getrlimit(RLIMIT_NOFILE, &rlim));
 	printf("rlim_cur is %ld\n", rlim.rlim_cur);
 	printf("rlim_max is %ld\n", rlim.rlim_max);
+	printf("FOPEN_MAX is %d\n", FOPEN_MAX);
+	printf("_POSIX_OPEN_MAX is %d\n", _POSIX_OPEN_MAX);
 	printf("fileno(stdin) is %d\n", fileno(stdin));
 	printf("fileno(stdout) is %d\n", fileno(stdout));
 	printf("fileno(stderr) is %d\n", fileno(stderr));
+	printf("get_openfds() is %d\n", get_openfds());
 	return EXIT_SUCCESS;
 }
