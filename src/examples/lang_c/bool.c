ef2f9f085d0b60be17e1523137a83e0fdd2dd0f1
Mark Veltzer <mark@veltzer.net>
2014-07-07 12:00:45 +0300

    added a demo for c style booleans

---
+++ src/examples/lang_c/bool.c
@@ -0,0 +1,36 @@
+/*
+ * This file is part of the linuxapi package.
+ * Copyright (C) 2011-2014 Mark Veltzer <mark.veltzer@gmail.com>
+ *
+ * linuxapi is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * linuxapi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with linuxapi. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <firstinclude.h>
+#include <stdio.h>	// for printf(3)
+#include <stdbool.h>	// for bool
+#include <stdlib.h>	// for EXIT_SUCCESS
+
+/*
+ * This shows the 'bool' type for the C language
+ * see 'man stdbool.h' for more info.
+ * Note that without this include you will not get bool, true or false.
+ */
+
+int main(int argc, char** argv, char** envp) {
+	bool __attribute__((unused)) b1=true;
+	bool __attribute__((unused)) b2=false;
+	int __attribute__((unused)) i1=true;
+	int __attribute__((unused)) i2=false;
+	return EXIT_SUCCESS;
+}
