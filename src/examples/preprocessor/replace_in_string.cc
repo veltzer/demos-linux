1227060a897f814713fc93549f8428a4cce37872
Mark Veltzer <mark.veltzer@gmail.com>
2014-07-17 12:20:47 +0300

    organized preprocessor examples

---
+++ src/examples/preprocessor/replace_in_string.cc
@@ -0,0 +1,33 @@
+/*
+ * This file is part of the linuxapi package.
+ * Copyright (C) 2011-2014 Mark Veltzer <mark.veltzer@gmail.com>
+ *
+ * linuxapi is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * linuxapi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with linuxapi. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <firstinclude.h>
+#include <stdio.h>	// for printf(3)
+#include <stdlib.h>	// for EXIT_SUCCESS
+
+/*
+ * This example explores whether the preprocessor will replace things appearing
+ * in strings or not.
+ * The short answer is NO. Preprocessors do not touch strings.
+ */
+int main(int argc, char** argv, char** envp) {
+	printf("linux is cool!\n");
+	printf("linux" "\n");
+	printf("%d is cool!\n", linux);
+	return EXIT_SUCCESS;
+}
