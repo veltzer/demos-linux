d560152962e5aeb023fdd3b7a44791685c314888
Mark Veltzer <mark@veltzer.net>
2014-08-03 15:38:54 +0300

    added a dump_stack example

---
+++ src/kernel/mod_dump_stack.c
@@ -0,0 +1,43 @@
+/*
+ * This file is part of the linuxapi package.
+ * Copyright (C) 2011-2014 Mark Veltzer <mark.veltzer@gmail.com>
+ *
+ * linuxapi is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * linuxapi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with linuxapi. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* #define DEBUG */
+#include <linux/module.h> /* for the MODULE_*, module_* */
+/* define DO_DEBUG */
+#include "kernel_helper.h" /* our own helper */
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Mark Veltzer");
+MODULE_DESCRIPTION("Module showing how to use BUG()");
+
+/*
+* This demo shows the use of the 'dump_stack()' function.
+*/
+static int __init mod_init(void)
+{
+	dump_stack();
+	return 0;
+}
+
+static void __exit mod_exit(void)
+{
+	dump_stack();
+}
+
+module_init(mod_init);
+module_exit(mod_exit);
