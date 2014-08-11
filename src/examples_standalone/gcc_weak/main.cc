b4a0f3fb5a3e97753907b7dd4b05e276373ae7a5
Mark Veltzer <mark@veltzer.net>
2014-08-11 13:34:28 +0300

    example of __attribute__((weak)) in gcc

---
+++ src/examples_standalone/gcc_weak/main.cc
@@ -0,0 +1,27 @@
+/*
+ * This file is part of the linuxapi package.
+ * Copyright (C) 2011-2014 Mark Veltzer <mark.veltzer@gmail.com>
+ *
+ * linuxapi is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * linuxapi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with linuxapi. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <firstinclude.h>
+#include <stdio.h>	// for printf(3)
+#include <stdlib.h>	// for EXIT_SUCCESS
+#include <add.hh>	// for add(U)
+
+int main(int argc, char** argv, char** envp) {
+	printf("did you know that 2+2=%d\n", add(2,2));
+	return EXIT_SUCCESS;
+}
