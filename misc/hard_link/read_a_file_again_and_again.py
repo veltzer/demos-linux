912915b0c25a6cb2dc0a55b01828bf2d1196f70f
Mark Veltzer <mark.veltzer@gmail.com>
2014-10-02 13:22:11 +0300

    another demo

---
+++ misc/hard_link/read_a_file_again_and_again.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+'''
+This demo shows the fact that removing a file does not stop the process accessing it from working
+and even working correctly...
+'''
+
+import time # for sleep
+import shutil # for copy
+
+filename='/tmp/passwd'
+shutil.copy('/etc/passwd', filename)
+print('while I am working please remove [{0}] and see that I keep working right...'.format(filename))
+f=open(filename, 'r')
+while True:
+	for line in f:
+		print(line, end="")
+		time.sleep(1)
+	f.seek(0)
