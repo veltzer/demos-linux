d5ba92cff6d5c9f171a71c3738706f2fedf9d1db
Mark Veltzer <mark.veltzer@gmail.com>
2014-10-25 04:44:06 +0300

    improved an example

--- misc/makefiles/Makefile.default.mk
+++
@@ -1,9 +0,0 @@
-# This makefile shows how to use the .DEFAULT target to make sure
-# everything gets regenerated when the Makefile changes....
-
-# THIS DOES NOT WORK!!!!
-
-.DEFAULT: Makefile.default.mk
-
-out: in
-	cp in out
--- misc/makefiles/Makefile.default_goal.mk
+++ misc/makefiles/Makefile.default_goal.mk
@@ -1,12 +1,19 @@
 # this examples shows how to set the default goal in GNU make
+# if it is not set then the first target will be the default goal
+# you don't have to put the .DEFAULT_GOAL at the beinging of the
+# makefile. Maybe a more logical place would be right before
+# the target which is really the default goal...
 
 .DEFAULT_GOAL=all
 
+.PHONY: echo
 echo:
 	echo $(.DEFAULT_GOAL)
 
+.PHONY: dummy
 dummy:
 	@echo dummy
 
+.PHONY: all
 all:
 	@echo all
