e4e3c61786da87bd333995070ee3303525ad6e2d
Mark Veltzer <mark.veltzer@gmail.com>
2014-11-17 02:23:05 +0200

    improved the examples

--- src/examples/pthreads/mutex/mutex_robust.cc
+++ src/examples/pthreads/mutex/mutex_robust.cc
@@ -17,17 +17,18 @@
  */
 
 #include <firstinclude.h>
-#include <unistd.h>	// for fork(2), getpagesize(2)
-#include <pthread.h>	// for pthread_mutex_init(3), pthread_mutex_lock(3), pthread_mutex_unlock(3), pthread_mutex_destroy(3), pthread_create(3), pthread_join(3)
-#include <err_utils.h>	// for CHECK_ZERO_ERRNO(), CHECK_ZERO(), CHECK_NOT_VOIDP()
+#include <unistd.h>	// for fork(2), getpagesize(2), sleep(3)
+#include <pthread.h>	// for pthread_mutexattr_init(3), pthread_mutexattr_destroy(3), pthread_mutex_init(3), pthread_mutex_lock(3), pthread_mutex_unlock(3), pthread_mutex_destroy(3), PTHREAD_PROCESS_SHARED
+#include <err_utils.h>	// for CHECK_ZERO_ERRNO(), CHECK_NOT_M1(), CHECK_NOT_VOIDP()
 #include <sys/mman.h>	// for mmap(2), PROT_READ, PROT_WRITE, MAP_SHARED, MAP_ANONYMOUS, MAP_FAILED
+#include <trace_utils.h>// for TRACE()
 
 /*
  * This example explores the robustness of futexes.
- * The idea:
+ * The algorithm:
  * Create two processes with shared memory that hold a futex in shared memory.
- * On of the processes locks the lock and crashes.
- * We want to see what the other will get.
+ * One of the processes locks the lock and crashes.
+ * The other, currently, does not break from the dead lock.
  *
  * EXTRA_LINK_FLAGS=-lpthread
  */
@@ -35,13 +36,32 @@
 int main(int argc, char** argv, char** envp) {
 	// setup shared memory where the futex will be
 	void* shared=CHECK_NOT_VOIDP(mmap(NULL, getpagesize(), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0), MAP_FAILED);
+
+	pthread_mutexattr_t attr;
+	CHECK_ZERO_ERRNO(pthread_mutexattr_init(&attr));
+	CHECK_ZERO_ERRNO(pthread_mutexattr_setpshared(&attr, PTHREAD_PROCESS_SHARED));
+
 	pthread_mutex_t* mylock=(pthread_mutex_t*)shared;
 	// create the mutex with the SHARED attribute
-	CHECK_ZERO_ERRNO(pthread_mutex_init(mylock, NULL));
+	CHECK_ZERO_ERRNO(pthread_mutex_init(mylock, &attr));
+	//CHECK_ZERO_ERRNO(pthread_mutexattr_destroy(&attr));
 	if(CHECK_NOT_M1(fork())) {
-		// parent
+		TRACE("parent");
+		sleep(1);
+		TRACE("parent after sleep");
+		CHECK_ZERO_ERRNO(pthread_mutex_lock(mylock));
+		TRACE("parent after lock");
+		TRACE("parent dies");
 	} else {
-		// child
+		TRACE("child");
+		CHECK_ZERO_ERRNO(pthread_mutex_lock(mylock));
+		TRACE("child after lock");
+		sleep(2);
+		TRACE("child after sleep");
+		*(volatile int*)0=0;
+		TRACE("child after crash?!?");
+		CHECK_ZERO_ERRNO(pthread_mutex_unlock(mylock));
+		TRACE("child dies");
 	}
 	return EXIT_SUCCESS;
 }
