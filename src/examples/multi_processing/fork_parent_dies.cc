7b2b88ddb8d51efe8f5d24992565d50bf71647bc
Mark Veltzer <mark@veltzer.net>
2014-11-19 04:45:21 +0200

    a few more fork(2) examples

---
+++ src/examples/multi_processing/fork_parent_dies.cc
@@ -0,0 +1,56 @@
+/*
+ * This file is part of the linuxapi package.
+ * Copyright (C) 2011-2014 Mark Veltzer <mark.veltzer@gmail.com>
+ *
+ * linuxapi is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * linuxapi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with linuxapi. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <firstinclude.h>
+#include <unistd.h>	// for fork(2), getpid(2), getppid(2), sleep(3)
+#include <stdlib.h>	// for EXIT_SUCCESS
+#include <stdio.h>	// for printf(3)
+#include <err_utils.h>	// for CHECK_NOT_M1()
+#include <sys/types.h>	// for getpid(2), getppid(2)
+
+/*
+ * This example shows what happens in fork(2) when the parent dies.
+ * We make sure here that the parent dies before the child prints
+ * all of it's output.
+ * In original UNIX systems the parent of the child was supposed
+ * to be changed to 1/init (adoption). In current Linux systems
+ * the child will be adopted by systemd, upstart which may or
+ * may not have pid=1 according to how those systems were designed.
+ * On my Ubuntu system the parent is 'upstart --user' which is process
+ * run to collect the dead per user on login.
+ */
+
+int main(int argc, char** argv, char** envp) {
+	pid_t child_pid=CHECK_NOT_M1(fork());
+	if(child_pid) {
+		printf("%d: this is the parent\n", getpid());
+		printf("%d: getpid()=%d\n", getpid(), getpid());
+		printf("%d: getppid()=%d\n", getpid(), getppid());
+		// let the parent die
+	} else {
+		printf("%d: this is the child\n", getpid());
+		printf("%d: getpid()=%d\n", getpid(), getpid());
+		printf("%d: getppid()=%d\n", getpid(), getppid());
+		// wait for the parent to die
+		sleep(5);
+		printf("%d: this is the child\n", getpid());
+		printf("%d: getpid()=%d\n", getpid(), getpid());
+		printf("%d: getppid()=%d\n", getpid(), getppid());
+	}
+	return EXIT_SUCCESS;
+}
