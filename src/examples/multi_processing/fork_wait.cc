5eb37d3d8fabcb4b7abde17f612970ad5c3c2037
Mark Veltzer <mark@veltzer.net>
2014-11-19 04:48:02 +0200

    another example

---
+++ src/examples/multi_processing/fork_wait.cc
@@ -0,0 +1,45 @@
+/*
+ * This file is part of the linuxapi package.
+ * Copyright (C) 2011-2014 Mark Veltzer <mark.veltzer@gmail.com>
+ *
+ * linuxapi is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * linuxapi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with linuxapi. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * This is a classic example of fork/wait.
+ */
+
+#include <firstinclude.h>
+#include <unistd.h>	// for fork(2)
+#include <stdlib.h>	// for EXIT_SUCCCESS
+#include <sys/types.h>	// for wait(2)
+#include <sys/wait.h>	// for wait(2)
+#include <err_utils.h>	// for CHECK_NOT_M1()
+#include <multiproc_utils.h>	// for print_status()
+#include <stdio.h>	// for printf(3)
+
+int main(int argc, char** argv, char** envp) {
+	pid_t pid=CHECK_NOT_M1(fork());
+	if(pid) {
+		// parent
+		int status;
+		pid_t child_that_died=CHECK_NOT_M1(wait(&status));
+		printf("child died with pid=%d\n", child_that_died);
+		print_status(status);
+		return EXIT_SUCCESS;
+	} else {
+		// child
+		return 7;
+	}
+}
