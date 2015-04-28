c9a8da5f233c241e54053dadc9d2d08278b02931
Mark Veltzer <mark.veltzer@gmail.com>
2015-04-28 14:13:00 +0300

    64-32

--- src/examples/multi_processing/make_zombie.cc
+++ src/examples/multi_processing/make_zombie.cc
@@ -54,9 +54,9 @@
  * putting 'pkg-config --libs libprocps' in the next line does not work
  * since there is no dynamically linked libprocps.
  * this is for 32 bit:
- * EXTRA_LINK_FLAG=/usr/lib/i386-linux-gnu/libprocps.a
+ * EXTRA_LINK_FLAGS=/usr/lib/i386-linux-gnu/libprocps.a
  * this is for 64 bit:
- * EXTRA_LINK_FLAGS=/usr/lib/x86_64-linux-gnu/libprocps.a
+ * EXTRA_LINK_FLAG=/usr/lib/x86_64-linux-gnu/libprocps.a
  */
 
 // print the state of a process in 3 different ways...
