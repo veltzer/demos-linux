1d590c2f4634aec2c0da91dc71d821fb26160062
Mark Veltzer <mark.veltzer@gmail.com>
2015-09-15 22:34:19 +0300

    new malloc contention example

--- src/examples/malloc/contention.cc
+++ src/examples/malloc/contention.cc
@@ -17,48 +17,73 @@
  */
 
 #include <firstinclude.h>
-#include <stdlib.h>	// for EXIT_SUCCESS
 #include <pthread.h>	// for pthread_create(3), pthread_join(3)
 #include <sched.h>	// for CPU_COUNT(3), CPU_SETSIZE, CPU_ISSET(3)
 #include <unistd.h>	// for sysconf(3)
 #include <trace_utils.h>// for TRACE()
 #include <err_utils.h>	// for CHECK_ZERO_ERRNO(), CHECK_NOT_M1()
+#include <stdlib.h>	// for EXIT_FAILURE, EXIT_SUCCESS, malloc(3), free(3)
 #include <cpu_set_utils.h>	// for cpu_set_print()
 
 /*
- * This shows how to create threads with a certain affinity.
+ * This example is designed to cause as much contention as possible
+ * for the malloc(3) library and to investigate it's behaviour under
+ * these conditions.
  *
- * Notes:
- * - Affinity in Linux is to a *group* of CPUs.
- * - If the group contains just one CPU then you dictate the CPU on which
- * the thread or process will run.
- * - You can see the affinity of a process [pid] this way:
- * cat /proc/[pid]/status | grep Cpus_allowed
- * - Threads and processes inherit the affinity of their parents and cannot
- * extend that affinity (unless they are root or have special privileges).
+ * TODO:
+ * - Analyze the results of this example.
+ * - Show that we block sometimes and sometimes we dont.
+ * - Show that we can block on futex in user space and in kernel space on the mmap(2) semaphore.
+ * - Show that we can play around with the blocking using MALLOC_ARENA_MAX.
+ *
+ * References:
+ * - https://devcenter.heroku.com/articles/tuning-glibc-memory-behavior
+ * - https://www.infobright.com/index.php/malloc_arena_max/#.VfhyG7PtD0o
+ * - http://journal.siddhesh.in/posts/malloc-per-thread-arenas-in-glibc.html
  *
  * EXTRA_LINK_FLAGS=-lpthread
  */
 
+static int num_threads;
+static int num_iterations;
+static int size_min;
+static int size_max;
+
 void *worker(void *p) {
-	int num=*(int *)p;
-	//TRACE("starting thread [%d]", num);
-	int cpunum=CHECK_NOT_M1(sched_getcpu());
-	TRACE("thread [%d] running on cpu [%d]", num, cpunum);
-	//TRACE("ending thread [%d]", num);
+	//int num=*(int *)p;
+	int diff=size_max-size_min;
+	for(int i=0;i<num_iterations;i++) {
+		int size_to_alloc;
+		if(diff==0) {
+			size_to_alloc=size_min;
+		} else {
+			size_to_alloc=size_min+rand()%diff;
+		}
+		void* p=CHECK_NOT_NULL(malloc(size_to_alloc));
+		// free(3) has not return value
+		free(p);
+	}
 	return NULL;
 }
 
 int main(int argc, char** argv, char** envp) {
-	const int cpu_num=CHECK_NOT_M1(sysconf(_SC_NPROCESSORS_ONLN));
-	const int num=cpu_num;
-	pthread_t threads[num];
-	pthread_attr_t attrs[num];
-	cpu_set_t cpu_sets[num];
-	int ids[num];
+	if(argc!=5) {
+		fprintf(stderr, "%s: usage: %s [num_threads] [num_iterations] [size_min] [size_max]\n", argv[0], argv[0]);
+		fprintf(stderr, "%s: example: %s 8 1048576 4096 4096\n", argv[0], argv[0]);
+		return EXIT_FAILURE;
+	}
+	num_threads=atoi(argv[1]);
+	num_iterations=atoi(argv[2]);
+	size_min=atoi(argv[3]);
+	size_max=atoi(argv[4]);
 
-	//TRACE("main starting");
-	for(int i=0; i<num; i++) {
+	const int cpu_num=CHECK_NOT_M1(sysconf(_SC_NPROCESSORS_ONLN));
+	pthread_t threads[num_threads];
+	pthread_attr_t attrs[num_threads];
+	cpu_set_t cpu_sets[num_threads];
+	int ids[num_threads];
+
+	for(int i=0; i<num_threads; i++) {
 		ids[i]=i;
 		CPU_ZERO(cpu_sets + i);
 		CPU_SET(i % cpu_num, cpu_sets + i);
@@ -67,10 +92,8 @@ int main(int argc, char** argv, char** envp) {
 		CHECK_ZERO_ERRNO(pthread_attr_setaffinity_np(attrs + i, sizeof(cpu_set_t), cpu_sets + i));
 		CHECK_ZERO_ERRNO(pthread_create(threads + i, attrs + i, worker, ids + i));
 	}
-	//TRACE("finished creating threads");
-	for(int i=0; i<num; i++) {
+	for(int i=0; i<num_threads; i++) {
 		CHECK_ZERO_ERRNO(pthread_join(threads[i], NULL));
 	}
-	//TRACE("main ended");
 	return EXIT_SUCCESS;
 }
