6a61d8eba8bfc5b0bdc59421d81ccd241880f7ee
Mark Veltzer <mark.veltzer@gmail.com>
2015-12-21 15:46:25 +0200

    new exercise

---
+++ src/exercises/watchdog_simple/watchdog.cc
@@ -0,0 +1,44 @@
+#include <stdio.h> // for printf(3), fprintf(3), stderr
+#include <stdlib.h>	// for EXIT_SUCCESS, exit(3)
+#include <unistd.h>	// for fork(2)
+#include <sys/wait.h>
+#include <errno.h>	// for errno
+#include <string.h>	// for strerror(3)
+
+int handle_error(int ret) {
+	if(ret==-1) {
+		int myerrno=errno;
+		fprintf(stderr, "there was an error from a system call\n");
+		fprintf(stderr, "the error was [%s]\n", strerror(myerrno));
+		exit(-1);
+		return -1;
+	} else {
+		return ret;
+	}
+}
+
+int main(int argc, char** argv, char** envp) {
+	printf("You will see this once...\n");
+	printf("The parent pid is %d\n", getpid());
+	bool need_to_run_child=true;
+	while(need_to_run_child) {
+		pid_t child_pid=handle_error(fork());
+		if(child_pid) {
+			printf("This is the parent\n");
+			printf("The child pid is %d\n", child_pid);
+			int status;
+			pid_t res=handle_error(wait(&status));
+			if(WIFEXITED(status)) {
+				need_to_run_child=0;
+			} else {
+				fprintf(stderr, "Oops, the child died abnormaly and the signal with which it was killed was %d\n", WTERMSIG(status));
+			}
+		} else {
+			printf("This is the child\n");
+			sleep(10);
+			*(char*)0=0;
+			return EXIT_SUCCESS;
+		}
+	}
+	return EXIT_SUCCESS;
+}
