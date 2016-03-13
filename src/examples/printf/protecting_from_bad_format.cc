6e19c06c9c62cf75a3c93bf9fa8be9deedfa59c0
Mark Veltzer <mark.veltzer@gmail.com>
2016-03-13 10:11:37 +0200

    pass check_all

--- src/examples/printf/protecting_from_bad_format.cc
+++ src/examples/printf/protecting_from_bad_format.cc
@@ -1,6 +1,25 @@
-#include<stdlib.h>	// for EXIT_SUCCESS
-#include<unistd.h>	// for malloc(3)
-#include<stdio.h>	// for printf(3)
+/*
+ * This file is part of the linuxapi package.
+ * Copyright (C) 2011-2016 Mark Veltzer <mark.veltzer@gmail.com>
+ *
+ * linuxapi is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * linuxapi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with linuxapi. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <firstinclude.h>
+#include <stdlib.h>	// for EXIT_SUCCESS
+#include <unistd.h>	// for malloc(3)
+#include <stdio.h>	// for printf(3)
 
 /*
  * This example proves that glibc does not protect against
