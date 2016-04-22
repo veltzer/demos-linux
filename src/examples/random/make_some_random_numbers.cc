3e48418f0bfa6eb32466b29295108dd849a2d7f9
Mark Veltzer <mark.veltzer@gmail.com>
2016-04-22 13:46:41 +0300

    pass 'check_all'

--- src/examples/random/make_some_random_numbers.cc
+++ src/examples/random/make_some_random_numbers.cc
@@ -1,5 +1,24 @@
-#include <stdio.h>
-#include <stdlib.h>
+/*
+ * This file is part of the linuxapi package.
+ * Copyright (C) 2011-2016 Mark Veltzer <mark.veltzer@gmail.com>
+ *
+ * linuxapi is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * linuxapi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with linuxapi. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <firstinclude.h>
+#include <stdio.h>	// for atoi(3), printf(3)
+#include <stdlib.h>	// for srandom(3), random(3), EXIT_SUCCESS
 
 int main(int argc, char** argv, char** envp) {
 	srandom(atoi(argv[1]));
