51d39111d4d4ef4d9ee4da7105b0ee15847336af
Mark Veltzer <mark.veltzer@gmail.com>
2019-12-24 14:53:24 +0200

    more code

---
+++ src/examples/real_time/disable_nmi.cc
@@ -0,0 +1,46 @@
+/*
+ * This file is part of the linuxapi package.
+ * Copyright (C) 2011-2018 Mark Veltzer <mark.veltzer@gmail.com>
+ *
+ * linuxapi is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * linuxapi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with linuxapi. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <firstinclude.h>
+#include <stdlib.h>	// for EXIT_SUCCESS
+#include <sys/io.h> // for inb(), outb(), ioperm(2)
+#include <err_utils.h> // for CHECK_NOT_M1()
+
+/*
+ * This example disables Non-Maskable-Interrupts (NMIs)
+ * see:
+ * https://stackoverflow.com/questions/55394608/how-do-i-disable-non-maskable-interrupts-programmatically
+ */
+
+void NMI_enable() {
+	outb(0x70, inb(0x70) & 0x7F);
+}
+
+void NMI_disable() {
+	outb(0x70, inb(0x70) | 0x80);
+}
+
+void NMI_set_permissions() {
+	CHECK_NOT_M1(ioperm(0x70, 8, 1));
+}
+
+int main(int argc, char** argv, char** envp) {
+	NMI_set_permissions();
+	NMI_disable();
+	return EXIT_SUCCESS;
+}
