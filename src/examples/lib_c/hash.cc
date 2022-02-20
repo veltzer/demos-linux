5de6601a9103bb50277b24a365ea63f7de9c559a
Mark Veltzer <mark.veltzer@gmail.com>
2022-02-20 23:32:26 +0200

    license change

--- src/examples/lib_c/hash.cc
+++ src/examples/lib_c/hash.cc
@@ -1,6 +1,6 @@
 /*
  * This file is part of the demos-linux package.
- * Copyright (C) 2011-2021 Mark Veltzer <mark.veltzer@gmail.com>
+ * Copyright (C) 2011-2022 Mark Veltzer <mark.veltzer@gmail.com>
  *
  * demos-linux is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
