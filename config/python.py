40b9ae06c259ef267a3f5e6fec1c0ce32e7486d3
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-06 01:58:34 +0300

    fixing build

--- config/python.py
+++ config/python.py
@@ -1,27 +1,16 @@
-console_scripts = [
-]
-
-setup_requires = [
-]
-
-run_requires = [
-]
-
-test_requires = [
-]
-
+console_scripts = []
+setup_requires = []
+run_requires = []
+test_requires = []
 dev_requires = [
     'pymakehelper',  # for the makefile
     'pydmt',  # for building
 ]
-
-test_container="[ 'ubuntu:21.10' ]"
-test_python="[3.9]"
-
 install_requires = list(setup_requires)
 install_requires.extend(run_requires)
 
-python_requires = ">=3.9"
+extras_require = {}
 
-extras_require = {
-}
+python_requires = ">=3.9"
+test_python=["3.9"]
+test_container=['ubuntu:21.10']
