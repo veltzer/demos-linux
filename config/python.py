5a6342d9444b2f79b3272de1495f5e3ba9fef82e
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-06 14:09:48 +0300

    better deps

--- config/python.py
+++ config/python.py
@@ -1,16 +1,8 @@
-console_scripts = []
-setup_requires = []
-run_requires = []
-test_requires = []
 dev_requires = [
-    'pymakehelper',  # for the makefile
-    'pydmt',  # for building
+    "pymakehelper",
+    "pydmt",
 ]
-install_requires = list(setup_requires)
-install_requires.extend(run_requires)
-
-extras_require = {}
 
 python_requires = ">=3.9"
 test_python=["3.9"]
-test_container=['ubuntu:21.10']
+test_container=["ubuntu:21.10"]
