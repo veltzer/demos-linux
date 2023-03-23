be061f63b2c68d01d9edb7d8ea2a005b08e469bb
Mark Veltzer <mark.veltzer@gmail.com>
2023-03-23 08:51:35 +0200

    no commit message given

--- src/kernel_standalone/sleep_wakeup/ioctl_sleep.c
+++ src/kernel_standalone/sleep_wakeup/ioctl_sleep.c
@@ -2,11 +2,17 @@
 #include <sys/ioctl.h>
 #include <unistd.h>
 #include <assert.h>
+#include <errno.h>
+#include <stdio.h>
 
 #include "ioctl.h"
 
 int main(int argc, char** argv, char** envp) {
 	int fd=open("/dev/ioctl", O_RDWR);
+	if(fd==-1) {
+		perror(argv[0]);
+		return -1;
+	}
 	assert(fd!=-1);
 	int ret=ioctl(fd, IOCTL_SLEEP);
 	assert(ret!=-1);
