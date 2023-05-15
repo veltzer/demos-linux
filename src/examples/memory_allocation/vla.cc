ca70dd80e4736c907dd8fd24a3160baf06db8f10
Mark Veltzer <mark.veltzer@gmail.com>
2023-05-15 15:56:47 +0300

    vla example added

---
+++ src/examples/memory_allocation/vla.cc
@@ -0,0 +1,69 @@
+/*
+ * This file is part of the demos-linux package.
+ * Copyright (C) 2011-2023 Mark Veltzer <mark.veltzer@gmail.com>
+ *
+ * demos-linux is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * demos-linux is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with demos-linux. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <firstinclude.h>
+#include <stdlib.h>	// EXIT_SUCCESS
+#include <stdio.h>	// printf(3), scanf(3)
+#include <err_utils.h>	// for CHECK_INT()
+
+/*
+ * This example shows how to allocate memory using what's called a VLA - Variable Length Array.
+ * This is available via GCC, ISO C99, C90 or C++14
+ * This memory is allocated on the stack, and this example demonstrates this althrough the gcc documentation
+ * does not state so explicitly.
+ * You can just run this example and see that the address of the variable length array is the same as the stack.
+ * This feature is therefore similar to alloca(3) but more elegant.
+ * Jumping or breaking out of the scope of the array name deallocates the storage.
+ * Jumping into the scope is not allowed; you get an error message for it.
+ * This is epecially important for longjmp(3) and exception handling in C++.
+ * Note that "pedantic" gcc mode does not allow the "vla" features and so we need to supply the
+ * "-Wno-vla" flag below.
+ *
+ * EXTRA_COMPILE_FLAGS=-Wno-vla
+ *
+ * References:
+ * - https://gcc.gnu.org/onlinedocs/gcc/Variable-Length.html
+ * - https://www.geeksforgeeks.org/variable-length-arrays-in-c-and-c/
+ */
+
+void function(int len) {
+	int array[len];
+	for(int i=0;i<len;i++) {
+		array[i]=i;
+	}
+	int sum=0;
+	for(int i=0;i<len;i++) {
+		sum+=array[i];
+	}
+	printf("sum is %d\n", sum);
+	printf("array location is %p\n", (void*)array);
+}
+
+void print_stack_adr() {
+	int a;
+	printf("stack location is %p\n", (void*)&a);
+}
+
+int main(int argc, char** argv, char** envp) {
+	print_stack_adr();
+	int len;
+	printf("Enter the size of the array: ");
+	CHECK_INT(scanf("%d", &len), 1);
+	function(len);
+	return EXIT_SUCCESS;
+}
