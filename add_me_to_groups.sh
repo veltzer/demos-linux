355d1116dc3088920d125d69ea7a6310113701eb
Mark Veltzer <mark.veltzer@gmail.com>
2023-07-01 05:56:04 +0300

    lint

--- add_me_to_groups.sh
+++ add_me_to_groups.sh
@@ -3,10 +3,10 @@
 # This script adds the current user to all the groups that you need to be
 # a member of in order to run all the demos in this repository.
 
-for x in adm cdrom sudo dip plugdev kvm lpadmin lxd sambashare docker libvirt microk8s stapusr stapdev stapsys
+for group in adm cdrom sudo dip plugdev kvm lpadmin lxd sambashare docker libvirt microk8s stapusr stapdev stapsys
 do
-	if ! sudo usermod -a -G $x $USER
+	if ! sudo usermod -a -G "${group}" "${USER}"
 	then
-		echo "failed to add user $USER to group $group"
+		echo "failed to add user ${USER} to group ${group}"
 	fi
 done
