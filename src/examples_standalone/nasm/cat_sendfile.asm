e4b2675e3b452260a93a64d0581ec0930f5dd7a2
Mark Veltzer <mark.veltzer@gmail.com>
2023-10-29 12:29:51 +0200

    no commit message given

--- src/examples_standalone/nasm/cat_sendfile.asm
+++ src/examples_standalone/nasm/cat_sendfile.asm
@@ -11,17 +11,25 @@ mov rax, 2 ; NR_OPEN
 mov rdi, [rsp+16] ; rsp+16 -> first argument; rsp+8 -> program name; rsp -> argc
 mov rsi, 0 ; O_RDONLY
 syscall
+mov r8, rax
+cmp r8, 0
+jl error
 
 ; sendfile()
-mov rsi, rax ; rax is the return value of open(2) before = 3
 mov rax, 40 ; NR_SENDFILE
+mov rsi, r8 ; r8 is the return value of open(2) before = 3
 mov rdi, 1 ; out_fd = stdout
 mov rdx, 0 ; offset, this is 0=NULL
 mov r10, 4096 ; count
 syscall
+mov r8, rax
+cmp r8, 0
+jl error
 jmp $
 
-; exit(7)
+error:
+end:
+; exit(r8)
 mov rax, 60
-mov rdi, 7
+mov rdi, r8
 syscall
