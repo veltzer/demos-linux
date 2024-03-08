dc2da1152639b19cb3bc6aa9da198dcb05078015
Mark Veltzer <mark.veltzer@gmail.com>
2024-03-08 06:43:14 +0200

    no commit message given

--- src/examples/lang_cpp/virtual_functions_increase_size.cc
+++ src/examples/lang_cpp/virtual_functions_increase_size.cc
@@ -18,8 +18,9 @@
 
 #include <firstinclude.h>
 #include <iostream>	// for std::cout, std::endl
-#include <assert.h>	// for assert(3)
+#include <cassert>	// for assert()
 #include <stdlib.h>	// for EXIT_SUCCESS
+#include <cstdint>	// for std::intptr_t
 
 /*
  * This example shows that a single virtual function in an object increases it's size.
@@ -29,34 +30,29 @@
 
 class A1 {
 public:
-	int a;
+	std::intptr_t a;
 
-	A1(int val) {
-		a=val;
+	A1(int val):a(val) {
 	}
 	void dosomething() {
-		for(int i=0; i<4; i++) {
-			std::cout << "Hello from A, i is " << i << std::endl;
-		}
+		std::cout << "Hello from A1" << std::endl;
 	}
 };
 
 class A2 {
 public:
-	int a;
+	std::intptr_t a;
 
-	A2(int val) {
-		a=val;
+	A2(int val):a(val) {
 	}
 
 	virtual void dosomething() {
-		for(int i=0; i<4; i++) {
-			std::cout << "Hello from A, i is " << i << std::endl;
-		}
+		std::cout << "Hello from A2" << std::endl;
 	}
 };
 
 int main(int argc, char** argv, char** envp) {
+	assert(sizeof(A1)+sizeof(nullptr)==sizeof(A2));
 	std::cout << "sizeof(A1) is " << sizeof(A1) << std::endl;
 	std::cout << "sizeof(A2) is " << sizeof(A2) << std::endl;
 	A2 obj1(4), obj2(5);
@@ -65,8 +61,8 @@ int main(int argc, char** argv, char** envp) {
 	void** vp2=(void**)&obj2;
 	assert(*vp1==*vp2);
 	// this is where the data is
-	int* p1=(int*)&obj1;
-	int* p2=(int*)&obj2;
+	std::intptr_t* p1=(std::intptr_t*)&obj1;
+	std::intptr_t* p2=(std::intptr_t*)&obj2;
 	assert(4==*(p1+1));
 	assert(5==*(p2+1));
 	return EXIT_SUCCESS;
