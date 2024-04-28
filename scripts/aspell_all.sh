97c4766728652605433a584ad6409611e4a8196a
Mark Veltzer <mark.veltzer@gmail.com>
2024-04-28 13:16:30 +0300

    remove common file

--- scripts/aspell_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-# the next line is to make the globbing work right
-shopt -s globstar extglob
-# for x in $(find syllabi -type f -and -name "*.md")
-for x in src/exercises/**/*.md
-do
-	aspell --conf-dir=. --conf=.aspell.conf check "${x}"
-done
