c9b20993c4feb8c5e20ddcc9a7191e239d81984d
Mark Veltzer <mark.veltzer@gmail.com>
2024-07-25 13:04:46 +0300

    no commit message given

--- src/exercises/cpp/basic_oo/circle.hh
+++ src/exercises/cpp/basic_oo/circle.hh
@@ -3,18 +3,18 @@
 #include <iostream>
 
 class Circle {
-    private:
-        double radius, x, y;
-    public:
-        Circle(double radius, double x, double y);
+	private:
+		double radius, x, y;
+	public:
+		Circle(double radius, double x, double y);
 
-        // getters and setters
-        double get_radius() {
-            return radius;
-        }        
-        void set_radius(double iradius) {
-            radius = iradius;
-        }
+		// getters and setters
+		double get_radius() {
+			return radius;
+		}		
+		void set_radius(double iradius) {
+			radius = iradius;
+		}
 	double get_x() {
 		return x;
 	}
@@ -22,7 +22,7 @@ class Circle {
 		return y;
 	}
 
-        bool inside(double, double);
+	bool inside(double, double);
 
-        friend std::ostream& operator<<(std::ostream& os, Circle& obj);
+	friend std::ostream& operator<<(std::ostream& os, Circle& obj);
 };
--- src/exercises/cpp/basic_oo/circle2.cc
+++ src/exercises/cpp/basic_oo/circle2.cc
@@ -6,15 +6,15 @@ Circle::Circle(const double iradius, const double ix, const double iy):radius(ir
 }
 
 bool Circle::inside(const double ix, const double iy) const {
-    double distance2 = (ix-x)*(ix-x) + (iy-y)*(iy-y);
-    return distance2 <= radius*radius;
+	double distance2 = (ix-x)*(ix-x) + (iy-y)*(iy-y);
+	return distance2 <= radius*radius;
 }
 
 std::ostream& operator<<(std::ostream& os, const Circle& obj) {
-        os << "radius is " << obj.get_radius() << endl;
-        os << "x is " << obj.get_x() << endl;
-        os << "y is " << obj.get_y() << endl;
-        return os;
+	os << "radius is " << obj.get_radius() << endl;
+	os << "x is " << obj.get_x() << endl;
+	os << "y is " << obj.get_y() << endl;
+	return os;
 }
 
 int main() {
