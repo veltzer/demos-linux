2ad78dbe92e33678adac1a8fe5123dcbdc207a1d
Mark Veltzer <mark.veltzer@gmail.com>
2024-07-25 10:11:47 +0300

    no commit message given

--- src/exercises/cpp/inheritance/solution.cc
+++ src/exercises/cpp/inheritance/solution.cc
@@ -1,33 +1,91 @@
 #include <iostream>
 #include <cstdlib>
+#include <vector>
 
 using namespace std;
 
-class Person {
-	private:
-		int age;
-		bool married;
+
+class Shape {
 	public:
-		Person(int iage, bool imarried) {
-			age=iage;
-			married=imarried;
-		}
-	friend ostream& operator<<(ostream& os, const Person& obj);
+		virtual void print_me() const=0;
 };
 
-ostream& operator<<(ostream& os, const Person& obj) {
-	os << "my age is " << obj.age << endl;
-	os << "married status is " << obj.married << endl;
-	return os;
-}
+class Rectangle: public Shape {
+	private:
+		double a, b;
+	public:
+		Rectangle(double ia, double ib):a(ia), b(ib) {}
+		virtual void print_me() const override {
+			cout << "Rectangle" << endl;
+			cout << "a is " << a << endl;
+			cout << "b is " << b << endl;
+		}
+};
+
+class Circle: public Shape {
+	private:
+		double r, x, y;
+	public:
+		Circle(double ir, double ix, double iy):r(ir), x(ix), y(iy) {}
+		void print_me() const override {
+			cout << "Circle" << endl;
+			cout << "r is " << r << endl;
+			cout << "x is " << x << endl;
+			cout << "y is " << y << endl;
+		}
+};
+
+class Triangle: public Shape {
+	private:
+		double a, b, c;
+	public:
+		Triangle(double ia, double ib, double ic):a(ia), b(ib), c(ic) {}
+		void print_me() const override {
+			cout << "Triangle" << endl;
+			cout << "a is " << a << endl;
+			cout << "b is " << b << endl;
+			cout << "c is " << c << endl;
+		}
+};
 
 
 int main() {
-	int age = 25;
-	bool allowed_to_drive = true;
-	cout << "my age is " << age << endl;
-	cout << "allowed to drive is " << allowed_to_drive << endl;
-	Person p(29, true);
-	cout << "person details are " << p << endl;
+	//std::vector<std::reference_wrapper<Shape>> v;
+	std::vector<Shape*> v;
+	while(true) {
+		cout << "1. add rectangle" << endl;
+		cout << "2. add circle" << endl;
+		cout << "3. add triangle" << endl;
+		cout << "4. stop" << endl;
+		int choice;
+		cin >> choice;
+		if(choice==4) {
+			break;
+		}
+		double a, b, c, r, x, y;
+		switch(choice) {
+			case 1:
+				cin >> a;
+				cin >> b;
+				v.push_back(new Rectangle(a, b));
+				break;
+			case 2:
+				cin >> r;
+				cin >> x;
+				cin >> y;
+				v.push_back(new Circle(r, x, y));
+				break;
+			case 3:
+				cin >> a;
+				cin >> b;
+				cin >> c;
+				v.push_back(new Triangle(a, b, c));
+				break;
+		}
+	}
+	for (size_t i = 0; i < v.size(); i++) {
+		//v[i].get().print_me();
+		v[i]->print_me();
+	}
 	return EXIT_SUCCESS;
 }
