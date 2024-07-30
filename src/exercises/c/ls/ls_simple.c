ad33d948fb1fc5c0fd1bebfbd71babe52d385e5b
Mark Veltzer <mark.veltzer@gmail.com>
2024-07-30 15:29:53 +0300

    no commit message given

---
+++ src/exercises/c/ls/ls_simple.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <assert.h>
+#include <sys/stat.h>
+
+int main() {
+	DIR* p=opendir(".");
+	assert(p!=NULL);
+	struct dirent* current;
+	while(current=readdir(p)) {
+		printf("got file [%s]\n", current->d_name);
+		struct stat statbuf;
+		int res=stat(current->d_name, &statbuf);
+		assert(res!=-1);
+		printf("uid is %d\n", statbuf.st_uid);
+		printf("gid is %d\n", statbuf.st_gid);
+
+	}
+	return 0;
+}
