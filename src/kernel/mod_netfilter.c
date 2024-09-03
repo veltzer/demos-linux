56ba39ac8c5ce0712399d63b6bfbef9e9a9a8738
Mark Veltzer <mark.veltzer@gmail.com>
2024-09-03 22:12:06 +0300

    no commit message given

--- src/kernel/mod_netfilter.c
+++ src/kernel/mod_netfilter.c
@@ -44,24 +44,8 @@ MODULE_AUTHOR("Mark Veltzer");
 MODULE_DESCRIPTION("Basic netfilter module");
 MODULE_LICENSE("GPL");
 
-/* #define DO_PROC */
-
 static struct nf_hook_ops nfho; /* net filter hook option struct */
-static int filter_value = 1; /* initialized automatically to NULL */
-#ifdef DO_PROC
-static const char *skb_filter_name = KBUILD_MODNAME;
-static struct proc_dir_entry *skb_filter;
-#endif /* DO_PROC */
 
-/*
- * old API
- * static unsigned int hook_func(
- *	unsigned int hooknum,
- *	struct sk_buff *skb,
- *	const struct net_device *in,
- *	const struct net_device *out,
- *	int (*okfn)(struct sk_buff *)) {
- */
 static unsigned int hook_func(
 	void *priv,
 	struct sk_buff *skb,
@@ -69,134 +53,45 @@ static unsigned int hook_func(
 )
 {
 	struct iphdr *ip_header; /* IP header struct */
-	struct udphdr *udp_header; /* UDP header struct */
-	struct icmphdr *icmp_header; /* ICMP Header */
 
 	if (!skb)
 		return NF_ACCEPT;
-	ip_header = (struct iphdr *)skb_network_header(skb);
-	if (ip_header->protocol == IPPROTO_UDP) {
-		udp_header = (struct udphdr *)(skb_transport_header(skb) +
-				ip_hdrlen(skb));
-		if (udp_header)
-			pr_info("SRC: (%pI4):%d --> DST: (%pI4):%d\n",
-					&ip_header->saddr,
-					ntohs(udp_header->source),
-					&ip_header->daddr,
-					ntohs(udp_header->dest)
-			);
-		else
-			return NF_DROP;
+	ip_header = ip_hdr(skb);
+	if (ip_header) {
+		pr_info(
+			"Packet intercepted: Source IP = %pI4, Dest IP = %pI4\n",
+			&ip_header->saddr,
+			&ip_header->daddr
+		);
 	}
-	if (ip_header->protocol == IPPROTO_ICMP) {
-		pr_info("---------- ICMP -------------\n");
-		icmp_header = (struct icmphdr *)(skb_transport_header(skb) +
-				ip_hdrlen(skb));
-		if (icmp_header) {
-			pr_info("SRC: (%pI4) --> DST: (%pI4)\n",
-					&ip_header->saddr,
-					&ip_header->daddr
-			);
-			pr_info("ICMP type: %d - ICMP code: %d\n",
-					icmp_header->type,
-					icmp_header->code
-			);
-		} else
-			return NF_DROP;
-	}
-	return filter_value == 0 ? NF_ACCEPT:NF_DROP;
+	return NF_ACCEPT;
 }
-
-#ifdef DO_PROC
-static int skb_read(char *page, char **start, off_t off, int count, int *eof,
-		void *data)
-{
-	int len;
-
-	if (off > 0) {
-		*eof = 1;
-		return 0;
-	}
-	if (count < sizeof(int)) {
-		*eof = 1;
-		return -ENOSPC;
-	}
-	/* copy to userspace */
-	memcpy(page, &filter_value, sizeof(int));
-	len = sizeof(int);
-	return len;
-}
-
-static int skb_write(struct file *file, const char *buffer, unsigned long len,
-		void *data)
-{
-	int ret;
-	unsigned char userData;
-
-	if (len > PAGE_SIZE || len < 0) {
-		pr_err("SKB System: cannot allow space for data\n");
-		return -ENOSPC;
-	}
-	/* write data to the buffer */
-	ret = copy_from_user(&userData, buffer, 1);
-	if (ret) {
-		pr_err("SKB System: cannot copy data from userspace\n");
-		return ret;
-	}
-	ret = kstrtol(&userData, 10, &filter_value);
-	if (ret) {
-		pr_err("bad value from user buffer\n");
-		return ret;
-	}
-	return len;
-}
-#endif /* DO_PROC */
 
 static int __init mod_init(void)
 {
 	int ret;
-	#ifdef DO_PROC
-	skb_filter = create_proc_entry(skb_filter_name, 0644, NULL);
-	if (IS_ERR(skb_filter)) {
-		pr_err("could not create proc entry\n");
-		ret = PTR_ERR(skb_filter);
-		goto error;
-	}
-	skb_filter->read_proc = skb_read;
-	skb_filter->write_proc = skb_write;
-	/* skb_filter->owner = THIS_MODULE; */
-	#endif /* DO_PROC */
-
 	/* Netfilter hook information, specify where and when we get the SKB */
 	nfho.hook = hook_func;
-	nfho.hooknum = NF_INET_PRE_ROUTING;
 	nfho.pf = PF_INET;
+	nfho.hooknum = NF_INET_PRE_ROUTING;
 	nfho.priority = NF_IP_PRI_LAST;
-	ret = nf_register_net_hook(NULL, &nfho);
+	ret = nf_register_net_hook(&init_net, &nfho);
 	if (ret) {
 		pr_err("could not register netfilter hook\n");
-		goto error_proc;
+		goto error;
 	}
 
-	pr_info("Registering SK Parse Module\n");
+	pr_info("Netfilter module loaded.\n");
 
-error_proc:
-	#ifdef DO_PROC
-	remove_proc_entry(skb_filter_name, NULL);
-	#endif /* DO_PROC */
-/* error: */
+error:
 	return ret;
 }
 
 static void __exit mod_exit(void)
 {
-	nf_unregister_net_hook(NULL, &nfho);
-	#ifdef DO_PROC
-	remove_proc_entry(skb_filter_name, NULL);
-	#endif /* DO_PROC */
-	pr_info("Unregistered the SK Parse Module\n");
+	nf_unregister_net_hook(&init_net, &nfho);
+	pr_info("Netfilter module unloaded.\n");
 }
 
-/* declaration of init/cleanup functions of this module */
 module_init(mod_init);
 module_exit(mod_exit);
