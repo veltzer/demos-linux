5788c771c00ff7369957ce3f599674603dc2edd7
Mark Veltzer <mark.veltzer@gmail.com>
2024-09-13 09:57:41 +0300

    fix warnings

--- src/kernel_standalone/netdev/main.c
+++ src/kernel_standalone/netdev/main.c
@@ -1,56 +1,77 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * This file is part of the demos-linux package.
+ * Copyright (C) 2011-2024 Mark Veltzer <mark.veltzer@gmail.com>
+ *
+ * demos-linux is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * demos-linux is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with demos-linux. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <linux/module.h>
 #include <linux/netdevice.h>
 #include <linux/etherdevice.h>
 
-static int my_netdev_open(struct net_device *netdev) {
-    // Initialize hardware, enable interrupts, etc.
-    netif_start_queue(netdev); 
-    return 0;
+static int my_netdev_open(struct net_device *netdev)
+{
+	// Initialize hardware, enable interrupts, etc.
+	netif_start_queue(netdev);
+	return 0;
 }
 
-static int my_netdev_stop(struct net_device *netdev) {
-    // Stop hardware, disable interrupts, etc.
-    netif_stop_queue(netdev); 
-    return 0;
+static int my_netdev_stop(struct net_device *netdev)
+{
+	// Stop hardware, disable interrupts, etc.
+	netif_stop_queue(netdev);
+	return 0;
 }
 
-static netdev_tx_t my_netdev_xmit(struct sk_buff *skb, struct net_device *netdev) {
-    // Transmit the packet (skb) using the appropriate PHY driver or hardware interface
-    // For this skeleton, we'll just consume the skb and return success
-    dev_kfree_skb_any(skb); 
-    return NETDEV_TX_OK;
+static netdev_tx_t my_netdev_xmit(struct sk_buff *skb, struct net_device *netdev)
+{
+	// Transmit the packet (skb) using the appropriate PHY driver or hardware interface
+	// For this skeleton, we'll just consume the skb and return success
+	dev_kfree_skb_any(skb);
+	return NETDEV_TX_OK;
 }
 
 // Other necessary functions (e.g., set_mac_address, ioctl, etc.)
 
-static struct net_device_ops my_netdev_ops = {
-    .ndo_open       = my_netdev_open,
-    .ndo_stop       = my_netdev_stop,
-    .ndo_start_xmit = my_netdev_xmit,
-    // ... other operations as needed
+static const struct net_device_ops my_netdev_ops = {
+	.owner = THIS_MODULE,
+	.ndo_open       = my_netdev_open,
+	.ndo_stop       = my_netdev_stop,
+	.ndo_start_xmit = my_netdev_xmit,
+	// ... other operations as needed
 };
 
-static int __init my_netdev_init(void) {
-    struct net_device *netdev;
+static int __init my_netdev_init(void)
+{
+	struct net_device *netdev;
 
-    netdev = alloc_etherdev(0); 
-    if (!netdev)
-        return -ENOMEM;
-
-    netdev->netdev_ops = &my_netdev_ops;
-
-    // Set up other necessary fields in netdev (e.g., name, MAC address, etc.)
-    // ...
-
-    if (register_netdev(netdev)) {
-        free_netdev(netdev);
-        return -ENODEV;
-    }
-
-    return 0;
+	netdev = alloc_etherdev(0);
+	if (!netdev)
+		return -ENOMEM;
+	netdev->netdev_ops = &my_netdev_ops;
+	// Set up other necessary fields in netdev (e.g., name, MAC address, etc.)
+	// ...
+	if (register_netdev(netdev)) {
+		free_netdev(netdev);
+		return -ENODEV;
+	}
+	return 0;
 }
 
-static void __exit my_netdev_exit(void) {
+static void __exit my_netdev_exit(void)
+{
     // Find and unregister the net_device
     // ...
 }
