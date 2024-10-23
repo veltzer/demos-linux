dac06c8dd79cf24aa22b08a1b45fffc9792077ed
Mark Veltzer <mark.veltzer@gmail.com>
2024-10-23 08:33:59 +0300

    no commit message given

---
+++ src/examples/floating_point/precision_problems.cc
@@ -0,0 +1,35 @@
+/*
+ * This file is part of the demos-linux package.
+ * Copyright (C) 2011-2024 Mark Veltzer <mark.veltzer@gmail.com>
+ *
+ * demos-linux is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * demos-linux is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with demos-linux. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <firstinclude.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	float f=345345345345345.0;
+	float g=f+100;
+	cout << f << endl;
+	cout << g << endl;
+	if(f==g) {
+		cout << "they are the same!" << endl;
+	}
+	return EXIT_SUCCESS;
+}
