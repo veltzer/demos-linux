797940441af876b0a05d1e4565326f0940da4728
Mark Veltzer <mark.veltzer@gmail.com>
2025-01-03 17:24:45 +0200

    .mdl.style sync

--- .mdl.style.rb
+++ .mdl.style.rb
@@ -1,6 +1,10 @@
 all
+rule 'MD007', :indent => 4
+rule 'MD013', :line_length => 1000
 exclude_rule 'MD022'
+exclude_rule 'MD024'
+exclude_rule 'MD026'
+exclude_rule 'MD031'
 exclude_rule 'MD032'
 exclude_rule 'MD033'
-rule 'MD013', :line_length => 1000
-rule 'MD007', :indent => 4
+exclude_rule 'MD034'
