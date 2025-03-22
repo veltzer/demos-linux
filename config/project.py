23d91b64722299d7d84e7144a235643fc2b80567
Mark Veltzer <mark.veltzer@gmail.com>
2025-03-22 21:27:09 +0200

    linting

--- config/project.py
+++ config/project.py
@@ -1,3 +1,4 @@
+""" project definitions """
 name = "demos-linux"
 description_short = "demos-linux is a project to demo and explore the Linux user space C/C++ API"
 description_long = description_short
