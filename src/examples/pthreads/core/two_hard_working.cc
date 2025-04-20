256cf6880147e965809d9aa7a16c8b18da1ade46
Mark Veltzer <mark.veltzer@gmail.com>
2025-04-20 05:34:08 +0300

    no commit message given

---
+++ src/examples/pthreads/core/two_hard_working.cc
@@ -0,0 +1,62 @@
+/*
+ * This file is part of the demos-linux package.
+ * Copyright (C) 2011-2024 Mark Veltzer <mark.veltzer@gmail.com>
+ *
+ * demos-linux is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * demos-linux is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with demos-linux. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <firstinclude.h>
+#include <pthread.h>	// for pthread_t, pthread_create(3), pthread_join(3), pthread_self(3)
+#include <unistd.h>	// for sleep(3), getpid(2)
+#include <sys/types.h>	// for getpid(2)
+#include <sched.h>	// for sched_getcpu(2)
+#include <trace_utils.h>// for TRACE()
+#include <pthread_utils.h>	// for gettid(2), gettid_cached()
+#include <err_utils.h>	// for CHECK_ZERO_ERRNO(), CHECK_ZERO()
+
+/*
+ * This is a standard pthread demo.
+ * This shows the various attributes of each of the threads:
+ * pthread_self, tid, tid cached, pid, core it is running
+ * on.
+ *
+ * EXTRA_COMPILE_FLAGS=-g
+ * EXTRA_LINK_FLAGS_AFTER=-lpthread
+ */
+
+static void* worker(void*) {
+	while(1) {
+	}
+	return NULL;
+}
+
+int main() {
+	const int num=2;
+	pthread_t threads[num];
+	int ids[num];
+	void* rets[num];
+
+	TRACE("main getpid() is %d", getpid());
+	TRACE("main starting");
+	for(int i=0; i<num; i++) {
+		ids[i]=i;
+		CHECK_ZERO_ERRNO(pthread_create(threads + i, NULL, worker, ids + i));
+	}
+	TRACE("main ended creating threads");
+	for(int i=0; i < num; i++) {
+		CHECK_ZERO_ERRNO(pthread_join(threads[i], rets + i));
+	}
+	TRACE("main ended");
+	return EXIT_SUCCESS;
+}
