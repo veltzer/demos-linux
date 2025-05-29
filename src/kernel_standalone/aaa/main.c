b60b4058c2d35e533999adaa2b5a418199de94ef
Mark Veltzer <mark.veltzer@gmail.com>
2025-05-29 12:10:27 +0300

    written with the help of gemini

--- src/kernel_standalone/aaa/main.c
+++ src/kernel_standalone/aaa/main.c
@@ -17,120 +17,54 @@
  * along with demos-linux. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <linux/module.h> /* for MODULE_*, module_* */
+#include <linux/module.h> /* for MODULE_*, module_*, printk */
 #include <linux/fs.h> /* for fops */
 #include <linux/device.h> /* for class_create */
 #include <linux/slab.h> /* for kmalloc */
 #include <linux/uaccess.h> /* for copy_to_user, access_ok */
-#include <linux/gfp.h> /* for get_zeroed_page */
+#include <linux/gfp.h> /* for get_aaaed_page */
 #include <linux/cdev.h> /* for cdev_* */
 #include <linux/sched.h> /* for cond_resched */
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Mark Veltzer");
-MODULE_DESCRIPTION("A simple implementation for something like /dev/zero");
+MODULE_DESCRIPTION("A simple implementation for something like /dev/aaa");
 
 /*
- * This is an implementation of a kernel module which exposes a /dev/zero like
+ * This is an implementation of a kernel module which exposes a /dev/aaa like
  * device to user space. Whenever someone reads from the device the buffer passed
- * to kernel gets filled with zeros.
+ * to kernel gets filled with aaas.
  * If you want to see the real implementation in the kernel see:
  * $KERNEL_SOURCES/drivers/char/mem.c
  */
-
-#define DO_CLEAR
-//#define DO_RESCHED
-//#define DO_COPY_TO_USER
 
 /* how many minors do we need ? */
 const int MINOR_COUNT = 1;
 
 /* these are the actual operations */
 
-static int open_zero(struct inode *inode, struct file *file)
-{
-#ifdef DO_COPY_TO_USER
-	file->private_data = get_zeroed_page(GFP_KERNEL);
-	pr_info("all is ok and buffer is %p\n", file->private_data);
-	/*
-	 * char* p=(char*)kmalloc(PAGE_SIZE, GFP_KERNEL);
-	 * if (IS_ERR(p))
-	 *	return ERR_PTR(p);
-	 * memset(p, 0, PAGE_SIZE);
-	 * file->private_data = (void*)p;
-	 */
-#endif // DO_COPY_TO_USER
-	return 0;
-}
-
-static ssize_t read_zero(struct file *file, char __user *buf, size_t count,
+static ssize_t read_aaa(struct file *file, char __user *buf, size_t count,
 		loff_t *ppos)
 {
-#ifdef DO_CLEAR
-	int ret;
-
-	ret = clear_user(buf, count);
-	if (ret)
-		return -EFAULT;
-	*ppos += count;
-	pr_info("returning %ld\n", count);
-	return count;
-#endif // DO_CLEAR
-#ifdef DO_COPY_TO_USER
-	ssize_t curr = min_t(ssize_t, remaining, PAGE_SIZE);
-
-	if (copy_to_user(buf, file->private_data, curr))
-		return -EFAULT;
-	*ppos += curr;
-	pr_info("returning %ld\n", count);
-	return curr;
-#endif // DO_COPY_TO_USER
-#ifdef DO_RESCHED
-	int ret;
-	ssize_t remaining;
-	/*
-	 * do the access checking right at the start so that we would not
-	 * start zeroing the users pages and only then find out that the
-	 * buffer is off the edge...
-	 */
-	ret = access_ok(buf, count);
+	size_t remaining;
+	int ret = access_ok(buf, count);
 	if (!ret)
 		return -EFAULT;
 
 	remaining = count;
 	while (remaining) {
-		ssize_t curr = min_t(ssize_t, remaining, PAGE_SIZE);
-		/*
-		 * very efficient way of clearing user memory once it has
-		 * been verified...
-		 */
-		__clear_user(buf, curr);
-		buf += curr;
-		remaining -= curr;
-		/* allow for voluntary pre-emption */
-		cond_resched();
+		ret = __put_user('a', buf++);
+		if (ret<0)
+			return ret;
+		remaining--;
 	}
-	*ppos += count;
-	pr_info("returning %ld\n", count);
 	return count;
-#endif // DO_RESCHED
-}
-
-static int release_zero(struct inode *inode, struct file *file)
-{
-#ifdef DO_COPY_TO_USER
-	free_page((unsigned long)file->private_data);
-	/* kfree(file->private_data); */
-#endif // DO_COPY_TO_USER
-	return 0;
 }
 
 /* this is the operations table */
-static const struct file_operations zero_fops = {
+static const struct file_operations aaa_fops = {
 	.owner = THIS_MODULE,
-	.open = open_zero,
-	.read = read_zero,
-	.release = release_zero,
+	.read = read_aaa,
 };
 
 /* this variable will store the class */
@@ -144,7 +78,7 @@ static dev_t first_dev;
 /* this is our first minor. automatically initialised to 0... */
 static int first_minor;
 
-static int zero_init(void)
+static int aaa_init(void)
 {
 	int err = 0;
 	/* allocate our own range of devices */
@@ -156,7 +90,7 @@ static int zero_init(void)
 	}
 	pr_info("allocated the region\n");
 	/* add the cdev structure, no error codes */
-	cdev_init(&my_cdev, &zero_fops);
+	cdev_init(&my_cdev, &aaa_fops);
 	err = cdev_add(&my_cdev, first_dev, MINOR_COUNT);
 	if (err) {
 		pr_err("cannot cdev_add\n");
@@ -195,7 +129,7 @@ err_final:
 	return err;
 }
 
-static void zero_exit(void)
+static void aaa_exit(void)
 {
 	device_destroy(my_class, first_dev);
 	class_destroy(my_class);
@@ -204,5 +138,5 @@ static void zero_exit(void)
 	pr_info("unloaded ok\n");
 }
 
-module_init(zero_init);
-module_exit(zero_exit);
+module_init(aaa_init);
+module_exit(aaa_exit);
